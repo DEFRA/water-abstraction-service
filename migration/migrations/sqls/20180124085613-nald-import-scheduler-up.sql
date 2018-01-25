43a4c053c652dbbef299e145c90b5c7597ed618e
Dave Cozens <davecozens@users.noreply.github.com>
2018-01-25 12:02:53 +0000

    hotfix dbmigrate

--- migration/migrations/sqls/20180124085613-nald-import-scheduler-up.sql
+++ migration/migrations/sqls/20180124085613-nald-import-scheduler-up.sql
@@ -1,21 +1,18 @@
-/* Replace with your SQL commands */
-
-CREATE TABLE "water"."scheduler" (
-  "task_id" SERIAL,
-  "task_type" varchar NOT NULL,
-  "licence_ref" varchar NOT NULL,
-  "task_config" varchar,
-  "next_run" timestamp(0),
-  "last_run" timestamp(0),
-  "log" varchar,
-  "status" smallint,
-  "running" smallint default 0,
-  "date_created" timestamp(0),
-  "date_updated" timestamp(0),
-
-  PRIMARY KEY ("task_type","licence_ref")
+CREATE TABLE water.scheduler
+(
+    task_id integer NOT NULL DEFAULT nextval('water.scheduler_task_id_seq'::regclass),
+    task_type character varying COLLATE pg_catalog."default" NOT NULL,
+    licence_ref character varying COLLATE pg_catalog."default" NOT NULL,
+    task_config character varying COLLATE pg_catalog."default",
+    next_run timestamp(0) without time zone,
+    last_run timestamp(0) without time zone,
+    log character varying COLLATE pg_catalog."default",
+    status smallint,
+    running smallint DEFAULT 0,
+    date_created timestamp(0) without time zone DEFAULT now(),
+    date_updated timestamp(0) without time zone DEFAULT now(),
+    CONSTRAINT scheduler_pkey PRIMARY KEY (task_type, licence_ref)
 )
-;
-
-ALTER TABLE "water"."scheduler" ALTER COLUMN date_created SET DEFAULT now();
-ALTER TABLE "water"."scheduler" ALTER COLUMN date_updated SET DEFAULT now();
+WITH (
+    OIDS = FALSE
+)
