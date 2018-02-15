3c985fd92e934362af83e02fcf962fde4a63b81a
James Carmichael <james@siteclick.co.uk>
2018-02-15 16:42:53 +0000

    NALD structure

--- scripts/export-anon.js
+++ scripts/export-anon.js
@@ -3,7 +3,12 @@
  */
 require('dotenv').config();
 const { Pool } = require('pg');
-const { mapValues, pick } = require('lodash');
+const { mapValues, pick, uniqBy } = require('lodash');
+const { walkObjectAsync, walkObject } = require('walk-object')
+const Promise = require('bluebird');
+const csvStringify = require('csv-stringify/lib/sync');
+const fs = require('fs');
+const writeFile = Promise.promisify(fs.writeFile);
 
 if (process.env.DATABASE_URL) {
  // get heroku db params from env vars
@@ -21,208 +26,326 @@ const pool = new Pool({
  connectionTimeoutMillis: 2000
 });
 
-
-
-/**
- * Get a random character
- * @param {String} char - the current value
- * @return {String} a randomised value
- */
-function getRandomChar(char) {
-  let possible;
-  if(char.match(/[ABCDEFGHIJKLMNOPQRSTUVWXYZ]/)) {
-    possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const structure = {
+  root : {
+    table : 'NALD_ABS_LICENCES',
+    cond : [{ LIC_NO : '03/28/01/0200'}],
+    hasMany : [{
+        table : 'NALD_ABS_LIC_VERSIONS',
+        join : {AABL_ID : 'ID', FGAC_REGION_CODE : 'FGAC_REGION_CODE'},
+        hasMany : [{
+          table : 'NALD_PARTIES',
+          join : {ID : 'ACON_APAR_ID', FGAC_REGION_CODE : 'FGAC_REGION_CODE'},
+          hasMany : [{
+            table : 'NALD_CONTACTS',
+            join : {APAR_ID : 'ID',  FGAC_REGION_CODE : 'FGAC_REGION_CODE'},
+            hasMany : [{
+              table : 'NALD_ADDRESSES',
+              join : {ID : 'AADD_ID',  FGAC_REGION_CODE : 'FGAC_REGION_CODE'},
+            }]
+          }]
+        }]
+    }, {
+        table : 'NALD_REP_UNITS',
+        join : {CODE : 'AREP_CAMS_CODE', FGAC_REGION_CODE : 'FGAC_REGION_CODE'},
+    }, {
+        table : 'NALD_LIC_ROLES',
+        join : {AABL_ID : 'ID', FGAC_REGION_CODE : 'FGAC_REGION_CODE'},
+        hasMany : [{
+          table : 'NALD_LIC_ROLE_TYPES',
+          join : { CODE : 'ALRT_CODE' }
+        }, {
+          table : 'NALD_PARTIES',
+          join : { ID : 'ACON_APAR_ID' }
+        }, {
+          table : 'NALD_ADDRESSES',
+          join : { ID : 'ACON_AADD_ID'}
+        }, {
+          table : 'NALD_CONT_NOS',
+          join : { ACON_APAR_ID : 'ACON_APAR_ID', ACON_AADD_ID : 'ACON_AADD_ID' },
+          hasMany : [{
+            table : 'NALD_CONT_NO_TYPES',
+            join : { CODE : 'ACNT_CODE' }
+          }]
+        }]
+    }, {
+      table : 'NALD_ABS_LIC_PURPOSES',
+      join : { AABV_AABL_ID : 'ID'},
+      hasMany : [
+        {
+          table : 'NALD_LIC_CONDITIONS',
+          join : { AABP_ID : 'ID',  FGAC_REGION_CODE : 'FGAC_REGION_CODE'},
+          hasMany : [{
+            table : 'NALD_LIC_COND_TYPES',
+            join : { CODE : 'ACIN_CODE' }
+          }]
+        },
+        {
+          table : 'NALD_LIC_AGRMNTS',
+          join : { AABP_ID : 'ID', FGAC_REGION_CODE : 'FGAC_REGION_CODE' }
+        },
+        {
+        table : 'NALD_ABS_PURP_POINTS',
+        join : { AABP_ID : 'ID'},
+        hasMany : [{
+          table : 'NALD_MEANS_OF_ABS',
+          join : { CODE : 'AMOA_CODE' }
+        }, {
+          table : 'NALD_POINTS',
+          join : { ID : 'AAIP_ID' },
+          hasMany : [{
+            table : 'NALD_SOURCES',
+            join : { CODE : 'ASRC_CODE' }
+          }]
+        }]
+      }]
+    }]
   }
-  else if(char.match(/[abcdefghijklmnopqrstuvwxyz]/)) {
-    possible = 'abcdefghijklmnopqrstuvwxyz';
-  }
-  else if(char.match(/[0123456789]/)) {
-    possible = '0123456789';
-  }
-  else {
-    return char;
-  }
-  return possible.charAt(Math.floor(Math.random() * possible.length));
 }
 
-
-/**
- * Randomise a single field value
- * @param {String} val - a value, e.g. a licence number, expiry date, text string
- * @return {String} a randomised version that looks similar
- */
-function randomiseValue(val) {
-  if(val.toLowerCase() === 'null') {
-    return val;
+async function getData(table, filter) {
+  let query = `SELECT * FROM import."${ table }" WHERE 1=1 `;
+  const params = [];
+  for(key in filter) {
+    params.push(filter[key]);
+    query += ` AND "${key}"=$${params.length} `;
   }
-  // Dates
-  if(val.match(/^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/)) {
-    return '01/01/2018';
+  console.log(query, params);
+  const {rows, error} = await pool.query(query, params);
+  if(error) {
+    console.error(error);
   }
-  // Other strings
-  let newStr = '';
-  for(let i =0; i<val.length; i++) {
-    newStr += getRandomChar(val.substr(i, 1));
-  }
-  return newStr;
-}
-
-
-/**
- * Randomise a row data object
- * @param {Object} row
- * @param {Array} omitFields - a list of fields to omit from randomisation
- * @return {Object} with data randomised
- */
-function randomiseRow(row, omitFields = []) {
-  const picked = pick(row, omitFields);
-  return Object.assign({}, mapValues(row, randomiseValue), picked);
-}
-
-
-/**
- * Gets the licence from licences table
- * @param {String} licenceNumber - the licence number
- * @return {Promise} resolves with randomised licence data
- */
-async function getLicence(licenceNumber) {
-  const sql = 'SELECT * FROM import."NALD_ABS_LICENCES" WHERE "LIC_NO"=$1';
-  const params = [licenceNumber];
-  const {rows} = await pool.query(sql, params);
-  return randomiseRow(rows[0], ['ID', 'LIC_NO', 'FGAC_REGION_CODE', 'CAMS_CODE']);
-}
-
-
-/**
- * Gets versions
- * @param {Number} licenceId
- * @param {Number} regionCode
- * @return {Promise} resolves with randomised version data
- */
- async function getVersions(licenceId, regionCode) {
-   const sql = 'select * from import."NALD_ABS_LIC_VERSIONS" where "AABL_ID"=$1 and "FGAC_REGION_CODE" = $2';
-   const params = [licenceId, regionCode];
-   const {rows} = await pool.query(sql, params);
-   const randomise = (row) => {
-     return randomiseRow(row, ['AABL_ID', 'FGAC_REGION_CODE', 'ACON_APAR_ID', 'ACON_AADD_ID']);
-   }
-   return rows.map(randomise);
- }
-
-
- /**
-  * Gets versions
-  * @param {Number} partyId
-  * @param {Number} regionCode
-  * @return {Promise} resolves with randomised version data
-  */
-async function getParties(partyId, regionCode) {
-  const sql = 'select p.* from import."NALD_PARTIES" p where p."ID"=$1 and "FGAC_REGION_CODE" = $2';
-  const params = [partyId, regionCode];
-  const {rows} = await pool.query(sql, params);
-  const randomise = (row) => {
-    return randomiseRow(row, ['ID', 'APAR_TYPE', 'FGAC_REGION_CODE']);
-  }
-  return rows.map(randomise);
-}
-
-/**
- * Gets party contacts
- * @param {Number} partyId
- * @param {Number} regionCode
- * @return {Promise} resolves with contact data rows
- */
-async function getPartyContacts(partyId, regionCode) {
-  const sql = 'select * FROM import."NALD_CONTACTS" c WHERE c."APAR_ID"=$1 and c."FGAC_REGION_CODE" = $2';
-  const params = [partyId, regionCode];
-  const {rows} = await pool.query(sql, params);
   return rows;
 }
 
 
+async function getCondData(table, cond) {
+  const rowSets = await Promise.map(cond, (cond) => {
+    return getData(table, cond);
+  });
+  // Merge arrays
+  const data = [];
+  rowSets.forEach((rows) => {
+    data.push(...rows);
+  });
+  return data;
+}
+
+async function nodeHandler ({ value, location, isLeaf }) {
+
+  if(value.table) {
+    if(value.cond) {
+
+      // Get data for this node
+      value.data = await getCondData(value.table, value.cond);
+
+      // Add conditions to child relationships
+      if(value.hasMany) {
+        value.hasMany.forEach(rel => {
+          rel.cond = value.data.map(row => {
+            return mapValues(rel.join, (foreignField) => {
+              return row[foreignField];
+            });
+          });
+        });
+      }
+    }
+  }
+  return;
+}
+
 /**
- * Gets party addresses
- * @param {Array} addressIds
- * @param {Number} regionCode
- * @return {Promise} resolves with address data rows
+ * Assumes that first column is primary key field
+ * @param {Array} data - array of objects
+ * @return {Array} - deduplicated on first column
  */
-async function getPartyAddresses(addressIds, regionCode) {
-  const sql = `select * from import."NALD_ADDRESSES" where "ID" IN (${ addressIds.map((x, i) => '$' +(i+1)).join(',') }) and "FGAC_REGION_CODE" = $${ addressIds.length+1}`;
-  const params = [...addressIds, regionCode];
-  const {rows} = await pool.query(sql, params);
-  const randomise = (row) => {
-    return randomiseRow(row, ['ID', 'FGAC_REGION_CODE']);
+function deDuplicate(data) {
+  if(data.length) {
+    const primaryKey = Object.keys(data[0])[0];
+
+    if(primaryKey === 'ID' || primaryKey.match(/_ID$/)) {
+        return uniqBy(data, item => item[primaryKey]);
+    }
+
+
   }
-  return rows.map(randomise);
+  return data;
 }
 
-
-
-/**
-* Gets cams
-* @param {Number} code
-* @param {Number} regionCode
-* @return {Promise} resolves with randomised version data
-*/
-async function getCams(code, regionCode) {
-  const sql = 'select * from import."NALD_REP_UNITS" where "CODE"=$1 and "FGAC_REGION_CODE" = $2';
-  const params = [code, regionCode];
-  const {rows} = await pool.query(sql, params);
-  const randomise = (row) => {
-    return randomiseRow(row, ['CODE', 'FGAC_REGION_CODE']);
-  }
-  return rows.map(randomise);
-}
-
-
-/**
-* Gets roles for licence
-* @param {Number} licenceId
-* @param {Number} regionCode
-* @return {Promise} resolves with randomised version data
-*/
-async function getRoles(licenceId, regionCode) {
-  const sql = 'select * from import."NALD_LIC_ROLES" where "CODE"=$1 and "FGAC_REGION_CODE" = $2';
-  const params = [code, regionCode];
-  const {rows} = await pool.query(sql, params);
-  const randomise = (row) => {
-    return randomiseRow(row, ['CODE', 'FGAC_REGION_CODE']);
-  }
-  return rows.map(randomise);
+function getExportData(structure) {
+  const data = {};
+  walkObject(structure, ({ value, location, isLeaf }) => {
+    if(value.data) {
+      data[value.table] = value.data;
+      //deDuplicate(value.data);
+    }
+  });
+  return data;
 }
 
 
 
 async function main() {
-  const data = {};
-  data.NALD_ABS_LICENCES = [];
-  data.NALD_ABS_LIC_VERSIONS = [];
-  data.NALD_PARTIES = [];
-  data.NALD_CONTACTS = [];
-  data.NALD_REP_UNITS = [];
-  data.NALD_ADDRESSES = [];
+    await walkObjectAsync(structure, nodeHandler);
 
+    const exportData = getExportData(structure);
 
-
-  const main = await getLicence(licNo);
-  const versions = await getVersions(main.ID, main.FGAC_REGION_CODE);
-
-  versions.forEach(async (version) => {
-    data.NALD_PARTIES.push(... await getParties(version.ACON_APAR_ID, main.FGAC_REGION_CODE));
-    const contacts = await getPartyContacts(version.ACON_APAR_ID, main.FGAC_REGION_CODE);
-    data.NALD_CONTACTS.push(...contacts);
-
-    const addressIds = contacts.map(contact => contact.AADD_ID);
-    data.NALD_ADDRESSES.push(...await getPartyAddresses(addressIds, main.FGAC_REGION_CODE));
-  });
-
-  data.NALD_ABS_LICENCES.push(main);
-  data.NALD_ABS_LIC_VERSIONS.push(versions);
-  data.NALD_REP_UNITS.push(... await getCams(main.CAMS_CODE, main.FGAC_REGION_CODE));
-
-  console.log(data);
-
-  return;
+    // Write CSV data
+    mapValues(exportData, (data, tableName) => {
+      writeFile('./data/' + tableName + '.csv', csvStringify(data, {header : true}));
+    });
 }
 
 main();
+
+
+
+
+
+//
+// /**
+//  * Gets the licence from licences table
+//  * @param {String} licenceNumber - the licence number
+//  * @return {Promise} resolves with randomised licence data
+//  */
+// async function getLicence(licenceNumber) {
+//   const sql = 'SELECT * FROM import."NALD_ABS_LICENCES" WHERE "LIC_NO"=$1';
+//   const params = [licenceNumber];
+//   const {rows} = await pool.query(sql, params);
+//   return randomiseRow(rows[0], ['ID', 'LIC_NO', 'FGAC_REGION_CODE', 'CAMS_CODE']);
+// }
+//
+//
+// /**
+//  * Gets versions
+//  * @param {Number} licenceId
+//  * @param {Number} regionCode
+//  * @return {Promise} resolves with randomised version data
+//  */
+//  async function getVersions(licenceId, regionCode) {
+//    const sql = 'select * from import."NALD_ABS_LIC_VERSIONS" where "AABL_ID"=$1 and "FGAC_REGION_CODE" = $2';
+//    const params = [licenceId, regionCode];
+//    const {rows} = await pool.query(sql, params);
+//    const randomise = (row) => {
+//      return randomiseRow(row, ['AABL_ID', 'FGAC_REGION_CODE', 'ACON_APAR_ID', 'ACON_AADD_ID']);
+//    }
+//    return rows.map(randomise);
+//  }
+//
+//
+//  /**
+//   * Gets versions
+//   * @param {Number} partyId
+//   * @param {Number} regionCode
+//   * @return {Promise} resolves with randomised version data
+//   */
+// async function getParties(partyId, regionCode) {
+//   const sql = 'select p.* from import."NALD_PARTIES" p where p."ID"=$1 and "FGAC_REGION_CODE" = $2';
+//   const params = [partyId, regionCode];
+//   const {rows} = await pool.query(sql, params);
+//   const randomise = (row) => {
+//     return randomiseRow(row, ['ID', 'APAR_TYPE', 'FGAC_REGION_CODE']);
+//   }
+//   return rows.map(randomise);
+// }
+//
+// /**
+//  * Gets party contacts
+//  * @param {Number} partyId
+//  * @param {Number} regionCode
+//  * @return {Promise} resolves with contact data rows
+//  */
+// async function getPartyContacts(partyId, regionCode) {
+//   const sql = 'select * FROM import."NALD_CONTACTS" c WHERE c."APAR_ID"=$1 and c."FGAC_REGION_CODE" = $2';
+//   const params = [partyId, regionCode];
+//   const {rows} = await pool.query(sql, params);
+//   return rows;
+// }
+//
+//
+// /**
+//  * Gets party addresses
+//  * @param {Array} addressIds
+//  * @param {Number} regionCode
+//  * @return {Promise} resolves with address data rows
+//  */
+// async function getPartyAddresses(addressIds, regionCode) {
+//   const sql = `select * from import."NALD_ADDRESSES" where "ID" IN (${ addressIds.map((x, i) => '$' +(i+1)).join(',') }) and "FGAC_REGION_CODE" = $${ addressIds.length+1}`;
+//   const params = [...addressIds, regionCode];
+//   const {rows} = await pool.query(sql, params);
+//   const randomise = (row) => {
+//     return randomiseRow(row, ['ID', 'FGAC_REGION_CODE']);
+//   }
+//   return rows.map(randomise);
+// }
+//
+//
+//
+// /**
+// * Gets cams
+// * @param {Number} code
+// * @param {Number} regionCode
+// * @return {Promise} resolves with randomised version data
+// */
+// async function getCams(code, regionCode) {
+//   const sql = 'select * from import."NALD_REP_UNITS" where "CODE"=$1 and "FGAC_REGION_CODE" = $2';
+//   const params = [code, regionCode];
+//   const {rows} = await pool.query(sql, params);
+//   const randomise = (row) => {
+//     return randomiseRow(row, ['CODE', 'FGAC_REGION_CODE']);
+//   }
+//   return rows.map(randomise);
+// }
+//
+//
+// /**
+// * Gets roles for licence
+// * @param {Number} licenceId
+// * @param {Number} regionCode
+// * @return {Promise} resolves with randomised version data
+// */
+// async function getRoles(licenceId, regionCode) {
+//   const sql = 'select * from import."NALD_LIC_ROLES" where "CODE"=$1 and "FGAC_REGION_CODE" = $2';
+//   const params = [code, regionCode];
+//   const {rows} = await pool.query(sql, params);
+//   const randomise = (row) => {
+//     return randomiseRow(row, ['CODE', 'FGAC_REGION_CODE']);
+//   }
+//   return rows.map(randomise);
+// }
+//
+//
+//
+// async function main() {
+//   const data = {};
+//   data.NALD_ABS_LICENCES = [];
+//   data.NALD_ABS_LIC_VERSIONS = [];
+//   data.NALD_PARTIES = [];
+//   data.NALD_CONTACTS = [];
+//   data.NALD_REP_UNITS = [];
+//   data.NALD_ADDRESSES = [];
+//
+//
+//
+//   const main = await getLicence(licNo);
+//   const versions = await getVersions(main.ID, main.FGAC_REGION_CODE);
+//
+//   versions.forEach(async (version) => {
+//     data.NALD_PARTIES.push(... await getParties(version.ACON_APAR_ID, main.FGAC_REGION_CODE));
+//     const contacts = await getPartyContacts(version.ACON_APAR_ID, main.FGAC_REGION_CODE);
+//     data.NALD_CONTACTS.push(...contacts);
+//
+//     const addressIds = contacts.map(contact => contact.AADD_ID);
+//     data.NALD_ADDRESSES.push(...await getPartyAddresses(addressIds, main.FGAC_REGION_CODE));
+//   });
+//
+//   data.NALD_ABS_LICENCES.push(main);
+//   data.NALD_ABS_LIC_VERSIONS.push(versions);
+//   data.NALD_REP_UNITS.push(... await getCams(main.CAMS_CODE, main.FGAC_REGION_CODE));
+//
+//   console.log(data);
+//
+//   return;
+// }
+
+// main();
