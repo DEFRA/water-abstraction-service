69075e0650504d5cbc24e8e5c2ada5385bc5d101
Dave Cozens <davecozens.defra@gmail.com>
2018-02-28 14:46:38 +0000

    Return contact methods (i.e. phone no, email) as an array in the main query rather than causing multiple rows duplicate contacts (see water-801)

--- src/lib/nald.js
+++ src/lib/nald.js
@@ -362,20 +362,41 @@ const getRoles = async(AABL_ID,FGAC_REGION_CODE) => {
   //  console.log('AABL_ID ',AABL_ID)
   client = await pool.connect()
   const res = await client.query(`
-      select row_to_json(r.*) AS role_detail,
-      row_to_json(t.*) AS role_type,
-      row_to_json(p.*) AS role_party,
-      row_to_json(a.*) AS role_address,
-      row_to_json(c.*) AS role_contact_no,
-      row_to_json(ct.*) AS role_contact_no_type
-      from
-      import."NALD_LIC_ROLES" r
-      left join import."NALD_LIC_ROLE_TYPES" t on r."ALRT_CODE"=t."CODE"
-      left join import."NALD_PARTIES" p on r."ACON_APAR_ID"=p."ID"
-      left join import."NALD_ADDRESSES" a on r."ACON_AADD_ID"=a."ID"
-      left join import."NALD_CONT_NOS" c on r."ACON_APAR_ID"=c."ACON_APAR_ID" and r."ACON_AADD_ID" = c."ACON_AADD_ID"
-      left join import."NALD_CONT_NO_TYPES" ct on c."ACNT_CODE"=ct."CODE"
-      where r."AABL_ID"=$1 and r."FGAC_REGION_CODE" = $2  and p."FGAC_REGION_CODE" = $2 and a."FGAC_REGION_CODE" = $2  and c."FGAC_REGION_CODE" = $2
+    select row_to_json(r.*) AS role_detail,
+          row_to_json(t.*) AS role_type,
+          row_to_json(p.*) AS role_party,
+          row_to_json(a.*) AS role_address,
+
+    			ARRAY(
+    			select
+    			row_to_json(x.*) AS contact_data from (
+    				SELECT
+    				*
+    				from import."NALD_CONT_NOS" c
+    				left join import."NALD_CONT_NO_TYPES" ct on c."ACNT_CODE"=ct."CODE"
+    				where r."ACON_APAR_ID"=c."ACON_APAR_ID" and r."ACON_AADD_ID" = c."ACON_AADD_ID"
+    				and c."FGAC_REGION_CODE" = $2
+
+    			) x
+    			)
+
+          from
+          import."NALD_LIC_ROLES" r
+          left join import."NALD_LIC_ROLE_TYPES" t on r."ALRT_CODE"=t."CODE"
+          left join import."NALD_PARTIES" p on r."ACON_APAR_ID"=p."ID"
+          left join import."NALD_ADDRESSES" a on r."ACON_AADD_ID"=a."ID"
+
+
+
+          where
+
+      r."AABL_ID"=$1
+      and r."FGAC_REGION_CODE" = $2
+      and p."FGAC_REGION_CODE" = $2
+      and a."FGAC_REGION_CODE" = $2
+      AND (r."EFF_END_DATE" = 'null'
+      OR to_date( r."EFF_END_DATE", 'DD/MM/YYYY' ) > now()
+      )
   `, [AABL_ID,FGAC_REGION_CODE])
   client.release()
   return res.rows
