a9ad557d1f8577c291870536adc4c98ec0dddfb0
James Carmichael <james@siteclick.co.uk>
2018-05-11 13:33:39 +0100

    Updating vulnerable packages

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,5 +1,3 @@
-var bcrypt = require('bcrypt');
-
 // contains generic functions unrelated to a specific component
 var rp = require('request-promise-native').defaults({
   proxy: null,
@@ -76,32 +74,7 @@ function createGUID () {
       .substring(1);
   }
   return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-s4() + '-' + s4() + s4() + s4();
-}
-
-function createHash (string, cb) {
-  const saltRounds = 10;
-  bcrypt.hash(string, saltRounds, function (err, hash) {
-    cb(err, hash);
-  });
-}
-
-function compareHash (string1, string2, cb) {
-  console.log('bcrypt compare');
-  console.log('atring 1 =' + string1);
-  console.log('atring 2 =' + string2);
-  bcrypt.compare(string1, string2, (err, res) => {
-    console.log('bcrypt compare');
-    console.log(err);
-    console.log(res);
-    if (res) {
-      console.log('password OK, authorised!');
-    } else {
-      console.log('password FAIL, unauthorised!');
-    }
-    console.log('-----');
-    cb(err, res);
-  });
+    s4() + '-' + s4() + s4() + s4();
 }
 
 function encryptToken (data) {
@@ -122,11 +95,8 @@ function decryptToken (token) {
 
 module.exports = {
   createGUID: createGUID,
-  createHash: createHash,
-  compareHash: compareHash,
   encryptToken: encryptToken,
   decryptToken: decryptToken,
   makeURIRequest,
   makeURIRequestWithBody
-
 };
