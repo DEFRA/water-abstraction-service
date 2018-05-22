2d5fe9703d9ef9543eded958eef9cd61d4ce3b94
James Carmichael <james@siteclick.co.uk>
2018-05-22 11:45:36 +0100

    Tweak migrations

--- migrations/sqls/20180518142203-notification-templates-up.sql
+++ migrations/sqls/20180518142203-notification-templates-up.sql
@@ -8,6 +8,6 @@ INSERT INTO "water"."notify_templates" (message_ref, template_id, notify_key)
   VALUES('notification_letter', 'c4b1f147-e357-4f81-b19e-fa686e05a9b1', 'test')
   ON CONFLICT(message_ref) DO NOTHING;
 
-INSERT INTO "water"."scheduler" (task_id, task_type, licence_ref, task_config, date_created)
-  VALUES(300, 'updateNotificationEvents', '-', '{"count":"30","period":"second"}', NOW())
+INSERT INTO "water"."scheduler" (task_id, status, task_type, licence_ref, task_config, date_created)
+  VALUES(300, 0, 'updateNotificationEvents', '-', '{"count":"2","period":"minute"}', NOW())
   ON CONFLICT(task_type, licence_ref) DO NOTHING;
--- migrations/sqls/20180521143238-notify-check-scheduler-up.sql
+++ migrations/sqls/20180521143238-notify-check-scheduler-up.sql
@@ -1,4 +1,4 @@
 /* Replace with your SQL commands */
-INSERT INTO "water"."scheduler" (task_id, task_type, licence_ref, task_config, date_created)
-  VALUES(301, 'updateNotifyStatus', '-', '{"count":"5","period":"minute"}', NOW())
+INSERT INTO "water"."scheduler" (task_id, status, task_type, licence_ref, task_config, date_created)
+  VALUES(301, 0, 'updateNotifyStatus', '-', '{"count":"2","period":"minute"}', NOW())
   ON CONFLICT(task_type, licence_ref) DO NOTHING;
