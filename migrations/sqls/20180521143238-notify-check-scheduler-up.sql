b8b4eac52a752dbfcbc05c5a89f90c5ebf13b3ea
James Carmichael <james@siteclick.co.uk>
2018-05-22 12:14:14 +0100

    Fix migrations

--- migrations/sqls/20180521143238-notify-check-scheduler-up.sql
+++ migrations/sqls/20180521143238-notify-check-scheduler-up.sql
@@ -1,4 +1,4 @@
 /* Replace with your SQL commands */
-INSERT INTO "water"."scheduler" (task_id, status, task_type, licence_ref, task_config, date_created)
-  VALUES(301, 0, 'updateNotifyStatus', '-', '{"count":"2","period":"minute"}', NOW())
+INSERT INTO "water"."scheduler" (task_id, status, task_type, licence_ref, task_config, date_created, next_run)
+  VALUES(301, 0, 'updateNotifyStatus', '-', '{"count":"2","period":"minute"}', NOW(), NOW())
   ON CONFLICT(task_type, licence_ref) DO NOTHING;
