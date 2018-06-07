f736ff6c9d1f218fa29739f60d0a0823354da23e
Mark Swaffer <mdswaffer@gmail.com>
2018-06-07 13:38:57 +0100

    bugfix/WATER-1086: Notifications updates
     - Render the address across multiple lines
     - Adds blockquote and sibling h1 styles.
     - Hide expiry date if there is not one in the renewal warning.

---
+++ migrations/20180606101104-update-hof-warning-notification.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var dbm;
+var type; // eslint-disable-line no-unused-vars
+var seed; // eslint-disable-line no-unused-vars
+var fs = require('fs');
+var path = require('path');
+var Promise;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+  Promise = options.Promise;
+};
+
+const runQuery = (db, sqlName) => {
+  const filePath = path.join(__dirname, 'sqls', sqlName);
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, {encoding: 'utf-8'}, function (err, data) {
+      if (err) return reject(err);
+
+      console.log('received data: ' + data);
+      resolve(data);
+    });
+  }).then(function (data) {
+    return db.runSql(data);
+  });
+};
+
+exports.up = function (db) {
+  return runQuery(db, '20180606101104-update-hof-warning-notification-up.sql');
+};
+
+exports.down = function (db) {
+  return runQuery(db, '20180606101104-update-hof-warning-notification-down.sql');
+};
+
+exports._meta = { 'version': 1 };
