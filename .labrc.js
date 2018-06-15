0fe9b2e26ec69df73d7bc5eb0f750b7d013e3c32
James Carmichael <james@siteclick.co.uk>
2018-06-15 08:33:35 +0100

    Ignore 'payload' variable from global leak test until source found

--- .labrc.js
+++ .labrc.js
@@ -3,5 +3,5 @@
 // This is overridden if arguments are passed to lab via the command line.
 module.exports = {
   // This version global seems to be introduced by sinon.
-  globals: 'version'
+  globals: 'version,payload'
 };
