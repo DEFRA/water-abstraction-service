779794497d5cbb034cfb0c5aad0d7fc0b3a54b3b
James Carmichael <james@siteclick.co.uk>
2018-06-19 11:42:07 +0100

    Feat/notify failsafe (#105)
    * Create task to update notify status for letters already queue
    * Tweaks to notify task.  Create migration to add scheduled task.
    * Run the status checks in series
    * Return {error} in catch block

---
+++ migrations/sqls/20180619075019-schedule-notify-check-up.sql
@@ -0,0 +1,4 @@
+/* Replace with your SQL commands */
+INSERT INTO "water"."scheduler" (task_id, status, task_type, licence_ref, task_config, date_created, next_run)
+  VALUES(301, 0, 'updateNotifyStatus', '-', '{"count":"1","period":"day"}', NOW(), NOW())
+  ON CONFLICT(task_type, licence_ref) DO NOTHING;
