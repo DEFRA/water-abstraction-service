6caa00a7e55ded82e024e5069106f4ba4361a978
James Carmichael <james@siteclick.co.uk>
2018-06-22 15:20:00 +0100

    Use correct notify key for checking message status

--- index.js
+++ index.js
@@ -21,23 +21,6 @@ logger.init(config.logger);
 // Define server
 const server = Hapi.server(config.server);
 
-/**
- * Validate JWT token
- * @param {Object} decoded - decoded data from JWT
- * @param {Object} request - current request
- * @return {Object} - result
- */
-async function validate (decoded, request) {
-  console.log('decoded', decoded);
-  if (!decoded.id) {
-    console.log('boo... JWT failed');
-    return { isValid: false };
-  } else {
-    console.log('huzah... JWT OK');
-    return { isValid: true };
-  }
-}
-
 const start = async function () {
   try {
     // Third-party plugins
@@ -58,25 +41,25 @@ const start = async function () {
     await server.register(HapiAuthJwt2);
     server.auth.strategy('jwt', 'jwt', {
       ...config.jwt,
-      validate
+      validate: async (decoded) => ({ isValid: !!decoded.id })
     });
     server.auth.default('jwt');
 
     // Import routes
     server.route(routes);
 
-    if (!module.parent) {
-      await server.start();
-      const name = process.env.servicename;
-      const uri = server.info.uri;
-      console.log(`Service ${name} running at: ${uri}`);
-    }
-
     // Set up PG Boss message queue
     await messageQueue.start();
     const { registerSubscribers } = require('./src/modules/notify')(messageQueue);
     registerSubscribers();
-    logger.info('Message queue started');
+    server.log('info', 'Message queue started');
+
+    if (!module.parent) {
+      await server.start();
+      const name = process.env.servicename;
+      const uri = server.info.uri;
+      server.log('info', `Service ${name} running at: ${uri}`);
+    }
   } catch (err) {
     logger.error(err);
   }
