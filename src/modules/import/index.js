0a2df87c4fa94da799d57285e32fa90f275941b5
James Carmichael <james@siteclick.co.uk>
2018-07-11 11:28:04 +0100

    Feat/import tweaks (#120)
    * Write prioritised list of licences to pending_imports table
    
    * Auto-import next licence in import queue
    
    * Add singleton key and retry to import job
    
    * Clear import log before downloading from S3
    
    * Only support full prioritised import for now
    
    * Simplify import by reducing higher order functions, use PG boss events to trigger sagas
    
    * Further tweaks.  Remove singleton key as this can prevent a job restarting.   When import fails, still schedule import of next licence.

--- src/modules/import/index.js
+++ src/modules/import/index.js
@@ -3,55 +3,88 @@ const Slack = require('../../lib/slack');
 const { downloadAndExtract } = require('./extract');
 const { loadScheduler } = require('./load-scheduler.js');
 const { load } = require('./load.js');
+const { getNextImport } = require('./lib/import-log.js');
+const { clearImportLog } = require('./lib/import-log');
 
-const registerImportLicence = (messageQueue) => {
-  messageQueue.subscribe('import.licence', async (job, done) => {
-    const {licenceNumber, index, licenceCount} = job.data;
-    try {
-      console.log(`Importing ${licenceNumber} (${index} of ${licenceCount})`);
-      await load(licenceNumber);
-    } catch (err) {
-      console.error(err);
-    }
+const importLicenceSubscriber = async (job, done) => {
+  try {
+    const { licenceNumber } = job.data;
+    console.log(`Importing ${licenceNumber}`);
+    await load(licenceNumber);
     done();
-  });
+  } catch (err) {
+    console.error(err);
+    done(err);
+  }
 };
 
-const registerLoadScheduler = (messageQueue) => {
-  messageQueue.subscribe('import.schedule', async (job, done) => {
-    try {
-      await Slack.post(`Import: scheduling licence imports`);
-      await loadScheduler(messageQueue);
-      await Slack.post(`Import: scheduling complete`);
-      done();
-    } catch (err) {
-      console.error(err);
-    }
-  });
+const scheduleImportSubscriber = async (job, done) => {
+  await Slack.post(`Import: scheduling licence imports`);
+  try {
+    await loadScheduler();
+    await Slack.post(`Import: scheduling complete`);
+    done();
+  } catch (err) {
+    console.error(err);
+  }
 };
 
-const createImportNald = messageQueue => {
+/**
+ * Creates a function which can handle a licence import
+ * @param {Object} messageQueue - PG Boss instance
+ * @return {Function} async function to import licence
+ */
+const importNextLicence = (messageQueue) => {
   return async () => {
     try {
-      await Slack.post(`Starting NALD data import`);
-      await downloadAndExtract();
-      await messageQueue.publish('import.schedule');
+      const row = await getNextImport();
+      if (row) {
+        const { licence_ref: licenceNumber } = row;
+        await messageQueue.publish('import.licence', { licenceNumber }, {
+          retryLimit: 3
+        });
+      } else {
+        messageQueue.publish('import.complete');
+      }
     } catch (err) {
       console.error(err);
     }
   };
 };
 
-const createRegisterSubscribers = messageQueue => {
-  return () => {
-    registerLoadScheduler(messageQueue);
-    registerImportLicence(messageQueue);
-  };
+const startImportSubscriber = async (job, done) => {
+  try {
+    await Slack.post(`Starting NALD data import`);
+    await clearImportLog();
+    await downloadAndExtract();
+    done();
+  } catch (err) {
+    console.error(err);
+    done(err);
+  }
 };
 
 module.exports = (messageQueue) => {
   return {
-    importNald: createImportNald(messageQueue),
-    registerSubscribers: createRegisterSubscribers(messageQueue)
+    importNald: () => {
+      messageQueue.publish('import.start');
+    },
+    registerSubscribers: async () => {
+      // Register event subscribers
+      await messageQueue.subscribe('import.start', startImportSubscriber);
+      await messageQueue.subscribe('import.schedule', scheduleImportSubscriber);
+      await messageQueue.subscribe('import.licence', importLicenceSubscriber);
+
+      // Register state-based subscribers
+      messageQueue.onComplete('import.start', () => {
+        messageQueue.publish('import.schedule');
+      });
+      messageQueue.onComplete('import.schedule', importNextLicence(messageQueue));
+      messageQueue.onComplete('import.licence', importNextLicence(messageQueue));
+      messageQueue.onFail('import.licence', importNextLicence(messageQueue));
+
+      // Import next licence in queue on startup
+      importNextLicence(messageQueue)();
+    }
   };
 };
--- src/modules/import/lib/nald-queries.js
+++ src/modules/import/lib/nald-queries.js
@@ -1,24 +1,4 @@
-const { pool } = require('../../../lib/connectors/db');
-
-/**
- * Perform a database query by getting a client from the connection pool and releasing
- * If an error is thrown, the client is still released and the error rethrown
- * @param {String} query - SQL query
- * @param {Array} params - bound SQL query params
- * @return {Promise} resolves with query data
- */
-const dbQuery = async (query, params = []) => {
-  const client = await pool.connect();
-  try {
-    const res = await client.query(query, params);
-    client.release();
-    return res.rows;
-  } catch (error) {
-    console.error(error);
-    client.release();
-    throw error;
-  }
-};
+const { dbQuery } = require('./db');
 
 const getMain = async (licenceNo) => {
   const query = `
@@ -243,15 +223,6 @@ const getPurposePointLicenceConditions = async (AABP_ID, FGAC_REGION_CODE) => {
   return dbQuery(query, params);
 };
 
-/**
- * Gets a list of all the licences for import
- * @return {Promise} resolves with array of data from import table
- */
-const getImportLicences = async () => {
-  const sql = `SELECT "LIC_NO", "REV_DATE", "LAPSED_DATE" FROM "import"."NALD_ABS_LICENCES"`;
-  return dbQuery(sql);
-};
-
 module.exports = {
   getMain,
   getCams,
@@ -266,6 +237,5 @@ module.exports = {
   getPurposePoints,
   getPurpose,
   getPurposePointLicenceAgreements,
-  getPurposePointLicenceConditions,
-  getImportLicences
+  getPurposePointLicenceConditions
 };
