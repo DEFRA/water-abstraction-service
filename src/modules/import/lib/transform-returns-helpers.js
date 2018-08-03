cf4823396a0026f9bd2421ad4287bd012a8e8e7d
James Carmichael <james@siteclick.co.uk>
2018-08-03 17:29:46 +0100

    Calculate returns cycles.  Also add endpoints for admin UI

--- src/modules/import/lib/transform-returns-helpers.js
+++ src/modules/import/lib/transform-returns-helpers.js
@@ -1,5 +1,6 @@
 const moment = require('moment');
-const { mapValues } = require('lodash');
+const { mapValues, chunk } = require('lodash');
+const { formatAbstractionPoint } = require('../../../lib/licence-transformer/nald-helpers');
 
 /**
  * Converts 'null' strings to real null in supplied object
@@ -85,11 +86,171 @@ const mapUsability = (u) => {
   return options[u];
 };
 
+/**
+ * Gets metadata object to store in returns table
+ * @param {Object} format
+ * @return {Object} return metadata
+ */
+const formatReturnMetadata = (format) => {
+  return {
+    version: 1,
+    description: format.SITE_DESCR,
+    purposes: format.purposes.map(purpose => ({
+      primary: {
+        code: purpose.APUR_APPR_CODE,
+        description: purpose.primary_purpose
+      },
+      secondary: {
+        code: purpose.APUR_APSE_CODE,
+        description: purpose.secondary_purpose
+      },
+      tertiary: {
+        code: purpose.APUR_APUS_CODE,
+        description: purpose.tertiary_purpose
+      }
+    })),
+    points: format.points.map(point => formatAbstractionPoint(convertNullStrings(point))),
+    nald: {
+      regionCode: parseInt(format.FGAC_REGION_CODE),
+      formatId: parseInt(format.ID),
+      // dateFrom: dateToIsoString(log.DATE_FROM),
+      // dateTo: dateToIsoString(log.DATE_TO),
+      // dateReceived: dateToIsoString(log.RECD_DATE),
+      periodStartDay: format.ABS_PERIOD_ST_DAY,
+      periodStartMonth: format.ABS_PERIOD_ST_MONTH,
+      periodEndDay: format.ABS_PERIOD_END_DAY,
+      periodEndMonth: format.ABS_PERIOD_END_MONTH
+      // underQuery: log.UNDER_QUERY_FLAG === 'Y'
+    }
+  };
+};
+
+/**
+ * Maps NALD production month
+ * @param {Number} month
+ * @return {Object}
+ */
+const mapProductionMonth = (month) => {
+  const intMonth = parseInt(month);
+  return {
+    isSummer: [65, 45, 80].includes(intMonth),
+    isUpload: [65, 66].includes(intMonth),
+    isLineEntry: [45, 46].includes(intMonth),
+    formProduced: [80, 70].includes(intMonth)
+  };
+};
+
+/**
+ * Gets returns cycles given a list of return formats
+ * @param {Array} formats
+ * @return {array}
+ */
+const getCycles = (formats) => {
+  const dates = [];
+
+  for (let format of formats) {
+    const info = mapProductionMonth(format.FORM_PRODN_MONTH);
+    const effStart = moment(format.EFF_ST_DATE, 'DD/MM/YYYY');
+    const effEnd = format.EFF_END_DATE === 'null' ? null : moment(format.EFF_END_DATE, 'DD/MM/YYYY');
+
+    const yearEnd = moment().month(2).date(31);
+
+    const endDate = effEnd || moment();
+
+    let datePtr = effStart;
+
+    while (datePtr.isBefore(endDate)) {
+      dates.push({format, info, date: effStart.format('YYYY-MM-DD')});
+
+      if (info.isSummer && datePtr.month() === 3) {
+        datePtr.month(8).date(30);
+        dates.push({format, info, date: datePtr.format('YYYY-MM-DD')});
+        datePtr.month(9).date(1);
+      } else {
+        datePtr.month(2).date(31).year(datePtr.year() + 1);
+        dates.push({format, info, date: datePtr.format('YYYY-MM-DD')});
+        datePtr.month(3).date(1);
+      }
+    }
+  }
+
+  const cycles = chunk(dates, 2).map(pair => ({
+    format: pair[0].format,
+    info: pair[0].info,
+    startDate: pair[0].date,
+    endDate: pair[1].date
+  }));
+
+  return cycles;
+};
+
+/**
+ * Gets period from the format and log
+ * @param {Object} format
+ * @param {Object} log
+ * @return {Object} start/end dates
+ */
+const getPeriod = (format, log) => {
+  const {
+    ABS_PERIOD_ST_DAY: startDay,
+    ABS_PERIOD_ST_MONTH: startMonth,
+    ABS_PERIOD_END_DAY: endDay,
+    ABS_PERIOD_END_MONTH: endMonth
+  } = format;
+
+  const startYear = moment(log.DATE_FROM, 'DD/MM/YYYY').year();
+  const endYear = endMonth < startMonth ? startYear + 1 : startYear;
+
+  // Need to create/retrieve a return for the specified period
+  const startDate = moment().year(startYear).month(startMonth - 1).date(startDay).format('YYYY-MM-DD');
+  const endDate = moment().year(endYear).month(endMonth - 1).date(endDay).format('YYYY-MM-DD');
+
+  return {
+    startDate,
+    endDate
+  };
+};
+
+/**
+ * Creates / retrieves return in list
+ * @param {Array} rows of returns
+ * @return {Object} created/retrieved return row
+ */
+// const createReturn = (licenceNumber, returns, format, log) => {
+//
+//
+//   const returnId = `v1:${format.FGAC_REGION_CODE}:${licenceNumber}:${format.ID}:${startDate}`;
+//
+//   // Create new return row
+//   returnRow = {
+//     return_id: returnId,
+//     regime: 'water',
+//     licence_type: 'abstraction',
+//     licence_ref: licenceNumber,
+//     start_date: startDate,
+//     end_date: endDate,
+//     returns_frequency: mapFrequency(format.ARTC_REC_FREQ_CODE),
+//     status: 'complete',
+//     source: 'NALD',
+//     metadata: JSON.stringify(formatReturnMetadata(format)),
+//     received_date: log.RECD_DATE === '' ? null : dateToIsoString(log.RECD_DATE)
+//   };
+//
+//   console.log('Create return!', startDate, endDate);
+//
+//   returns.push(returnRow);
+//   return returnRow;
+// };
+
 module.exports = {
   convertNullStrings,
   mapFrequency,
   mapPeriod,
   getStartDate,
   mapUnit,
-  mapUsability
+  mapUsability,
+  // findReturn,
+  getPeriod,
+  formatReturnMetadata,
+  getCycles
 };
--- src/modules/import/transform-returns.js
+++ src/modules/import/transform-returns.js
@@ -1,36 +1,39 @@
-const { dateToIsoString, returnsDateToIso } = require('./lib/date-helpers');
-
-const { formatAbstractionPoint } = require('../../lib/licence-transformer/nald-helpers');
+const { returnsDateToIso } = require('./lib/date-helpers');
+// const { formatAbstractionPoint } = require('../../lib/licence-transformer/nald-helpers');
 
 const {
   getFormats,
   getFormatPurposes,
   getFormatPoints,
-  getLogs,
+  // getLogs,
   getLines
 } = require('./lib/nald-returns-queries.js');
 
 const {
-  convertNullStrings,
+  // convertNullStrings,
   mapFrequency,
   mapPeriod,
   getStartDate,
   mapUnit,
-  mapUsability
+  mapUsability,
+  formatReturnMetadata,
+  // getPeriod,
+  getCycles
 } = require('./lib/transform-returns-helpers.js');
 
 const buildReturnsPacket = async (licenceNumber) => {
   const formats = await getFormats(licenceNumber);
 
-  for (let format of formats) {
-    let logs = await getLogs(format.ID, format.FGAC_REGION_CODE);
-    for (let log of logs) {
-      log.lines = await getLines(format.ID, format.FGAC_REGION_CODE, log.DATE_FROM);
-    }
+  // Calculate return cycles for formats
+  const cycles = getCycles(formats);
+  for (let cycle of cycles) {
+    cycle.lines = await getLines(cycle.format.ID, cycle.format.FGAC_REGION_CODE, cycle.startDate, cycle.endDate);
+  }
 
+  // Get format points/purposes
+  for (let format of formats) {
     format.purposes = await getFormatPurposes(format.ID, format.FGAC_REGION_CODE);
     format.points = await getFormatPoints(format.ID, format.FGAC_REGION_CODE);
-    format.logs = logs;
   }
 
   const returnsData = {
@@ -39,87 +42,57 @@ const buildReturnsPacket = async (licenceNumber) => {
     lines: []
   };
 
-  for (let format of formats) {
-    for (let log of format.logs) {
-      const startDate = dateToIsoString(log.DATE_FROM);
-      const endDate = dateToIsoString(log.DATE_TO);
-      const logId = `${startDate}:${endDate}`;
-      const returnId = `v1:${format.FGAC_REGION_CODE}:${licenceNumber}:${format.ID}:${logId}`;
+  for (let cycle of cycles) {
+    const { format, startDate, endDate } = cycle;
+    const returnId = `v1:${format.FGAC_REGION_CODE}:${licenceNumber}:${format.ID}:${startDate}:${endDate}`;
 
-      const returnRow = {
-        return_id: returnId,
-        regime: 'water',
-        licence_type: 'abstraction',
-        licence_ref: licenceNumber,
+    // Create new return row
+    const returnRow = {
+      return_id: returnId,
+      regime: 'water',
+      licence_type: 'abstraction',
+      licence_ref: licenceNumber,
+      start_date: startDate,
+      end_date: endDate,
+      returns_frequency: mapFrequency(format.ARTC_REC_FREQ_CODE),
+      status: 'complete',
+      source: 'NALD',
+      metadata: JSON.stringify(formatReturnMetadata(format)),
+      received_date: '2018-01-01'
+    };
+
+      // Create new version row
+    const versionRow = {
+      version_id: returnId,
+      return_id: returnId,
+      version_number: 1,
+      user_id: 'water-abstraction-service',
+      user_type: 'agency',
+      metadata: '{}',
+      nil_return: false
+    };
+
+    returnsData.returns.push(returnRow);
+    returnsData.versions.push(versionRow);
+    //
+
+    for (let line of cycle.lines) {
+      const startDate = getStartDate(line.ARFL_DATE_FROM, line.RET_DATE, format.ARTC_REC_FREQ_CODE);
+      const endDate = returnsDateToIso(line.RET_DATE);
+      const lineRow = {
+        line_id: `${returnId}:${line.ARFL_DATE_FROM}:${startDate}:${endDate}`,
+        version_id: returnId,
+        substance: 'water',
+        quantity: line.RET_QTY === '' ? null : parseFloat(line.RET_QTY),
+        unit: mapUnit(line.UNIT_RET_FLAG) || '?',
         start_date: startDate,
         end_date: endDate,
-        returns_frequency: mapFrequency(format.ARTC_REC_FREQ_CODE),
-        status: 'complete',
-        source: 'NALD',
-        metadata: JSON.stringify({
-          version: 1,
-          description: format.SITE_DESCR,
-          purposes: format.purposes.map(purpose => ({
-            primary: {
-              code: purpose.APUR_APPR_CODE,
-              description: purpose.primary_purpose
-            },
-            secondary: {
-              code: purpose.APUR_APSE_CODE,
-              description: purpose.secondary_purpose
-            },
-            tertiary: {
-              code: purpose.APUR_APUS_CODE,
-              description: purpose.tertiary_purpose
-            }
-          })),
-          points: format.points.map(point => formatAbstractionPoint(convertNullStrings(point))),
-          nald: {
-            regionCode: parseInt(format.FGAC_REGION_CODE),
-            formatId: parseInt(format.ID),
-            dateFrom: dateToIsoString(log.DATE_FROM),
-            dateTo: dateToIsoString(log.DATE_TO),
-            dateReceived: dateToIsoString(log.RECD_DATE),
-            periodStartDay: format.ABS_PERIOD_ST_DAY,
-            periodStartMonth: format.ABS_PERIOD_ST_MONTH,
-            periodEndDay: format.ABS_PERIOD_END_DAY,
-            periodEndMonth: format.ABS_PERIOD_END_MONTH,
-            underQuery: log.UNDER_QUERY_FLAG === 'Y'
-          }
-        }),
-        received_date: log.RECD_DATE === '' ? null : dateToIsoString(log.RECD_DATE)
-      };
-
-      const versionRow = {
-        version_id: returnId,
-        return_id: returnId,
-        version_number: 1,
-        user_id: 'water-abstraction-service',
-        user_type: 'agency',
+        time_period: mapPeriod(format.ARTC_REC_FREQ_CODE),
         metadata: '{}',
-        nil_return: log.lines.length === 0
+        reading_type: mapUsability(line.RET_QTY_USABILITY)
       };
 
-      returnsData.returns.push(returnRow);
-      returnsData.versions.push(versionRow);
-
-      for (let line of log.lines) {
-        const endDate = returnsDateToIso(line.RET_DATE);
-        const lineRow = {
-          line_id: `${returnId}:${line.RET_DATE}`,
-          version_id: returnId,
-          substance: 'water',
-          quantity: line.RET_QTY === '' ? null : parseFloat(line.RET_QTY),
-          unit: mapUnit(line.UNIT_RET_FLAG) || '?',
-          start_date: getStartDate(line.ARFL_DATE_FROM, line.RET_DATE, format.ARTC_REC_FREQ_CODE),
-          end_date: endDate,
-          time_period: mapPeriod(format.ARTC_REC_FREQ_CODE),
-          metadata: '{}',
-          reading_type: mapUsability(line.RET_QTY_USABILITY)
-        };
-
-        returnsData.lines.push(lineRow);
-      }
+      returnsData.lines.push(lineRow);
     }
   }
 
