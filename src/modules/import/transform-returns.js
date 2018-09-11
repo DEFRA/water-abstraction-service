9024cb25c71d9ec575cc5b5d68ff19ac185be5cc
James Carmichael <james@siteclick.co.uk>
2018-09-11 11:30:28 +0100

    Improve split log handling (#147)
    * Improve split log handling
    
    * Refactor
    
    * Refactor build returns packet
    
    * Reduce nesting
    
    * Move calculation of version start date to reduce buildReturnsPacket complexity
    
    * Tweak return cycles calculator, add additional tests
    
    * Re-write return cycle calculator, add unit tests, delete unused code

--- src/modules/import/transform-returns.js
+++ src/modules/import/transform-returns.js
@@ -4,7 +4,6 @@ const {
   getFormats,
   getFormatPurposes,
   getFormatPoints,
-  getLogs,
   getLogsForPeriod
 } = require('./lib/nald-returns-queries.js');
 
@@ -12,18 +11,19 @@ const {
   mapPeriod,
   formatReturnMetadata,
   getFormatCycles,
-  getCurrentCycles,
   mapReceivedDate,
   getReturnId
 } = require('./lib/transform-returns-helpers.js');
 
 /**
- * @param {String} licenceNumber - the abstraction licence number
- * @param {String} currentVersionStart - the start date of the current version of the licence in format DD/MM/YYYY
+ * Loads licence formats from DB
+ * @param {String} licenceNumber
+ * @param {Object} currentVersionStart - DD/MM/YYYY
+ * @return {Promise} resolves with array of formats
  */
-const buildReturnsPacket = async (licenceNumber, currentVersionStart) => {
+const getLicenceFormats = async (licenceNumber, currentVersionStart) => {
   // Create moment for the start date of the current licence version
-  const versionStartDate = currentVersionStart ? moment(currentVersionStart, 'DD/MM/YYYY') : null;
+  const versionStartDate = currentVersionStart ? moment(currentVersionStart, 'DD/MM/YYYY').format('YYYY-MM-DD') : null;
 
   const formats = await getFormats(licenceNumber);
 
@@ -31,9 +31,18 @@ const buildReturnsPacket = async (licenceNumber, currentVersionStart) => {
   for (let format of formats) {
     format.purposes = await getFormatPurposes(format.ID, format.FGAC_REGION_CODE);
     format.points = await getFormatPoints(format.ID, format.FGAC_REGION_CODE);
-    format.logs = await getLogs(format.ID, format.FGAC_REGION_CODE);
-    format.cycles = getCurrentCycles(getFormatCycles(format), versionStartDate);
+    format.cycles = getFormatCycles(format, versionStartDate);
   }
+
+  return formats;
+};
+
+/**
+ * @param {String} licenceNumber - the abstraction licence number
+ * @param {String} currentVersionStart - the start date of the current version of the licence in format DD/MM/YYYY
+ */
+const buildReturnsPacket = async (licenceNumber, currentVersionStart) => {
+  const formats = await getLicenceFormats(licenceNumber, currentVersionStart);
 
   const returnsData = {
     returns: []
@@ -45,6 +54,12 @@ const buildReturnsPacket = async (licenceNumber, currentVersionStart) => {
 
       // Get all form logs relating to this cycle
       const cycleLogs = await getLogsForPeriod(format.ID, format.FGAC_REGION_CODE, startDate, endDate);
+
+      // Only create return cycles for formats with logs to allow NALD prepop to
+      // drive online returns
+      if (cycleLogs.length === 0) {
+        continue;
+      }
 
       const returnId = getReturnId(licenceNumber, format, startDate, endDate);
 
