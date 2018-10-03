81511cf662c4cdde9e04ef76dcc359451c8e8340
James Carmichael <james@siteclick.co.uk>
2018-10-03 14:19:34 +0100

    Add unit tests for de-duplication

---
+++ test/modules/returns-invitation/lib/de-duplicate.js
@@ -0,0 +1,176 @@
+const { expect } = require('code');
+const { experiment, test } = exports.lab = require('lab').script();
+
+const {
+  dedupe,
+  getContactId,
+  getPreferredContact,
+  transformContact
+} = require('../../../../src/modules/returns-invitation/lib/de-duplicate.js');
+
+const createContact = (contact, group, licenceRef, role) => {
+  const contacts = {
+    A: {
+      name: 'Bob',
+      address_line_1: 'Daisy Farm',
+      postcode: 'DD1 1DD'
+    },
+    B: {
+      name: 'Dave',
+      address_line_1: 'Buttercup Farm',
+      postcode: 'BB1 1BB'
+    },
+    C: {
+      name: 'Katie',
+      address_line_1: 'Lavender Farm',
+      postcode: 'LL1 1LL'
+    }
+  };
+
+  const selectedContact = contacts[contact];
+  const contactWithRole = role ? { ...selectedContact, role } : selectedContact;
+
+  return {
+    contact: contactWithRole,
+    data: {
+      licence_ref: licenceRef
+    },
+    group
+  };
+};
+
+experiment('De-duplication', () => {
+  test('It should de-duplicate when contact is the same and in the same group', async () => {
+    const contacts = [createContact('A', 'A', 'A'), createContact('A', 'A', 'B')];
+    const result = dedupe(contacts, getContactId);
+    expect(result).to.equal([
+      {
+        'contact': {
+          'name': 'Bob',
+          'address_line_1': 'Daisy Farm',
+          'postcode': 'DD1 1DD'
+        },
+        'data': [
+          {
+            'licence_ref': 'A'
+          },
+          {
+            'licence_ref': 'B'
+          }
+        ]
+      }
+    ]);
+  });
+
+  test('It should not de-duplicate when contact is different and in the same group', async () => {
+    const contacts = [createContact('A', 'A', 'A'), createContact('B', 'A', 'B')];
+    const result = dedupe(contacts, getContactId);
+    expect(result).to.equal([
+      {
+        'contact': {
+          'name': 'Bob',
+          'address_line_1': 'Daisy Farm',
+          'postcode': 'DD1 1DD'
+        },
+        'data': [
+          {
+            'licence_ref': 'A'
+          }
+        ]
+      },
+      {
+        'contact': {
+          'name': 'Dave',
+          'address_line_1': 'Buttercup Farm',
+          'postcode': 'BB1 1BB'
+        },
+        'data': [
+          {
+            'licence_ref': 'B'
+          }
+        ]
+      }
+    ]);
+  });
+
+  test('It should not de-duplicate when contact is the same and in different group', async () => {
+    const contacts = [createContact('A', 'A', 'A'), createContact('A', 'B', 'B')];
+    const result = dedupe(contacts, getContactId);
+    expect(result).to.equal([
+      {
+        'contact': {
+          'name': 'Bob',
+          'address_line_1': 'Daisy Farm',
+          'postcode': 'DD1 1DD'
+        },
+        'data': [
+          {
+            'licence_ref': 'A'
+          }
+        ]
+      },
+      {
+        'contact': {
+          'name': 'Bob',
+          'address_line_1': 'Daisy Farm',
+          'postcode': 'DD1 1DD'
+        },
+        'data': [
+          {
+            'licence_ref': 'B'
+          }
+        ]
+      }
+    ]);
+  });
+});
+
+experiment('getContactId', () => {
+  test('It should create a contact ID when none is present', async () => {
+    const contact = createContact('A', 'A', 'A');
+    const contactId = getContactId(contact);
+    expect(contactId).to.equal('d90742fb30bd56e913ed9ef6159cc4edef010a0c');
+  });
+  test('It should use entity ID if present', async () => {
+    const entityId = 'ce0c2172-1589-4214-8d20-eab7760e87ce';
+    const contact = {
+      ...createContact('A', 'A', 'A'),
+      entity_id: entityId
+    };
+    const contactId = getContactId(contact);
+    expect(contactId).to.equal(entityId);
+  });
+});
+
+experiment('getPreferredContact', () => {
+  const contactA = createContact('A', 'A', 'A', 'licence_holder');
+  const contactB = createContact('B', 'A', 'A', 'returns_contact');
+
+  const contacts = [contactA, contactB].map(data => data.contact);
+
+  test('It should select preferred contact by role', async () => {
+    const contact = getPreferredContact(contacts, ['licence_holder', 'returns_contact']);
+    expect(contact).to.equal({ name: 'Bob',
+      address_line_1: 'Daisy Farm',
+      postcode: 'DD1 1DD',
+      role: 'licence_holder' });
+  });
+
+  test('It should select preferred contact by role', async () => {
+    const contact = getPreferredContact(contacts, ['returns_contact', 'licence_holder']);
+    expect(contact).to.equal({ name: 'Dave',
+      address_line_1: 'Buttercup Farm',
+      postcode: 'BB1 1BB',
+      role: 'returns_contact' });
+  });
+
+  test('It should return nothing if no contact found', async () => {
+    const contact = getPreferredContact(contacts, ['no_contacts_with_this_role']);
+    expect(contact).to.equal(undefined);
+  });
+});
+
+experiment('transformContact', () => {
+  const contactA = createContact('A', 'A', 'A', 'licence_holder');
+  const contactB = createContact('B', 'A', 'A', 'returns_contact');
+});
