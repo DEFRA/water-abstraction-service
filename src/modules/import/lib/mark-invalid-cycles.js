7749ec68f65c5af9a9aeab593dd1a66cee1e7053
James Carmichael <james@siteclick.co.uk>
2018-10-05 10:53:56 +0100

    Delete records directly

--- src/modules/import/lib/mark-invalid-cycles.js
+++ src/modules/import/lib/mark-invalid-cycles.js
@@ -3,36 +3,8 @@
  * others and marks them for deletion by setting a metadata flag
  *
  */
-const Boom = require('boom');
 const { returns } = require('../../../lib/connectors/returns');
 const { findAllPages } = require('../../../lib/api-client-helpers');
-
-/**
- * Loads the metadata for a given return, merges with supplied metadata,
- * and saves the return
- * @param {String} returnId
- * @param {Object} metadata
- * @return {Promise} resolves when return updated
- */
-const mergeReturnMetadada = async (returnId, metadata = {}) => {
-  const filter = { return_id: returnId };
-
-  // Find return
-  const { error, data: [ret] } = await returns.findMany(filter, {}, null, ['metadata']);
-  if (error) {
-    throw Boom.badImplementation(`Error retrieving return ${returnId}`, error);
-  }
-  if (!ret) {
-    throw Boom.notFound(`Return ${returnId} not found`);
-  }
-
-  const newMetadata = {
-    ...ret.metadata,
-    ...metadata
-  };
-
-  return returns.updateMany(filter, { metadata: JSON.stringify(newMetadata) });
-};
 
 /**
  * Gets the filter which will be used to find returns whose IDs
@@ -42,15 +14,18 @@ const mergeReturnMetadada = async (returnId, metadata = {}) => {
  * @return {Object}
  */
 const getFilter = (licenceNumber, returnIds) => {
-  return {
-    return_id: {
-      $nin: returnIds
-    },
+  const filter = {
     regime: 'water',
     licence_type: 'abstraction',
     licence_ref: licenceNumber,
     source: 'NALD'
   };
+  if (returnIds.length) {
+    filter.return_id = {
+      $nin: returnIds
+    };
+  }
+  return filter;
 };
 
 /**
@@ -63,8 +38,8 @@ const markInvalidCycles = async (licenceNumber, returnIds) => {
   const filter = getFilter(licenceNumber, returnIds);
   const rows = await findAllPages(returns, filter, {}, ['return_id', 'metadata']);
   const tasks = rows.map(row => {
-    console.log(`Marking ${row.return_id} for deletion`);
-    return mergeReturnMetadada(row.return_id, { deleted: true });
+    console.log(`Deleting invalid return ${row.return_id}`);
+    return returns.delete(row.return_id);
   });
 
   return Promise.all(tasks);
