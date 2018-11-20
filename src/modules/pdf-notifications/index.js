9c3949c756d5e8228fe18a0272a7d2a08e4a273d
James Carmichael <james@siteclick.co.uk>
2018-11-20 13:59:05 +0000

    Add return reminder template.  Add QR code containing return ID to both paper form and reminder templates

--- src/modules/pdf-notifications/index.js
+++ src/modules/pdf-notifications/index.js
@@ -1,3 +1,4 @@
+const QRCode = require('qrcode-svg');
 const scheduledNotification = require('../../controllers/notifications').repository;
 const { findOne } = require('../../lib/repository-helpers');
 const nunjucks = require('nunjucks');
@@ -10,11 +11,32 @@ const env = nunjucks.configure('./src/views/pdf-notifications/', {
   noCache: testMode
 });
 
+/**
+ * Create render function that renders using Nunjucks environment
+ * @param {String} view - the view/template name
+ * @param {Object} data - data to send to view
+ * @return {Promise} resolves with rendered template, or rejects with error
+ */
+const render = (view, data) => {
+  return new Promise((resolve, reject) => {
+    env.render(view, data, (err, result) => {
+      return err ? reject(err) : resolve(result);
+    });
+  });
+};
+
 env.addFilter('naldRegion', viewHelpers.naldRegion);
 env.addFilter('naldArea', viewHelpers.naldArea);
 env.addFilter('date', viewHelpers.dateFormat);
 env.addGlobal('paginateReturnLines', viewHelpers.paginateReturnLines);
 env.addFilter('stringify', viewHelpers.stringify);
+env.addGlobal('qrCode', (content, size) => {
+  return new QRCode({
+    content,
+    width: size,
+    height: size
+  }).svg();
+});
 
 /**
  * Gets the relevant view template path given a message ref
@@ -38,7 +60,7 @@ const getRenderNotification = async (notificationId) => {
   const view = getViewPath(notification.message_ref);
 
   logger.info(JSON.stringify({ notification }, null, 2));
-  return env.render(view, { notification });
+  return render(view, { notification });
 };
 
 module.exports = {
