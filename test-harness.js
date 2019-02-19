73034de49d2ccc99e56b4d16129854657f153a59
Dana Gajewska <danusia.x@outlook.com>
2019-02-19 14:20:35 +0000

    Delete test-harness.js

--- test-harness.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const job = require('./src/modules/returns/lib/jobs/start-xml-upload');
-
-job.handler()
-  .then(res => console.log(res))
-  .catch(err => console.error(err));
