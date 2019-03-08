3f1e946de4cc2df842e60514c13202e05bac1670
Mark Swaffer <mdswaffer@gmail.com>
2019-03-08 09:29:47 +0000

    Feat: Adds a script to create/delete test returns (#338)
    * Feat: Adds a script to create/delete test returns
    
    Usage:
    
    `node scripts/returns create the-return-id`
    `node scripts/returns show`
    `node scripts/returns delete the-return-id`
    `node scripts/returns delete-all`

---
+++ scripts/returns/index.js
@@ -0,0 +1,104 @@
+const program = require('commander');
+const config = require('../../config');
+const { throwIfError } = require('@envage/hapi-pg-rest-api');
+const fs = require('fs');
+const path = require('path');
+
+const {
+  saveReturn,
+  deleteReturn,
+  loadReturn,
+  getAllTestReturns,
+  deleteAllReturns
+} = require('./data-access');
+
+const testReturn = require('./test-return');
+
+const printJson = obj => console.log(JSON.stringify(obj, null, 2));
+
+const printJsonAndExit = json => {
+  printJson(json);
+  process.exit();
+};
+
+const ERROR_CODES = {
+  NOT_IN_ENV_DIR: 2,
+  NOT_TEST_MODE: 3
+};
+
+const createTestReturn = async existingReturnId => {
+  const { data: existingReturn, error } = await loadReturn(existingReturnId);
+  throwIfError(error);
+
+  const ret = testReturn.create(existingReturn);
+  return saveReturn(ret);
+};
+
+const logErrorAndExit = (err, exitCode = 1) => {
+  console.error(err);
+  process.exit(1);
+};
+
+const validateCwd = () => {
+  if (!fs.existsSync(path.join(process.cwd(), '.env'))) {
+    const error = 'Must be run in the same working dir as the .env file';
+    logErrorAndExit({ error }, ERROR_CODES.NOT_IN_ENV_DIR);
+  }
+};
+
+const validateTestMode = () => {
+  if (!config.testMode) {
+    const error = 'Cannot use this tool outside of test mode';
+    console.error({ error }, ERROR_CODES.NOT_TEST_MODE);
+  };
+};
+
+validateCwd();
+validateTestMode();
+
+program
+  .command('create <return-id>')
+  .description('Creates a test return by copying the given return id')
+  .action(returnId => {
+    return createTestReturn(returnId)
+      .then(response => printJsonAndExit({ return: response.data }))
+      .catch(logErrorAndExit);
+  });
+
+program
+  .command('delete <return-id>')
+  .description('Deletes a test return')
+  .action(returnId => {
+    return deleteReturn(returnId)
+      .then(printJsonAndExit)
+      .catch(logErrorAndExit);
+  });
+
+program
+  .command('delete-all')
+  .description('Deletes any test returns that are saved')
+  .action(() => {
+    return deleteAllReturns()
+      .then(response => printJsonAndExit({ deleted: response }))
+      .catch(logErrorAndExit);
+  });
+
+program
+  .command('show')
+  .option('-v --verbose', ' Show the full return object')
+  .description('Shows any returns that have been made for testing')
+  .action(async cmd => {
+    const returns = await getAllTestReturns();
+
+    const output = cmd.verbose
+      ? { returns }
+      : { returns: returns.map(r => r.return_id) };
+
+    printJsonAndExit(output);
+  });
+
+program.parse(process.argv);
+
+if (program.args.length === 0) {
+  program.help();
+}
---
+++ scripts/returns/test-return.js
@@ -0,0 +1,50 @@
+const { cloneDeep } = require('lodash');
+const { getReturnId } = require('../../src/lib/returns');
+
+const startDate = '2018-11-01';
+const endDate = '2019-03-01';
+
+const createTestReturnId = existingReturn => {
+  return getReturnId(
+    existingReturn.metadata.nald.regionCode,
+    existingReturn.licence_ref,
+    existingReturn.return_requirement,
+    startDate,
+    endDate
+  );
+};
+
+/**
+ * Creates a new return object based on the existing return, but certain
+ * properties will be reset to create a due return in a known date range.
+ *
+ * Also adds the copiedForTesting flag to the metadata to help identify
+ * test returns that have been created.
+ *
+ * @param {Object} existingReturn The valid return from the service that is to be modified
+ * @returns {Object} The modified test return
+ */
+const create = existingReturn => {
+  const returnId = createTestReturnId(existingReturn, startDate, endDate);
+
+  const testReturn = cloneDeep(existingReturn);
+
+  testReturn.return_id = returnId;
+  testReturn.status = 'due';
+  testReturn.start_date = startDate;
+  testReturn.end_date = endDate;
+  testReturn.metadata.version = 1;
+  testReturn.metadata.isCurrent = true;
+  testReturn.metadata.copiedForTesting = true;
+  testReturn.received_date = null;
+  testReturn.due_date = '2019-04-01';
+  testReturn.under_query = false;
+
+  delete testReturn.created_at;
+  delete testReturn.updated_at;
+  delete testReturn.under_query_comment;
+
+  return testReturn;
+};
+
+exports.create = create;
