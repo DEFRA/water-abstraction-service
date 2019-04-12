883ee7807d9d7491e541a1797d1683a3649961af
James Carmichael <james@siteclick.co.uk>
2019-04-12 09:21:20 +0100

    Refactor cron job handlers, fix CodeClimate issues in status files

--- src/modules/batch-notifications/cron.js
+++ src/modules/batch-notifications/cron.js
@@ -6,35 +6,63 @@ const refreshEvent = require('./lib/jobs/refresh-event');
 const checkStatus = require('./lib/jobs/check-status');
 
 /**
- * Loads a batch of messages that are in 'sending' status and publishes
- * PG boss messages for each to send them
- * @return {Promise}
+ * Creates a batch handling function
+ * @param  {Function} batchLoader  - an async function to load a batch of data
+ * @param  {Function} mapper       - a function to map each data row to an ID
+ * @param  {Function} jobPublisher - function to publish PG boss job
+ * @param {String} message         - log message
+ * @return {Function}
  */
-const sendMessageBatch = async () => {
-  const messages = await queries.getSendingMessageBatch();
-  const tasks = messages.map(message => sendMessage.publish(message.id));
-  logger.info(`Sending batch notifications - ${messages.length} messages to send`);
-  return Promise.all(tasks);
+const createBatchProcessor = (batchLoader, mapper, jobPublisher, message) => {
+  return async () => {
+    const batch = await batchLoader();
+    const tasks = batch.map(item => jobPublisher(mapper(item)));
+    logger.info(`${message} - ${batch.length} item(s) found`);
+    return Promise.all(tasks);
+  };
 };
+
+const messageMapper = message => message.id;
+const eventMapper = ev => ev.event_id;
+
+/**
+* Loads a batch of messages that are in 'sending' status and publishes
+* PG boss jobs to send each one
+* @type {Function}
+* @return {Promise}
+*/
+const sendMessageBatch = createBatchProcessor(
+  queries.getSendingMessageBatch,
+  messageMapper,
+  sendMessage.publish,
+  'Processing notification message batch'
+);
 
 /**
  * Loads all notification events that are currently in 'sending' status and
- * updates the event with the latest stats on the batch
+ * publishes PG boss jobs to refresh each event with current status
+ * @type {Function}
  * @return {Promise}
  */
-const refreshEvents = async () => {
-  const events = await queries.getSendingEvents();
-  logger.info(`Refreshing batch notification events - ${events.length} events to check`);
-  const tasks = events.map(ev => refreshEvent.publish(ev.event_id));
-  return Promise.all(tasks);
-};
+const refreshEvents = createBatchProcessor(
+  queries.getSendingEvents,
+  eventMapper,
+  refreshEvent.publish,
+  'Refreshing notification events'
+);
 
-const checkNotifyStatuses = async () => {
-  const messages = await queries.getNotifyStatusChecks();
-  logger.info(`Checking notify statuses - ${messages.length} messages to check`);
-  const tasks = messages.map(message => checkStatus.publish(message.id));
-  return Promise.all(tasks);
-};
+/**
+ * Loads a batch of messages that are due to have their Notify status checked,
+ * and publishes PG boss jobs to check and update Notify status for each one
+ * @type {Function}
+ * @return {Promise}
+ */
+const checkNotifyStatuses = createBatchProcessor(
+  queries.getNotifyStatusChecks,
+  messageMapper,
+  checkStatus.publish,
+  'Checking notify statuses'
+);
 
 /**
  * Scheduled jobs for batch notification handling
