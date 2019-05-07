5ef0a20698a1d13749468ccc6bf233adfa59cbc1
James Carmichael <james@siteclick.co.uk>
2019-05-07 16:03:41 +0100

    Add CSV to JSON mapper

---
+++ src/modules/returns/lib/csv-to-json-mapping.js
@@ -0,0 +1,236 @@
+const { unzip } = require('lodash');
+const common = require('./common-mapping');
+const util = require('util');
+const parseCsv = util.promisify(require('csv-parse'));
+const moment = require('moment');
+const DATE_FORMAT = 'YYYY-MM-DD';
+const GDS_DATE_FORMAT = 'D MMMM YYYY';
+const GDS_MONTH_FORMAT = 'MMMM YYYY';
+
+/**
+ * Trims and lowercases value
+ * @param  {String} value
+ * @return {String}
+ */
+const normalize = value => value.trim().toLowerCase();
+
+/**
+ * Parses a date label found in the CSV into a frequency
+ * @param  {String} date - the date label from the CSV
+ * @return {String}      - return frequency day|week|month
+ */
+const parseDateFrequency = date => {
+  const d = normalize(date);
+  if (d.startsWith('week ending ')) {
+    return 'week';
+  };
+  if (d.match(/^[a-z]+ [0-9]{4}$/)) {
+    return 'month';
+  }
+  if (d.match(/^[0-9]{1,2} [a-z]+ [0-9]{4}$/)) {
+    return 'day';
+  }
+  throw new Error(`Could not parse return frequency from ${date}`);
+};
+
+/**
+ * Creates a day line skeleton for the return
+ * @param  {String} date - the date label in the CSV
+ * @return {Object}      - a return line skeleton
+ */
+const createDay = date => ({
+  startDate: moment(date, GDS_DATE_FORMAT).format(DATE_FORMAT),
+  endDate: moment(date, GDS_DATE_FORMAT).format(DATE_FORMAT),
+  timePeriod: 'day'
+});
+
+/**
+ * Creates a week line skeleton for the return
+ * @param  {String} date - the date label in the CSV
+ * @return {Object}      - a return line skeleton
+ */
+const createWeek = date => ({
+  startDate: moment(date, GDS_DATE_FORMAT).subtract(6, 'day').format(DATE_FORMAT),
+  endDate: moment(date, GDS_DATE_FORMAT).format(DATE_FORMAT),
+  timePeriod: 'week'
+});
+
+/**
+ * Creates a month line skeleton for the return
+ * @param  {String} date - the date label in the CSV
+ * @return {Object}      - a return line skeleton
+ */
+const createMonth = date => ({
+  startDate: moment(date, GDS_MONTH_FORMAT).startOf('month').format(DATE_FORMAT),
+  endDate: moment(date, GDS_MONTH_FORMAT).endOf('month').format(DATE_FORMAT),
+  timePeriod: 'month'
+});
+
+/**
+ * Creates a skeleton return line object
+ * @param  {String} dateLabel - the date label in the CSV
+ * @return {Object}           - return line object
+ */
+const createReturnLine = dateLabel => {
+  const frequency = parseDateFrequency(dateLabel);
+
+  const actions = {
+    day: createDay,
+    week: createWeek,
+    month: createMonth
+  };
+
+  const date = normalize(dateLabel).replace('week ending', '');
+
+  return actions[frequency](date);
+};
+
+/**
+ * Parses return ID into constituent parts
+ * @param  {String} returnId
+ * @return {Object}
+ */
+const parseReturnId = returnId => {
+  const parts = returnId.split(':');
+  const [ version, regionCode, licenceNumber, returnRequirement, startDate, endDate ] = parts;
+  return {
+    version: parseFloat(version.replace('v', '')),
+    regionCode,
+    licenceNumber,
+    returnRequirement,
+    startDate,
+    endDate
+  };
+};
+
+/**
+ * Creates return lines array by combining CSV header and column data
+ * @param  {Array} headers      - header column from CSV data
+ * @param  {Array} column       - return column from CSV data
+ * @param  {String} readingType - the reading type - estimated|measured
+ * @return {Array}              - return lines array
+ */
+const mapLines = (headers, column, readingType) => {
+  const lineHeaders = headers.slice(6, -1);
+  const lineCells = column.slice(6, -1);
+  return lineHeaders.reduce((acc, dateLabel, index) => {
+    const value = normalize(lineCells[index]);
+    if (value === 'do not edit') {
+      return acc;
+    }
+    return [...acc, {
+      unit: 'm³',
+      userUnit: 'm³',
+      ...createReturnLine(dateLabel),
+      quantity: value === '' ? null : parseFloat(value),
+      readingType
+    }];
+  }, []);
+};
+
+/**
+ * Maps reading object
+ * @param  {Array} column  - column of return data from CSV
+ * @return {Object}        - return reading object
+ */
+const mapReading = column => ({
+  type: normalize(column[3]) === 'y' ? 'measured' : 'estimated',
+  method: 'abstractionVolumes',
+  units: 'm³',
+  totalFlag: false
+});
+
+/**
+ * Maps CSV column data and reading type to a meters array for the return
+ * object
+ * @param  {Array} column  - column of return data from CSV
+ * @param  {String} readingType - the reading type - estimated|measured
+ * @return {Array}             array of meter objects (only 1 supported at present)
+ */
+const mapMeters = (column, readingType) => {
+  if (readingType === 'estimated') {
+    return [];
+  }
+  const manufacturer = column[4].trim();
+  const serialNumber = column[5].trim();
+  const meterDetailsProvided = manufacturer !== '' || serialNumber !== '';
+  return [{
+    meterDetailsProvided,
+    manufacturer,
+    serialNumber,
+    multiplier: 1
+  }];
+};
+
+/**
+ * Maps column and context data into a return object compatible with the
+ * water service schema
+ * @param  {Array} column  - column of return data from CSV
+ * @param  {Object} context - additional data to generate the return
+ * @param {Object} context.user - current user data
+ * @param {String} context.today - current date
+ * @param {Array} context.headers - the header column from the CSV data
+ * @return {Object}         a single return object
+ */
+const mapReturn = (column, context) => {
+  const isNil = normalize(column[2]) === 'y';
+  const returnId = column.slice(-1)[0];
+  const { startDate, endDate } = parseReturnId(returnId);
+
+  // Create return skeleton
+  const ret = {
+    returnId,
+    licenceNumber: column[0],
+    receivedDate: context.today,
+    startDate,
+    endDate,
+    isNil,
+    ...common.getReturnSkeleton(),
+    user: common.mapUser(context.user)
+  };
+
+  // Add lines/reading etc.
+  if (!isNil) {
+    ret.reading = mapReading(column);
+    ret.lines = mapLines(context.headers, column, ret.reading.type);
+    ret.meters = mapMeters(column, ret.reading.type);
+  }
+
+  // Return
+  return ret;
+};
+
+/**
+ * Maps a CSV file in string form to an array of return objects
+ * @param  {String}  csvStr - CSV file in string form
+ * @param  {Object}  user   - current user
+ * @param  {String}  [today]- current date YYYY-MM-DD
+ * @return {Promise<Array>} resolves with array of return objects
+ */
+const mapCsv = async (csvStr, user, today) => {
+  const data = await parseCsv(csvStr);
+
+  const [headers, ...returns] = unzip(data);
+
+  const context = {
+    user,
+    today: today || moment().format(DATE_FORMAT),
+    headers
+  };
+
+  return returns.map(ret => mapReturn(ret, context));
+};
+
+exports._normalize = normalize;
+exports._parseDateFrequency = parseDateFrequency;
+exports._createDay = createDay;
+exports._createWeek = createWeek;
+exports._createMonth = createMonth;
+exports._createReturnLine = createReturnLine;
+exports._parseReturnId = parseReturnId;
+exports._mapLines = mapLines;
+exports._mapReading = mapReading;
+exports._mapMeters = mapMeters;
+exports._mapReturn = mapReturn;
+
+exports.mapCsv = mapCsv;
---
+++ test/modules/returns/lib/csv-to-json-mapping.js
@@ -0,0 +1,340 @@
+const { expect } = require('code');
+const { find } = require('lodash');
+const { experiment, test, beforeEach } = exports.lab = require('lab').script();
+const csvMapper = require('../../../../src/modules/returns/lib/csv-to-json-mapping');
+
+const headers = [
+  'Licence number',
+  'Return reference',
+  'Nil return Y/N',
+  'Did you use a meter Y/N',
+  'Meter make',
+  'Meter serial number',
+  'Week ending 6 April 2019',
+  'Week ending 13 April 2019',
+  'Week ending 20 April 2019',
+  'Unique return reference'
+];
+
+const column = [
+  '01/123',
+  '01234',
+  'N',
+  'Y',
+  'Superpump',
+  '1234',
+  '45',
+  '',
+  'do not edit',
+  'v1:1:01/123:01234:2019-04-01:2020-03-31'
+];
+
+const nilReturn = [
+  '01/123',
+  '01234',
+  'Y',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  'v1:1:01/123:01234:2019-04-01:2020-03-31'
+];
+
+const csv = `Licence number,01/23
+Return reference,1234
+Nil return Y/N,N
+Did you use a meter Y/N,N
+Meter make,Superpump
+Meter serial number,789
+April 2018,Do not edit
+May 2018,Do not edit
+June 2018,Do not edit
+July 2018,Do not edit
+August 2018,Do not edit
+September 2018,Do not edit
+October 2018,10
+November 2018,25
+December 2018,4
+January 2019,6
+February 2019,
+March 2019,2
+Unique return reference,v1:8:01/123:1234:2018-10-30:2019-03-31`;
+
+const user = {
+  user_name: 'mail@example.com',
+  role: {
+    scopes: ['internal']
+  },
+  external_id: 'entity_id'
+};
+
+experiment('returns CSV to JSON mapper', () => {
+  experiment('normalize', () => {
+    test('should trim and lowercase values', async () => {
+      const result = csvMapper._normalize('  Some Data ');
+      expect(result).to.equal('some data');
+    });
+  });
+
+  experiment('parseDateFrequency', () => {
+    test('should detect a daily return line frequency', async () => {
+      const result = csvMapper._parseDateFrequency('31 January 2019');
+      expect(result).to.equal('day');
+    });
+
+    test('should detect a weekly return line frequency', async () => {
+      const result = csvMapper._parseDateFrequency('week ending 31 January 2019');
+      expect(result).to.equal('week');
+    });
+
+    test('should detect a monthly return line frequency', async () => {
+      const result = csvMapper._parseDateFrequency('January 2019');
+      expect(result).to.equal('month');
+    });
+
+    test('should throw an error if frequency not supported', async () => {
+      const func = () => csvMapper._parseDateFrequency('Millenium ending 31 December 1999');
+      expect(func).to.throw();
+    });
+  });
+
+  experiment('createDay', async () => {
+    test('should create a day return line skeleton', async () => {
+      const result = csvMapper._createDay('7 May 2019');
+      expect(result).to.equal({
+        startDate: '2019-05-07',
+        endDate: '2019-05-07',
+        timePeriod: 'day'
+      });
+    });
+  });
+
+  experiment('createWeek', async () => {
+    test('should create a weekly return line skeleton', async () => {
+      const result = csvMapper._createWeek('11 May 2019');
+      expect(result).to.equal({
+        startDate: '2019-05-05',
+        endDate: '2019-05-11',
+        timePeriod: 'week'
+      });
+    });
+  });
+
+  experiment('createMonth', async () => {
+    test('should create a month return line skeleton', async () => {
+      const result = csvMapper._createMonth('May 2019');
+      expect(result).to.equal({
+        startDate: '2019-05-01',
+        endDate: '2019-05-31',
+        timePeriod: 'month'
+      });
+    });
+  });
+
+  experiment('createReturnLine', async () => {
+    test('it should detect and return a daily line', async () => {
+      const result = csvMapper._createReturnLine('7 May 2019');
+      expect(result).to.equal({
+        startDate: '2019-05-07',
+        endDate: '2019-05-07',
+        timePeriod: 'day'
+      });
+    });
+
+    test('it should detect and return a weekly line', async () => {
+      const result = csvMapper._createReturnLine('Week ending 11 May 2019');
+      expect(result).to.equal({
+        startDate: '2019-05-05',
+        endDate: '2019-05-11',
+        timePeriod: 'week'
+      });
+    });
+
+    test('it should detect and return a monthly line', async () => {
+      const result = csvMapper._createReturnLine('May 2019');
+      expect(result).to.equal({
+        startDate: '2019-05-01',
+        endDate: '2019-05-31',
+        timePeriod: 'month'
+      });
+    });
+  });
+
+  experiment('parseReturnId', async () => {
+    test('it should parse a return ID into its component parts', async () => {
+      const result = csvMapper._parseReturnId('v1:1:01/123:1234:2018-04-01:2019-03-31');
+      expect(result).to.equal({
+        version: 1,
+        regionCode: '1',
+        licenceNumber: '01/123',
+        returnRequirement: '1234',
+        startDate: '2018-04-01',
+        endDate: '2019-03-31'
+      });
+    });
+  });
+
+  experiment('mapLines', async () => {
+    let result;
+
+    beforeEach(async () => {
+      result = csvMapper._mapLines(headers, column, 'measured');
+    });
+
+    test('maps numbers into a return line with numeric quantity', async () => {
+      const line = find(result, { endDate: '2019-04-06' });
+      expect(line).to.equal({
+        unit: 'm³',
+        userUnit: 'm³',
+        startDate: '2019-03-31',
+        endDate: '2019-04-06',
+        timePeriod: 'week',
+        quantity: 45,
+        readingType: 'measured'
+      });
+    });
+
+    test('maps empty cell to a return line with a null quantity', async () => {
+      const line = find(result, { endDate: '2019-04-13' });
+      expect(line).to.equal({
+        unit: 'm³',
+        userUnit: 'm³',
+        startDate: '2019-04-07',
+        endDate: '2019-04-13',
+        timePeriod: 'week',
+        quantity: null,
+        readingType: 'measured'
+      });
+    });
+
+    test('excludes cells containing "Do not edit" from the mapped lines', async () => {
+      const line = find(result, { endDate: '2019-04-20' });
+      expect(line).to.be.undefined();
+    });
+  });
+
+  experiment('mapReading', () => {
+    test('maps reading type to "measured" when cell marked as "Y"', async () => {
+      const column = ['', '', '', 'Y'];
+      const result = csvMapper._mapReading(column);
+      expect(result).to.equal({
+        type: 'measured',
+        method: 'abstractionVolumes',
+        units: 'm³',
+        totalFlag: false
+      });
+    });
+
+    test('maps reading type to "estimated" when cell marked as "N"', async () => {
+      const column = ['', '', '', 'N'];
+      const result = csvMapper._mapReading(column);
+      expect(result).to.equal({
+        type: 'estimated',
+        method: 'abstractionVolumes',
+        units: 'm³',
+        totalFlag: false
+      });
+    });
+  });
+
+  experiment('mapMeters', () => {
+    test('returns an empty array when reading type is "estimated"', async () => {
+      const column = [];
+      const result = csvMapper._mapMeters(column, 'estimated');
+      expect(result).to.equal([]);
+    });
+
+    test('returns meter details when reading type is "measured"', async () => {
+      const column = ['', '', '', '', 'Superpump', '1234'];
+      const result = csvMapper._mapMeters(column, 'measured');
+      expect(result).to.equal([{
+        meterDetailsProvided: true,
+        manufacturer: 'Superpump',
+        serialNumber: '1234',
+        multiplier: 1
+      }]);
+    });
+
+    test('returns meter details with meterDetailsProvided flag false when manufacturer/serial are blank"', async () => {
+      const column = ['', '', '', '', '', ''];
+      const result = csvMapper._mapMeters(column, 'measured');
+      expect(result).to.equal([{
+        meterDetailsProvided: false,
+        manufacturer: '',
+        serialNumber: '',
+        multiplier: 1
+      }]);
+    });
+  });
+
+  experiment('mapReturn', () => {
+    const context = {
+      headers,
+      user,
+      today: '2019-05-07'
+    };
+
+    test('maps common return data to return object', async () => {
+      const result = csvMapper._mapReturn(nilReturn, context);
+      expect(result.returnId).to.equal('v1:1:01/123:01234:2019-04-01:2020-03-31');
+      expect(result.licenceNumber).to.equal('01/123');
+      expect(result.receivedDate).to.equal('2019-05-07');
+      expect(result.startDate).to.equal('2019-04-01');
+      expect(result.endDate).to.equal('2020-03-31');
+      expect(result.isUnderQuery).to.equal(false);
+      expect(result.versionNumber).to.equal(1);
+      expect(result.user).to.equal({
+        email: 'mail@example.com',
+        type: 'internal',
+        entityId: 'entity_id'
+      });
+    });
+
+    test('maps a nil return object', async () => {
+      const result = csvMapper._mapReturn(nilReturn, context);
+
+      expect(result.isNil).to.equal(true);
+      expect(result.reading).to.be.undefined();
+      expect(result.lines).to.be.undefined();
+      expect(result.meters).to.be.undefined();
+    });
+
+    test('maps a non-nil return object', async () => {
+      const result = csvMapper._mapReturn(column, context);
+
+      expect(result.isNil).to.equal(false);
+      expect(result.reading).to.be.an.object();
+      expect(result.meters[0]).to.be.an.object();
+      expect(result.lines).to.be.an.array();
+    });
+  });
+
+  experiment('mapCsv', async () => {
+    test('maps a CSV string to an array of return objects', async () => {
+      const result = await csvMapper.mapCsv(csv, user, '2019-05-07');
+      expect(result).to.be.an.array();
+      expect(result.length).to.equal(1);
+
+      const [ ret ] = result;
+
+      const keys = Object.keys(ret);
+      expect(keys).to.include([
+        'returnId',
+        'licenceNumber',
+        'receivedDate',
+        'startDate',
+        'endDate',
+        'isNil',
+        'isUnderQuery',
+        'versionNumber',
+        'isCurrent',
+        'user',
+        'reading',
+        'lines',
+        'meters'
+      ]);
+    });
+  });
+});
