0f9cfe77a0bf565ab86dc2303691baf9cd65477a
James Carmichael <james@siteclick.co.uk>
2019-06-18 12:15:08 +0100

    Bugfix/water 2157 scheduled renewal email (#484)
    * Remove old version
    
    * Rewritten task for scheduled renewal emails
    
    * Add additional tests

--- src/controllers/tasks/lib/notify.js
+++ src/controllers/tasks/lib/notify.js
@@ -1,8 +1,11 @@
 const messageQueue = require('../../../lib/message-queue');
 const notify = require('../../../modules/notify');
 
+const _notify = notify(messageQueue);
+
 const sendMessage = config => {
-  return notify(messageQueue).enqueue(config);
+  return _notify.enqueue(config);
 };
 
 exports.sendMessage = sendMessage;
+exports._notify = _notify;
