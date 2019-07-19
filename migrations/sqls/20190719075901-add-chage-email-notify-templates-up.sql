e333c445406ac1fc3d220de82ed90f2fff9f487e
Dana Gajewska <danusia.x@outlook.com>
2019-07-19 11:26:48 +0100

    Feat: Change email functionality
    WATER-2206
    
    Migration to add new templates to water.notify_templates table. Create change-email module, controller and routes. Move notify connector from /modules to /lib & updated relevant file references.

---
+++ migrations/sqls/20190719075901-add-chage-email-notify-templates-up.sql
@@ -0,0 +1,6 @@
+/* Replace with your SQL commands */
+INSERT INTO "water"."notify_templates"("message_ref", "template_id", "notes", "notify_key")
+VALUES ('email_change_verification_code_email', '1fd2e8f2-8cb9-4ed1-8fa6-691918c15430', NULL, 'test');
+
+INSERT INTO "water"."notify_templates"("message_ref", "template_id", "notes", "notify_key")
+VALUES ('email_change_email_in_use_email', 'adb02416-e9d3-4e05-a9cc-b24e25675672', NULL, 'test');
