a094631d3cbef61b7bde3d6c53047e3cbf9af00e
James Carmichael <james@siteclick.co.uk>
2019-08-15 04:08:04 +0100

    Create tables for charge versions/elements

---
+++ migrations/sqls/20190814154806-charge-versions-down.sql
@@ -0,0 +1,10 @@
+/* Replace with your SQL commands */
+DROP TABLE IF EXISTS "water"."charge_elements";
+DROP TABLE IF EXISTS "water"."charge_versions";
+
+DROP TYPE IF EXISTS  water.charge_scheme;
+DROP TYPE IF EXISTS  water.charge_version_status;
+DROP TYPE IF EXISTS  water.charge_version_source;
+DROP TYPE IF EXISTS  water.charge_element_season;
+DROP TYPE IF EXISTS  water.charge_element_source;
+DROP TYPE IF EXISTS  water.charge_element_loss;
