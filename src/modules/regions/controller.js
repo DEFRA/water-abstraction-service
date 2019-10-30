e64760b98a2c15a6bf395539bb45d71cde1c8f7e
Mark Swaffer <mdswaffer@gmail.com>
2019-10-30 13:35:24 +0000

    Feat: Adds regions data to water service
    WATER-2380
    
    Creates the water.regions table and populates with the data from NALD.
    
    Adds a new resource at water/1.0/regions to get the regions from the
    database.

---
+++ src/modules/regions/controller.js
@@ -0,0 +1,17 @@
+const regionsConnector = require('../../lib/connectors/regions');
+const camelCaseKeys = require('../../lib/camel-case-keys');
+const { throwIfError } = require('@envage/hapi-pg-rest-api');
+
+/**
+ * Gets the regions from the database and transforms all keys to camel case
+ * for easier consumption upstream
+ */
+const getRegions = async () => {
+  const { data: regions, error } = await regionsConnector.getRegions();
+  throwIfError(error);
+  return {
+    data: camelCaseKeys(regions)
+  };
+};
+
+exports.getRegions = getRegions;
---
+++ src/modules/regions/routes.js
@@ -0,0 +1,12 @@
+const controller = require('./controller');
+
+const getRegions = {
+  method: 'GET',
+  path: '/water/1.0/regions',
+  handler: controller.getRegions,
+  config: {
+    description: 'Gets the regions that the system contains'
+  }
+};
+
+exports.getRegions = getRegions;
