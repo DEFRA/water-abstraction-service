141e0397ca6168d153055280f5b70e94e3bf559e
Mark Swaffer <mdswaffer@gmail.com>
2019-11-01 15:07:58 +0000

    Feat: Adds new tables for billing
    WATER-2381
    
    Adds billing tables and updates charge_* tables to use uuid data type

---
+++ migrations/sqls/20191101101124-billing-tables-down.sql
@@ -0,0 +1,9 @@
+drop table water.billing_transactions;
+drop table water.billing_invoice_licences;
+drop table water.billing_batch_invoices;
+drop table water.billing_invoices;
+drop table water.billing_batch_charge_versions;
+drop table water.billing_batches;
+
+drop type water.charge_type;
+drop type water.charge_batch_type;
