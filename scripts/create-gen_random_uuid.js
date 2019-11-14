2836a072bff582c775ccb7b12bf0e5415f6d9d31
James Carmichael <james@siteclick.co.uk>
2019-11-14 16:18:35 +0000

    Set gen_random_uuid() function to public

--- scripts/create-gen_random_uuid.js
+++
@@ -1,9 +0,0 @@
-const { pool } = require('../src/lib/connectors/db');
-
-async function run () {
-  const { error } = await pool.query('CREATE EXTENSION IF NOT EXISTS pgcrypto;');
-  console.log(error || 'Created postgres extension pgcrypto');
-  process.exit(error ? 1 : 0);
-}
-
-run();
