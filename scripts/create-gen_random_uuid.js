33ac86a501fe150d236a09749b6088a133aaa1ae
James Carmichael <james@siteclick.co.uk>
2019-11-14 15:09:53 +0000

    Ensure postgres pgcrypto extension installed on Travis

--- scripts/create-gen_random_uuid.js
+++ scripts/create-gen_random_uuid.js
@@ -2,7 +2,7 @@ const { pool } = require('../src/lib/connectors/db');
 
 async function run () {
   const { error } = await pool.query('CREATE EXTENSION IF NOT EXISTS pgcrypto;');
-  console.log(error || 'OK');
+  console.log(error || 'Created postgres extension pgcrypto');
   process.exit(error ? 1 : 0);
 }
 
