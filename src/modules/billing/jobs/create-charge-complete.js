d188b222185ea7876f4e9931f911a0317907af79
Mark Swaffer <mdswaffer@gmail.com>
2019-12-04 17:17:39 +0000

    Feat: Handle process charge version job completion (#669)
    WATER-2418
    
    Handles the completion of the process charge version job, so that if
    there are charge version years created, then jobs are queued to
    calculated the transactions (skeletom implementation). If there are no
    charge version years completed then the batch is considered complete.

---
+++ src/modules/billing/jobs/create-charge-complete.js
@@ -0,0 +1,26 @@
+const createChargeJob = require('./create-charge');
+const jobService = require('../services/jobService');
+
+const { logger } = require('../../../logger');
+
+const handleCreateChargeComplete = async job => {
+  logger.info(`onComplete - ${createChargeJob.jobName}`);
+
+  /**
+   * Placeholder
+   *
+   * Find billing_transactions for the batch that are
+   * not completed
+   *
+   * If there are transactions left to process do nothing
+   *
+   * If there are no more transactions to process then
+   * the batch is complete. Update the batch status and the
+   * event status
+   */
+  const { eventId } = job.data.request.data;
+  const { batch } = job.data.response;
+  await jobService.setCompletedJob(eventId, batch.billing_batch_id);
+};
+
+module.exports = handleCreateChargeComplete;
--- test/modules/billing/register-subscribers.js
+++ test/modules/billing/register-subscribers.js
@@ -9,11 +9,18 @@ const sinon = require('sinon');
 const sandbox = sinon.createSandbox();
 
 const registerSubscribers = require('../../../src/modules/billing/register-subscribers');
-const populateBatchChargeVersions = require('../../../src/modules/billing/jobs/populate-batch-charge-versions');
-const processChargeVersion = require('../../../src/modules/billing/jobs/process-charge-version');
+const jobs = require('../../../src/modules/billing/jobs');
 
 experiment('modules/billing/register-subscribers', () => {
   let server;
+
+  const getOnCompleteHandlerForJobName = jobName => {
+    const onCompleteCall = server.messageQueue.onComplete.getCalls().find(call => {
+      return call.args[0] === jobName;
+    });
+
+    return onCompleteCall.args[1];
+  };
 
   beforeEach(async () => {
     server = {
@@ -22,6 +29,11 @@ experiment('modules/billing/register-subscribers', () => {
         onComplete: sandbox.stub().resolves()
       }
     };
+
+    sandbox.stub(jobs.populateBatchChargeVersions, 'onCompleteHandler').resolves();
+    sandbox.stub(jobs.processChargeVersion, 'onCompleteHandler').resolves();
+    sandbox.stub(jobs.prepareTransactions, 'onCompleteHandler').resolves();
+    sandbox.stub(jobs.createCharge, 'onCompleteHandler').resolves();
   });
 
   afterEach(async () => {
@@ -41,22 +53,92 @@ experiment('modules/billing/register-subscribers', () => {
       await registerSubscribers.register(server);
     });
 
-    test('the populate billing batch charge versions job is registered', async () => {
-      expect(server.messageQueue.subscribe.calledWith(
-        populateBatchChargeVersions.jobName, populateBatchChargeVersions.handler
-      )).to.be.true();
+    experiment('for the populateBatchChargeVersions job', () => {
+      test('a subscribe handler is registered', async () => {
+        const { job } = jobs.populateBatchChargeVersions;
+        expect(server.messageQueue.subscribe.calledWith(job.jobName, job.handler)).to.be.true();
+      });
+
+      test('an onComplete handler is registered', async () => {
+        const { job } = jobs.populateBatchChargeVersions;
+        expect(server.messageQueue.onComplete.calledWith(job.jobName)).to.be.true();
+      });
+
+      test('on execution, the onComplete is passed a job and the message queue', async () => {
+        const { job, onCompleteHandler } = jobs.populateBatchChargeVersions;
+        const pgBossOnCompleteHandler = getOnCompleteHandlerForJobName(job.jobName);
+
+        const testJob = { id: 'test-job' };
+        await pgBossOnCompleteHandler(testJob);
+
+        expect(onCompleteHandler.calledWith(testJob, server.messageQueue)).to.be.true();
+      });
     });
 
-    test('an onComplete handler is registered for populateBatchChargeVersions', async () => {
-      expect(server.messageQueue.onComplete.calledWith(populateBatchChargeVersions.jobName)).to.be.true();
+    experiment('for the processChargeVersion job', () => {
+      test('a subscribe handler is registered for processChargeVersion', async () => {
+        const { job } = jobs.processChargeVersion;
+        expect(server.messageQueue.subscribe.calledWith(job.jobName, job.handler)).to.be.true();
+      });
+
+      test('an onComplete handler is registered for processChargeVersion', async () => {
+        const { job } = jobs.processChargeVersion;
+        expect(server.messageQueue.onComplete.calledWith(job.jobName)).to.be.true();
+      });
+
+      test('on execution, the onComplete is passed a job and the message queue', async () => {
+        const { job, onCompleteHandler } = jobs.processChargeVersion;
+        const pgBossOnCompleteHandler = getOnCompleteHandlerForJobName(job.jobName);
+
+        const testJob = { id: 'test-job' };
+        await pgBossOnCompleteHandler(testJob);
+
+        expect(onCompleteHandler.calledWith(testJob, server.messageQueue)).to.be.true();
+      });
     });
 
-    test('a subscribe handler is registered for processChargeVersion', async () => {
-      expect(server.messageQueue.subscribe.calledWith(processChargeVersion.jobName)).to.be.true();
+    experiment('for the prepareTransactions job', () => {
+      test('a subscribe handler is registered', async () => {
+        const { job } = jobs.prepareTransactions;
+        expect(server.messageQueue.subscribe.calledWith(job.jobName, job.handler)).to.be.true();
+      });
+
+      test('an onComplete handler is registered', async () => {
+        const { job } = jobs.prepareTransactions;
+        expect(server.messageQueue.onComplete.calledWith(job.jobName)).to.be.true();
+      });
+
+      test('on execution, the onComplete is passed a job and the message queue', async () => {
+        const { job, onCompleteHandler } = jobs.prepareTransactions;
+        const pgBossOnCompleteHandler = getOnCompleteHandlerForJobName(job.jobName);
+
+        const testJob = { id: 'test-job' };
+        await pgBossOnCompleteHandler(testJob);
+
+        expect(onCompleteHandler.calledWith(testJob, server.messageQueue)).to.be.true();
+      });
     });
 
-    test('an onComplete handler is registered for processChargeVersion', async () => {
-      expect(server.messageQueue.onComplete.calledWith(processChargeVersion.jobName)).to.be.true();
+    experiment('for the createCharge job', () => {
+      test('a subscribe handler is registered', async () => {
+        const { job } = jobs.createCharge;
+        expect(server.messageQueue.subscribe.calledWith(job.jobName, job.handler)).to.be.true();
+      });
+
+      test('an onComplete handler is registered', async () => {
+        const { job } = jobs.createCharge;
+        expect(server.messageQueue.onComplete.calledWith(job.jobName)).to.be.true();
+      });
+
+      test('on execution, the onComplete is passed a job and the message queue', async () => {
+        const { job, onCompleteHandler } = jobs.createCharge;
+        const pgBossOnCompleteHandler = getOnCompleteHandlerForJobName(job.jobName);
+
+        const testJob = { id: 'test-job' };
+        await pgBossOnCompleteHandler(testJob);
+
+        expect(onCompleteHandler.calledWith(testJob, server.messageQueue)).to.be.true();
+      });
     });
   });
 });
