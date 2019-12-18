a987abd14cda8ed539a44c77ee66d0b8dc5c2340
James Carmichael <james@siteclick.co.uk>
2019-12-18 15:49:14 +0000

    Remove water.lookup table, scheduled job and API (#699)

--- src/routes/water.js
+++ src/routes/water.js
@@ -4,7 +4,6 @@ API page, pending real back end - uses fs to read and write to lkocal json files
 */
 const sessionRoutes = require('../controllers/sessions');
 const schedulerRoutes = require('../controllers/scheduler');
-const lookupRoutes = require('../controllers/lookup');
 const notificationsRoutes = require('../controllers/notifications');
 const eventsRoutes = require('../controllers/events');
 const notifyTemplatesRoutes = require('../controllers/notifytemplates');
@@ -27,7 +26,6 @@ module.exports = [
   ...notificationsRoutes,
   ...notifyTemplatesRoutes,
   ...importedLicencesClient.getRoutes(),
-  ...lookupRoutes,
   ...taskConfigRoutes,
   ...moduleRoutes,
   ...gaugingStationRoutes,
