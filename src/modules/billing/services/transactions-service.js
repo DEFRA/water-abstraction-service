06b4888eb96c52f1e6001a49eb4556ed8ec93216
James Carmichael <james@siteclick.co.uk>
2020-01-16 15:46:41 +0000

    Bugfix with transaction mapping

--- src/modules/billing/services/transactions-service.js
+++ src/modules/billing/services/transactions-service.js
@@ -70,7 +70,7 @@ const createTransaction = (chargeLine, chargeElement, data = {}) => {
     chargePeriod: new DateRange(chargeLine.startDate, chargeLine.endDate),
     description: chargeElement.description,
     chargeElement: chargeElementsService.mapRowToModel(chargeElement),
-    volume: chargeElement.volume
+    volume: chargeElement.billableAnnualQuantity || chargeElement.authorisedAnnualQuantity
   });
   return transaction;
 };
