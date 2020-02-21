be11c4678ea08e4982e791307430c3ce1b03fe14
James Carmichael <james@siteclick.co.uk>
2020-02-21 09:19:15 +0000

    Tests for new Purpose model, additional tests for billingInvoices repo

--- test/lib/connectors/repos/billing-invoices.js
+++ test/lib/connectors/repos/billing-invoices.js
@@ -8,7 +8,7 @@ const { expect } = require('@hapi/code');
 const sinon = require('sinon');
 const sandbox = sinon.createSandbox();
 
-const { bookshelf } = require('../../../../src/lib/connectors/bookshelf');
+const { bookshelf, BillingInvoice } = require('../../../../src/lib/connectors/bookshelf');
 const queries = require('../../../../src/lib/connectors/repos/queries/billing-invoices');
 
 const billingInvoices = require('../../../../src/lib/connectors/repos/billing-invoices');
@@ -20,8 +20,24 @@ const result = {
 };
 
 experiment('lib/connectors/repos/billing-invoices', () => {
+  let model, stub;
+
   beforeEach(async () => {
     sandbox.stub(bookshelf.knex, 'raw').resolves(result);
+
+    model = {
+      toJSON: sandbox.stub().returns({ foo: 'bar' })
+    };
+
+    stub = {
+      fetch: sandbox.stub().resolves(model),
+      orderBy: sandbox.stub().returnsThis(),
+      fetchPage: sandbox.stub().resolves(model),
+      where: sandbox.stub().returnsThis(),
+      save: sandbox.stub().resolves(model)
+    };
+
+    sandbox.stub(BillingInvoice, 'forge').returns(stub);
   });
 
   afterEach(async () => {
@@ -63,6 +79,41 @@ experiment('lib/connectors/repos/billing-invoices', () => {
       expect(params).to.equal({
         batchId: 'test-batch-id'
       });
+    });
+  });
+
+  experiment('.findOne', () => {
+    let result;
+
+    beforeEach(async () => {
+      result = await billingInvoices.findOne('test-id');
+    });
+
+    test('calls model.forge with correct id', async () => {
+      const [params] = BillingInvoice.forge.lastCall.args;
+      expect(params).to.equal({ billingInvoiceId: 'test-id' });
+    });
+
+    test('calls fetch() with related models', async () => {
+      const [params] = stub.fetch.lastCall.args;
+      expect(params.withRelated).to.equal([
+        'billingBatch',
+        'billingBatch.region',
+        'billingInvoiceLicences',
+        'billingInvoiceLicences.licence',
+        'billingInvoiceLicences.licence.region',
+        'billingInvoiceLicences.billingTransactions',
+        'billingInvoiceLicences.billingTransactions.chargeElement',
+        'billingInvoiceLicences.billingTransactions.chargeElement.purposeUse'
+      ]);
+    });
+
+    test('calls toJSON() on returned models', async () => {
+      expect(model.toJSON.callCount).to.equal(1);
+    });
+
+    test('returns the result of the toJSON() call', async () => {
+      expect(result).to.equal({ foo: 'bar' });
     });
   });
 });
