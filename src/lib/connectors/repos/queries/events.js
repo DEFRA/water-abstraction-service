e7c9c3fbaf95bb84a29250d5e0a2d93f5d5dc004
Dana Gajewska <danusia.x@outlook.com>
2020-03-10 13:12:43 +0000

    Update Return Reminder template names in service (#828)
    * Moves test batch-notification config files to correct folder
    
    * Adds new reminder template names. Maps invitation template to relevant reminder template.
    
    WATER-2576
    
    * Adds extra check to SQL query. Fixes issues with updating events.
    
    Co-authored-by: James Carmichael <james@siteclick.co.uk>

--- src/lib/connectors/repos/queries/events.js
+++ src/lib/connectors/repos/queries/events.js
@@ -2,6 +2,7 @@ exports.getMostRecentReturnInvitation = `
 SELECT * FROM water.scheduled_notification
     WHERE event_id = (SELECT event_id FROM water.events 
       WHERE subtype = 'returnInvitation'
+      AND status = 'completed'
       ORDER BY created DESC LIMIT 1)
     AND licences \\? :licenceRef
     `;
