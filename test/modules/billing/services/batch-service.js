2ac985dc2d915260cf379b13f85362fc128487e6
James Carmichael <james@siteclick.co.uk>
2020-03-13 13:52:14 +0000

    Additional tests

--- test/modules/billing/services/batch-service.js
+++ test/modules/billing/services/batch-service.js
@@ -66,8 +66,10 @@ experiment('modules/billing/services/batch-service', () => {
     sandbox.stub(newRepos.billingBatches, 'update').resolves();
 
     sandbox.stub(newRepos.billingInvoices, 'deleteByBatchAndInvoiceAccountId').resolves();
+    sandbox.stub(newRepos.billingInvoices, 'deleteEmptyByBatchId').resolves();
 
     sandbox.stub(newRepos.billingInvoiceLicences, 'deleteByBatchAndInvoiceAccount').resolves();
+    sandbox.stub(newRepos.billingInvoiceLicences, 'deleteEmptyByBatchId').resolves();
 
     sandbox.stub(newRepos.billingTransactions, 'findStatusCountsByBatchId').resolves();
     sandbox.stub(newRepos.billingTransactions, 'findByBatchId').resolves();
@@ -808,6 +810,24 @@ experiment('modules/billing/services/batch-service', () => {
         expect(result.id).to.equal(batch.id);
         expect(result.status).to.equal(Batch.BATCH_STATUS.empty);
       });
+    });
+  });
+
+  experiment('.cleanup', async () => {
+    beforeEach(async () => {
+      await batchService.cleanup(BATCH_ID);
+    });
+
+    test('deletes licences in the batch that have no transactions', async () => {
+      expect(
+        newRepos.billingInvoiceLicences.deleteEmptyByBatchId.calledWith(BATCH_ID)
+      ).to.be.true();
+    });
+
+    test('deletes invoices in the batch that have no licences', async () => {
+      expect(
+        newRepos.billingInvoices.deleteEmptyByBatchId.calledWith(BATCH_ID)
+      ).to.be.true();
     });
   });
 });
--- test/modules/billing/services/charge-version-year.js
+++ test/modules/billing/services/charge-version-year.js
@@ -18,6 +18,7 @@ const TEST_ID = uuid();
 experiment('modules/billing/services/charge-version-year', () => {
   beforeEach(async () => {
     sandbox.stub(repos.billingBatchChargeVersionYears, 'update').resolves();
+    sandbox.stub(repos.billingBatchChargeVersionYears, 'findStatusCountsByBatchId');
   });
 
   afterEach(async () => {
@@ -48,6 +49,50 @@ experiment('modules/billing/services/charge-version-year', () => {
       expect(id).to.equal(TEST_ID);
       expect(data).to.equal({
         status: 'error'
+      });
+    });
+  });
+
+  experiment('.getStatusCounts', () => {
+    let result;
+    const batchId = 'test-batch-id';
+
+    experiment('when no rows found in batch', () => {
+      beforeEach(async () => {
+        repos.billingBatchChargeVersionYears.findStatusCountsByBatchId.resolves([]);
+        result = await chargeVersionYearService.getStatusCounts(batchId);
+      });
+
+      test('the repo .findStatusCountsByBatchId() method is called with the correct batch ID', async () => {
+        expect(
+          repos.billingBatchChargeVersionYears.findStatusCountsByBatchId.calledWith(batchId)
+        ).to.be.true();
+      });
+
+      test('zeros are returned for all statuses', async () => {
+        expect(result).to.equal({
+          ready: 0,
+          error: 0,
+          processing: 0
+        });
+      });
+    });
+
+    experiment('when rows found in batch', () => {
+      beforeEach(async () => {
+        repos.billingBatchChargeVersionYears.findStatusCountsByBatchId.resolves([
+          { status: 'ready', count: '3' },
+          { status: 'error', count: '9' }
+        ]);
+        result = await chargeVersionYearService.getStatusCounts(batchId);
+      });
+
+      test('counts are returned as integers', async () => {
+        expect(result).to.equal({
+          ready: 3,
+          error: 9,
+          processing: 0
+        });
       });
     });
   });
