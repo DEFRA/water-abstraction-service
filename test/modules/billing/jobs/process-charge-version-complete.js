02f088abd413f7aface927735c816ce300f0a7b9
Dana Gajewska <danusia.x@outlook.com>
2020-04-02 15:45:08 +0100

    Adds tests to process-charge-version onComplete handler

--- test/modules/billing/jobs/process-charge-version-complete.js
+++ test/modules/billing/jobs/process-charge-version-complete.js
@@ -12,8 +12,9 @@ const sandbox = sinon.createSandbox();
 const { logger } = require('../../../../src/logger');
 const processChargeVersionComplete = require('../../../../src/modules/billing/jobs/process-charge-version-complete');
 const batchJob = require('../../../../src/modules/billing/jobs/lib/batch-job');
-const { BATCH_ERROR_CODE } = require('../../../../src/lib/models/batch');
+const { BATCH_ERROR_CODE, BATCH_STATUS } = require('../../../../src/lib/models/batch');
 const chargeVersionYearService = require('../../../../src/modules/billing/services/charge-version-year');
+const batchService = require('../../../../src/modules/billing/services/batch-service');
 
 const batchId = 'test-batch-id';
 
@@ -86,6 +87,53 @@ experiment('modules/billing/jobs/process-charge-version-complete', () => {
 
     test('remaining onComplete jobs are not deleted', async () => {
       expect(batchJob.deleteOnCompleteQueue.called).to.be.false();
+    });
+  });
+
+  experiment('for a two part tariff batch, when all charge version years have been processed', async () => {
+    let job;
+    beforeEach(async () => {
+      sandbox.stub(batchService, 'setStatus').resolves({});
+      chargeVersionYearService.getStatusCounts.resolves({
+        processing: 0
+      });
+
+      job = {
+        data: {
+          request: {
+            data: {
+              eventId: 'test-event-id'
+            }
+          },
+          response: {
+            chargeVersionYear: {
+              billing_batch_id: batchId
+            },
+            batch: {
+              billing_batch_id: batchId,
+              batch_type: 'two_part_tariff'
+            }
+          }
+        }
+      };
+
+      await processChargeVersionComplete(job, messageQueue);
+    });
+
+    test('remaining onComplete jobs are deleted', async () => {
+      expect(batchJob.deleteOnCompleteQueue.calledWith(
+        job, messageQueue
+      )).to.be.true();
+    });
+
+    test('sets the batch status to "review', async () => {
+      expect(batchService.setStatus.calledWith(
+        batchId, BATCH_STATUS.review
+      )).to.be.true();
+    });
+
+    test('the prepare-transactions job is not published', async () => {
+      expect(messageQueue.publish.called).to.be.false();
     });
   });
 
