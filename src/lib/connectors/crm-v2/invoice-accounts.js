52d80cf71c1496a8a13775ba4857d6b53a0dbe17
James Carmichael <james@siteclick.co.uk>
2020-04-30 16:40:24 +0100

    Fixes issue where CRM v2 invoice accounts connector generated query strings that were too long

--- src/lib/connectors/crm-v2/invoice-accounts.js
+++ src/lib/connectors/crm-v2/invoice-accounts.js
@@ -1,21 +1,29 @@
 'use strict';
 
+const { chunk, flatMap } = require('lodash');
 const urlJoin = require('url-join');
 const { serviceRequest } = require('@envage/water-abstraction-helpers');
 const config = require('../../../../config');
 
 const getUri = (...tail) => urlJoin(config.services.crm_v2, 'invoice-accounts', ...tail);
 
+const getBatch = ids =>
+  serviceRequest.get(getUri(), {
+    qs: { id: ids },
+    qsStringifyOptions: { arrayFormat: 'repeat' }
+  });
+
 /**
  * Gets the invoice accounts including company data
  * for the given for the given invoice account ids
  * @param {Array<String>} invoiceAccountids The array of invoice account ids to fetch
  */
-const getInvoiceAccountsByIds = ids => {
-  return serviceRequest.get(getUri(), {
-    qs: { id: ids },
-    qsStringifyOptions: { arrayFormat: 'repeat' }
-  });
+const getInvoiceAccountsByIds = async ids => {
+  // Split array into chunks to avoid exceeding max query limit of 1024
+  const idBatches = chunk(ids, 24);
+  // Get batches
+  const results = await Promise.all(idBatches.map(getBatch));
+  return flatMap(results);
 };
 
 /**
