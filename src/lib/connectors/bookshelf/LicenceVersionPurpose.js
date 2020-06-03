cc2d2d6419dd238c5cbb3279ff4569e993ded1cd
Mark Swaffer <mdswaffer@gmail.com>
2020-06-03 15:47:13 +0100

    Feat: New licence version tables
    WATER-2681
    
    Part 2 of 2
    
    Adds the new database tables for the licence versions data and provides
    bookshelf models that map the relationships
    
    Updates some dependencies based on dependabot recommendations

---
+++ src/lib/connectors/bookshelf/LicenceVersionPurpose.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { bookshelf } = require('./bookshelf.js');
+
+module.exports = bookshelf.model('LicenceVersionPurpose', {
+  tableName: 'licence_version_purposes',
+
+  idAttribute: 'licence_version_purpose_id',
+
+  hasTimestamps: ['date_created', 'date_updated'],
+
+  licenceVersion () {
+    return this.belongsTo('LicenceVersion', 'licence_version_id', 'licence_version_id');
+  },
+
+  purposePrimary () {
+    return this.hasOne('PurposePrimary', 'purpose_primary_id', 'purpose_primary_id');
+  },
+
+  purposeSecondary () {
+    return this.hasOne('PurposeSecondary', 'purpose_secondary_id', 'purpose_secondary_id');
+  },
+
+  purposeUse () {
+    return this.hasOne('PurposeUse', 'purpose_use_id', 'purpose_use_id');
+  }
+});
