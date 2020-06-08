b5ef2ce79ef5c8084c63ca8456d2df792843cf6b
Mark Swaffer <mdswaffer@gmail.com>
2020-06-08 11:56:40 +0100

    Feat: Import licence versions
    WATER-2682
    
    Adds unique constraints on the licence_versions and
    licence_version_purposes tables to allow usperts in postgres in the
    water_import project when importing a licence

---
+++ migrations/sqls/20200605100709-licence-versions-unique-constraint-down.sql
@@ -0,0 +1,5 @@
+alter table water.licence_versions
+  drop constraint uindx_licence_versions_external_id;
+
+alter table water.licence_version_purposes
+  drop constraint uindx_licence_version_purposes;
