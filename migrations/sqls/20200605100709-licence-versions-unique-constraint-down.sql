0d824a6684bdc354a472ddddb06fb44fb048258a
Mark Swaffer <mdswaffer@gmail.com>
2020-06-10 11:33:03 +0100

    Uses external id for licence purposes unique constraint

--- migrations/sqls/20200605100709-licence-versions-unique-constraint-down.sql
+++ migrations/sqls/20200605100709-licence-versions-unique-constraint-down.sql
@@ -2,4 +2,4 @@ alter table water.licence_versions
   drop constraint uindx_licence_versions_external_id;
 
 alter table water.licence_version_purposes
-  drop constraint uindx_licence_version_purposes;
+  drop constraint uindx_licence_version_purposes_external_id;
--- migrations/sqls/20200605100709-licence-versions-unique-constraint-up.sql
+++ migrations/sqls/20200605100709-licence-versions-unique-constraint-up.sql
@@ -1,14 +1,10 @@
 alter table water.licence_versions
   add constraint uindx_licence_versions_external_id unique (external_id);
 
+truncate table water.licence_version_purposes;
+
 alter table water.licence_version_purposes
-  add constraint uindx_licence_version_purposes unique (
-    licence_version_id,
-    purpose_primary_id,
-    purpose_secondary_id,
-    purpose_use_id,
-    abstraction_period_start_day,
-    abstraction_period_start_month,
-    abstraction_period_end_day,
-    abstraction_period_end_month
-  );
+  add column "external_id" varchar not null;
+
+alter table water.licence_version_purposes
+  add constraint uindx_licence_version_purposes_external_id unique (external_id);
