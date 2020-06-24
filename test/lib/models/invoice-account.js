5417d5e8915e63ebe610b09d3bf84c50f3cb236f
James Carmichael <james@siteclick.co.uk>
2020-06-24 08:59:13 +0100

    Fix linter error

--- test/lib/models/invoice-account.js
+++ test/lib/models/invoice-account.js
@@ -6,7 +6,6 @@ const { expect } = require('@hapi/code');
 const InvoiceAccount = require('../../../src/lib/models/invoice-account');
 const Company = require('../../../src/lib/models/company');
 const Address = require('../../../src/lib/models/address');
-const InvoiceAccountAddresses = require('../../../src/lib/models/invoice-account-address');
 const InvoiceAccountAddress = require('../../../src/lib/models/invoice-account-address');
 
 const TEST_GUID = 'add1cf3b-7296-4817-b013-fea75a928580';
