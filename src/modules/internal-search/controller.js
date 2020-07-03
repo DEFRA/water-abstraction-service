8adc47974f1644d0a4b0570ee11d6f5b74bc8395
Mark Swaffer <mdswaffer@gmail.com>
2020-07-03 10:42:47 +0100

    Feat: Handle internal search error
    WATER-2724
    
    Catches and logs the internal search error to aid debugging of errors in
    the future

--- src/modules/internal-search/controller.js
+++ src/modules/internal-search/controller.js
@@ -1,7 +1,36 @@
+'use strict';
+
 const { parseQuery } = require('./lib/query-parser');
 const search = require('./lib/index');
 const { buildResponse } = require('./lib/build-response');
+const { logger } = require('../../logger');
 
+const buildInternalSearchResponse = async (request) => {
+  const { query, page } = request.query;
+  const { isUser, isNumeric, isReturnId } = parseQuery(query);
+
+  const response = {};
+
+  if (isReturnId) {
+    const data = await search.searchReturns(query);
+    buildResponse(response, 'returns', data);
+  } else if (isUser) {
+  // User search
+    const data = await search.searchUsers(query, page);
+    buildResponse(response, 'users', data);
+  } else {
+  // Search returns
+    if (isNumeric) {
+      const data = await search.searchReturns(query);
+      buildResponse(response, 'returns', data);
+    }
+    // Search CRM documents
+    const data = await search.searchDocuments(query, page);
+    buildResponse(response, 'documents', data);
+  }
+
+  return response;
+};
 /**
  * Provides an API for internal search.
  * Internal searches for:
@@ -10,29 +39,12 @@ const { buildResponse } = require('./lib/build-response');
  * - Users
  */
 const getInternalSearch = async (request) => {
-  const { query, page } = request.query;
-
-  const { isUser, isNumeric, isReturnId } = parseQuery(query);
-
-  const response = {};
-
-  // Single return
-  if (isReturnId) {
-    const data = await search.searchReturns(query);
-    buildResponse(response, 'returns', data);
-  } else if (isUser) {
-    // User search
-    const data = await search.searchUsers(query, page);
-    buildResponse(response, 'users', data);
-  } else {
-    // Search returns
-    if (isNumeric) {
-      const data = await search.searchReturns(query);
-      buildResponse(response, 'returns', data);
-    }
-    // Search CRM documents
-    const data = await search.searchDocuments(query, page);
-    buildResponse(response, 'documents', data);
+  let response;
+  try {
+    // Single return
+    response = buildInternalSearchResponse(request, response);
+  } catch (err) {
+    logger.error('Internal search error', err);
   }
 
   return response;
