9eb7e34ade010c022e9c87e4d2e4090f526966ba
James Carmichael <james@siteclick.co.uk>
2020-07-03 11:37:20 +0100

    Don't use TLS options on Travis

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ const testMode = parseInt(process.env.TEST_MODE) === 1;
 const isAcceptanceTestTarget = ['local', 'dev', 'development', 'test', 'preprod'].includes(process.env.NODE_ENV);
 const isProduction = ['production'].includes(process.env.NODE_ENV);
 const isLocal = process.env.NODE_ENV === 'local';
+const isTravis = process.env.TRAVIS;
 
 const crmUri = process.env.CRM_URI || 'http://127.0.0.1:8002/crm/1.0';
 
@@ -206,7 +207,7 @@ module.exports = {
     host: process.env.REDIS_HOST || '127.0.0.1',
     port: process.env.REDIS_PORT || 6379,
     password: process.env.REDIS_PASSWORD || '',
-    ...!isLocal && { tls: {} },
+    ...!(isLocal || isTravis) && { tls: {} },
     db: 2
   }
 };
