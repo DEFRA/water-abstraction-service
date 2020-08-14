c1dced69c4e6489b40544cb5c74f7c41bcbf5d37
James Carmichael <james@siteclick.co.uk>
2020-08-14 14:03:36 +0100

    Add bookshelf models, service models, mappers and service to get return requirement versions for licence

---
+++ src/lib/services/return-requirement-versions.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const repos = require('../connectors/repos');
+const returnVersionMapper = require('../mappers/return-requirement-version');
+
+/**
+ * Gets a list of all return requirement versions for the supplied licence ID
+ * @param {String} licenceId
+ * @return {Array<ReturnRequirementVersion>}
+ */
+const getByLicenceId = async licenceId => {
+  const data = await repos.returnVersions.findByLicenceId(licenceId);
+  console.log(data);
+  return data.map(returnVersionMapper.dbToModel);
+};
+
+exports.getByLicenceId = getByLicenceId;
