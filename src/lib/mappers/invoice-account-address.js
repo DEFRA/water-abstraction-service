e4f9c7d30f42d3c46dca6b80e51084268144a16e
Stephan Hough <43169948+Stephan-Hough@users.noreply.github.com>
2020-08-21 16:43:40 +0100

    bugfix/2795 -- invoice account address mapper updated -- invoice service tests fixed

--- src/lib/mappers/invoice-account-address.js
+++ src/lib/mappers/invoice-account-address.js
@@ -4,6 +4,9 @@ const InvoiceAccountAddress = require('../models/invoice-account-address');
 
 const { address, company, contact } = require('../mappers');
 const DateRange = require('../models/date-range');
+const Company = require('../models/company');
+const Address = require('../models/address');
+const Contact = require('../models/contact-v2');
 
 /**
  * Maps CRM invoice account and (optionally) company data to a water service model
@@ -13,14 +16,28 @@ const DateRange = require('../models/date-range');
  */
 const crmToModel = invoiceAccountAddress => {
   const model = new InvoiceAccountAddress(invoiceAccountAddress.invoiceAccountAddressId);
-  model.fromHash({
-    dateRange: new DateRange(invoiceAccountAddress.startDate, invoiceAccountAddress.endDate),
-    invoiceAccountId: invoiceAccountAddress.invoiceAccountId,
-    address: address.crmToModel(invoiceAccountAddress.address),
-    agentCompany: company.crmToModel(invoiceAccountAddress.agentCompany),
-    contact: contact.crmToModel(invoiceAccountAddress.contact)
-  });
 
+  model.dateRange = new DateRange(invoiceAccountAddress.startDate, invoiceAccountAddress.endDate);
+  model.invoiceAccountId = invoiceAccountAddress.invoiceAccountId;
+
+  if (invoiceAccountAddress.address) {
+    model.address = address.crmToModel(invoiceAccountAddress.address);
+  } else {
+    model.address = new Address(invoiceAccountAddress.addressId);
+  }
+  // add the Agent company if there is one otherwise add an empty company model
+  if (invoiceAccountAddress.agentCompany) {
+    model.agentCompany = company.crmToModel(invoiceAccountAddress.agentCompany);
+  } else if (invoiceAccountAddress.agentCompanyId) {
+    model.agentCompany = new Company(invoiceAccountAddress.agentCompanyId);
+  } else { model.agentCompany = new Company(); }
+
+  // add the contact if there is one otherwise add an empty contact model
+  if (invoiceAccountAddress.contact) {
+    model.contact = contact.crmToModel(invoiceAccountAddress.contact);
+  } else if (invoiceAccountAddress.contactId) {
+    model.contact = new Contact(invoiceAccountAddress.contactId);
+  } else { model.contact = new Contact(); }
   return model;
 };
 
