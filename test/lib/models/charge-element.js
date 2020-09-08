4bca2e3dc7b3b0f404b2c005d74c304056ac45ca
James Carmichael <james@siteclick.co.uk>
2020-09-08 09:07:55 +0100

    Additional tests for mappers/models

--- test/lib/models/charge-element.js
+++ test/lib/models/charge-element.js
@@ -7,6 +7,7 @@ const ChargeElement = require('../../../src/lib/models/charge-element');
 const { CHARGE_SEASON } = require('../../../src/lib/models/constants');
 const AbstractionPeriod = require('../../../src/lib/models/abstraction-period');
 const PurposeUse = require('../../../src/lib/models/purpose-use');
+const Purpose = require('../../../src/lib/models/purpose');
 const DateRange = require('../../../src/lib/models/date-range');
 
 const TEST_GUID = 'add1cf3b-7296-4817-b013-fea75a928580';
@@ -241,6 +242,34 @@ experiment('lib/models/charge-element', () => {
 
     test('throws an error if set to a number', async () => {
       const func = () => { chargeElement.description = 123; };
+      expect(func).to.throw();
+    });
+  });
+
+  experiment('.purposePrimary', () => {
+    test('can be set to a Purpose instance', async () => {
+      const purpose = new Purpose();
+      chargeElement.purposePrimary = purpose;
+      expect(chargeElement.purposePrimary).to.equal(purpose);
+    });
+
+    test('throws an error if setting to an instance of another model', async () => {
+      const period = new AbstractionPeriod();
+      const func = () => { chargeElement.purposePrimary = period; };
+      expect(func).to.throw();
+    });
+  });
+
+  experiment('.purposeSecondary', () => {
+    test('can be set to a Purpose instance', async () => {
+      const purpose = new Purpose();
+      chargeElement.purposeSecondary = purpose;
+      expect(chargeElement.purposeSecondary).to.equal(purpose);
+    });
+
+    test('throws an error if setting to an instance of another model', async () => {
+      const period = new AbstractionPeriod();
+      const func = () => { chargeElement.purposeSecondary = period; };
       expect(func).to.throw();
     });
   });
