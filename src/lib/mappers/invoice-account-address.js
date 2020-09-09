808d2d15ba33d6296dc9b1aa3045714fec9b3c4c
Dana Gajewska <danusia.x@outlook.com>
2020-09-09 15:39:16 +0100

    Refactors invoiceAccountAddress mapper

--- src/lib/mappers/invoice-account-address.js
+++ src/lib/mappers/invoice-account-address.js
@@ -2,11 +2,35 @@
 
 const InvoiceAccountAddress = require('../models/invoice-account-address');
 
-const { address, company, contact } = require('../mappers');
+const { address, company, contact, invoiceAccountAddress } = require('../mappers');
 const DateRange = require('../models/date-range');
 const Address = require('../models/address');
 const Company = require('../models/company');
 const Contact = require('../models/contact-v2');
+
+const setModelAddress = (model, invoiceAccountAddress) => {
+  if (invoiceAccountAddress.address) {
+    model.address = address.crmToModel(invoiceAccountAddress.address);
+  } else if (invoiceAccountAddress.addressId) {
+    model.address = new Address(invoiceAccountAddress.addressId);
+  }
+};
+
+const setModelContact = (model, invoiceAccountAddress) => {
+  if (invoiceAccountAddress.contact) {
+    model.contact = contact.crmToModel(invoiceAccountAddress.contact);
+  } else if (invoiceAccountAddress.contactId) {
+    model.contact = new Contact(invoiceAccountAddress.contactId);
+  }
+};
+
+const setModelAgentCompany = (model, invoiceAccountAddress) => {
+  if (invoiceAccountAddress.agentCompany) {
+    model.agentCompany = company.crmToModel(invoiceAccountAddress.agentCompany);
+  } else if (invoiceAccountAddress.agentCompanyId) {
+    model.agentCompany = new Company(invoiceAccountAddress.agentCompanyId);
+  }
+};
 
 /**
  * Maps CRM invoice account and (optionally) company data to a water service model
@@ -19,23 +43,9 @@ const crmToModel = invoiceAccountAddress => {
   model.dateRange = new DateRange(invoiceAccountAddress.startDate, invoiceAccountAddress.endDate);
   model.invoiceAccountId = invoiceAccountAddress.invoiceAccountId;
 
-  if (invoiceAccountAddress.address) {
-    model.address = address.crmToModel(invoiceAccountAddress.address);
-  } else if (invoiceAccountAddress.addressId) {
-    model.address = new Address(invoiceAccountAddress.addressId);
-  }
-
-  if (invoiceAccountAddress.contact) {
-    model.contact = contact.crmToModel(invoiceAccountAddress.contact);
-  } else if (invoiceAccountAddress.contactId) {
-    model.contact = new Contact(invoiceAccountAddress.contactId);
-  }
-
-  if (invoiceAccountAddress.agentCompany) {
-    model.agentCompany = company.crmToModel(invoiceAccountAddress.agentCompany);
-  } else if (invoiceAccountAddress.agentCompanyId) {
-    model.agentCompany = new Company(invoiceAccountAddress.agentCompanyId);
-  }
+  setModelAddress(model, invoiceAccountAddress);
+  setModelContact(model, invoiceAccountAddress);
+  setModelAgentCompany(model, invoiceAccountAddress);
 
   return model;
 };
