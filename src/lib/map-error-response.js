faa1e8cf9090aae4fc763728d32b37f32bb97ba8
James Carmichael <james@siteclick.co.uk>
2020-09-22 12:06:21 +0100

    Fix CodeClimate issue with mapErrorResponse function

--- src/lib/map-error-response.js
+++ src/lib/map-error-response.js
@@ -1,26 +1,32 @@
+'use strict';
+
 const Boom = require('@hapi/boom');
-const { NotFoundError, InvalidEntityError, ConflictingDataError } = require('./errors');
-const { BatchStatusError, TransactionStatusError } = require('../modules/billing/lib/errors');
 
-// caters for error triggered in this service and 404s returned from the CRM
-const isNotFoundError = err => err instanceof NotFoundError || err.statusCode === 404;
+const commandMap = new Map();
+commandMap.set('NotFoundError', Boom.notFound);
+commandMap.set('BatchStatusError', Boom.forbidden);
+commandMap.set('TransactionStatusError', Boom.forbidden);
+commandMap.set('InvalidEntityError', Boom.badData);
+commandMap.set('ConflictingDataError', Boom.conflict);
 
-module.exports = error => {
-  if (isNotFoundError(error)) {
+/**
+ * Maps a service error to a Boom error for providing an HTTP response
+ * If the error is unknown, it is rethrown
+ * @param {Error}
+ * @return {Error} Boom error
+ */
+const mapErrorResponse = error => {
+  const boomError = commandMap.get(error.name);
+
+  if (boomError) {
+    return boomError(error.message);
+  }
+
+  if (error.statusCode === 404) {
     return Boom.notFound(error.message);
   }
-  if (error instanceof BatchStatusError) {
-    return Boom.forbidden(error.message);
-  }
-  if (error instanceof TransactionStatusError) {
-    return Boom.forbidden(error.message);
-  }
-  if (error instanceof InvalidEntityError) {
-    return Boom.badData(error.message);
-  }
-  if (error instanceof ConflictingDataError) {
-    return Boom.conflict(error.message);
-  }
-  // Unexpected error
+
   throw error;
 };
+
+module.exports = mapErrorResponse;
