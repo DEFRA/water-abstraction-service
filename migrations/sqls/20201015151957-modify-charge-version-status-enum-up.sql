bfb35e818d83b8a7ed8af0938e7674f701de92c9
Tim <timtamimi@Tims-MacBook-Pro.local>
2020-10-15 17:28:17 +0100

    Migration script change

--- migrations/sqls/20201015151957-modify-charge-version-status-enum-up.sql
+++ migrations/sqls/20201015151957-modify-charge-version-status-enum-up.sql
@@ -1,11 +1,12 @@
-alter type water.charge_version_workflow_status add value 'review';
+alter type water.charge_version_workflow_status
+  rename to _charge_version_workflow_status;
 
-update water.charge_version_workflows set status = 'review' where status = 'draft';
+create type water.charge_version_workflow_status as enum (
+  'review',
+  'changes_requested'
+);
 
-alter type water.charge_version_workflow_status rename to charge_version_workflow_status_old;
+alter table water.charge_version_workflow
+  alter column status type water.charge_version_workflow_status using status::text::water.charge_version_workflow_status;
 
-create type water.charge_version_workflow_status as enum ('review', 'changes_requested');
-
-alter table water.charge_version_workflows alter column status type charge_version_workflow_status using status::text::charge_version_workflow_status;
-
-drop type water.charge_version_workflow_status_old;
+drop type water._charge_version_workflow_status_old;
