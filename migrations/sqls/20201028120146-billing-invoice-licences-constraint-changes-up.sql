487f5dd10461f3f297b18d355803b7c322f11e8b
Dana Gajewska <danusia.x@outlook.com>
2020-11-05 15:33:59 +0000

    Fixes constraint issue in migration

--- migrations/sqls/20201028120146-billing-invoice-licences-constraint-changes-up.sql
+++ migrations/sqls/20201028120146-billing-invoice-licences-constraint-changes-up.sql
@@ -1,6 +1,7 @@
-ALTER TABLE water.billing_invoice_licences ADD CONSTRAINT billing_invoice_licences_unique UNIQUE (billing_invoice_id,licence_id);
+ALTER TABLE water.billing_invoice_licences DROP CONSTRAINT IF EXISTS unique_batch_invoice_licence;
 ALTER TABLE water.billing_invoice_licences DROP COLUMN company_id;
 ALTER TABLE water.billing_invoice_licences DROP COLUMN contact_id;
 ALTER TABLE water.billing_invoice_licences DROP COLUMN address_id;
 ALTER TABLE water.billing_invoice_licences DROP COLUMN licence_holder_name;
 ALTER TABLE water.billing_invoice_licences DROP COLUMN licence_holder_address;
+ALTER TABLE water.billing_invoice_licences ADD CONSTRAINT billing_invoice_licences_unique UNIQUE (billing_invoice_id,licence_id);
