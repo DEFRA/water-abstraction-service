876d1c091ee39414d144db79be1af85026ea667e
James Carmichael <james@siteclick.co.uk>
2020-11-06 17:37:42 +0000

    Round billing volumes to 6dp not 3dp (#1216)
    * Round billing volumes to 6dp not 3dp
    
    * Initial work to use Decimal type for TPT matching calculations
    
    * Fix tests
    
    * FIx typo in comment
    
    * Fix code quality issue
    
    * Allow BillingVolume.calculatedVolume to accept any value valid for a Decimal constructor
    
    * Additional tests, remove redundant code
    
    * Improve comment

---
+++ src/lib/decimal-helpers.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const is = require('@sindresorhus/is');
+const Decimal = require('decimal.js-light');
+
+/**
+ * Analogous to Math.min, returns the smallest Decimal value
+ * @param  {...Decimal} decimals
+ * @return {Decimal} the smallest provided decimal
+ */
+const min = (...decimals) => decimals.reduce(
+  (acc, decimal) => decimal.lessThan(acc) ? decimal : acc
+);
+
+/**
+ * Analogous to Math.max, returns the greatest Decimal value
+ * @param  {...Decimal} decimals
+ * @return {Decimal} the largest provided decimal
+ */
+const max = (...decimals) => decimals.reduce(
+  (acc, decimal) => decimal.greaterThan(acc) ? decimal : acc
+);
+
+/**
+ * Checks whether the supplied value is a Decimal
+ * @param {*} value
+ * @return {Boolean}
+ */
+const isDecimal = value => is.object(value) && is.directInstanceOf(value, Decimal);
+
+exports.min = min;
+exports.max = max;
+exports.isDecimal = isDecimal;
