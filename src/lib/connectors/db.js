cc470b51e53d6d199b2d540a1d75444bc98f9df9
James Carmichael <james@siteclick.co.uk>
2020-11-24 09:07:38 +0000

    Tweak knex/pg compatibility function and add comments to regex

--- src/lib/connectors/db.js
+++ src/lib/connectors/db.js
@@ -18,10 +18,27 @@ const query = (sqlQuery, params = []) => {
   }
 
   const data = params.reduce((acc, param, i) => {
-    acc.params[`param_${i}`] = param;
-    const r = new RegExp(`\\$${(i + 1)}(?![0-9])`, 'g');
-    acc.sqlQuery = acc.sqlQuery.replace(r, `:param_${i}`);
-    return acc;
+    // This is the parameter name for the knex query - in knex
+    // parameters are passed as a hash
+    const key = `param_${i}`;
+
+    // This regex is looking for a pattern such as $4 in the query
+    // as this is how bound parameters are specified in the underlying
+    // pg library.
+    // It is made up of:
+    // \\$ a dollar symbol with the necessary escaping since $ is a special char in regex
+    // ${i + 1} is the integer to search for
+    // (?![0-9]) is a negative lookahead, and ensures the integer isn't followed by another integer
+    // - this avoids e.g. $11 being a match when we are replacing $1
+    const r = new RegExp(`\\$${i + 1}(?![0-9])`, 'g');
+
+    return {
+      params: {
+        ...acc.params,
+        [key]: param
+      },
+      sqlQuery: acc.sqlQuery.replace(r, `:${key}`)
+    };
   }, { sqlQuery, params: {} });
 
   return knex.knex.raw(data.sqlQuery, data.params);
