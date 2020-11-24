ad1bb9c70f6c09674d62f775a194519c44237202
Tim <tim@tamimi.dev>
2020-11-24 12:52:11 +0000

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,6 @@ const server = Hapi.server({
 
 const plugins = [
   require('./src/lib/message-queue').plugin,
-  require('./src/modules/billing/register-subscribers'),
   require('./src/modules/invoice-accounts/register-subscribers'),
   require('./src/modules/returns/register-subscribers'),
   require('./src/plugins/internal-calling-user'),
