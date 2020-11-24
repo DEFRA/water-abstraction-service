fe54aff50306794de83be3205d7d8b2c3196eb26
James Carmichael <james@siteclick.co.uk>
2020-11-24 09:49:19 +0000

    Additional comments and use Map in QueueManager class

--- src/modules/billing/jobs/lib/QueueManager.js
+++ src/modules/billing/jobs/lib/QueueManager.js
@@ -8,10 +8,17 @@ const STATUS_FAILED = 'failed';
 
 /**
  * @class provides a container for the Bull MQ job queues
+ * This is to extract the dependency on Bull out of the job code to make them
+ * more testable.
+ * This class contains a map of queues, where the keys are the job names.
+ * Each item contains a Bull Queue instance and necessary workers.
+ * The onComplete handlers are wrapped so that the QueueManager instance
+ * can pass itself as an argument - this allows the onComplete handlers
+ * to add jobs on another queue
  */
 class QueueManager {
   constructor () {
-    this._queues = {};
+    this._queues = new Map();
   }
 
   /**
@@ -20,8 +27,9 @@ class QueueManager {
    * @param  {...any} args
    */
   add (jobName, ...args) {
-    const { createMessage } = this._queues[jobName].jobContainer;
-    return this._queues[jobName].queue.add(
+    const queueContainer = this._queues.get(jobName);
+    const { createMessage } = queueContainer.jobContainer;
+    return queueContainer.queue.add(
       ...createMessage(...args)
     );
   }
@@ -43,30 +51,33 @@ class QueueManager {
     // Create queue
     const queue = new bull.Queue(jobContainer.jobName, { connection });
 
-    // Register worker
+    // Create worker with handler
     const workerOpts = {
       ...(jobContainer.workerOptions || {}),
       connection
     };
     const worker = new bull.Worker(jobContainer.jobName, jobContainer.handler, workerOpts);
 
-    // Create scheduler
+    // Create scheduler - this is only set up if the hasScheduler flag is set.
+    // This is needed if the job makes use of Bull features such as retry/cron
     const scheduler = jobContainer.hasScheduler
       ? new bull.QueueScheduler(jobContainer.jobName, { connection: ioRedis.createConnection() })
       : null;
 
-    // Register handlers
+    // Register onComplete handler if defined
     if (jobContainer.onComplete) {
       worker.on(STATUS_COMPLETED, job => jobContainer.onComplete(job, this));
     }
+    // An onFailed handler must always be defined
     worker.on(STATUS_FAILED, jobContainer.onFailed);
 
-    this._queues[jobContainer.jobName] = {
+    // Register all the details in the map
+    this._queues.set(jobContainer.jobName, {
       jobContainer,
       queue,
       worker,
       scheduler
-    };
+    });
 
     return this;
   }
