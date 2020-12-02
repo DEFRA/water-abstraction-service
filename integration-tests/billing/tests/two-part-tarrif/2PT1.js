4887108bcb753f216066c963adcd2a72e0aa5846
Stephan Hough <43169948+Stephan-Hough@users.noreply.github.com>
2020-12-02 16:16:03 +0000

    feat/water-2746 -- tests added

--- integration-tests/billing/tests/two-part-tarrif/2PT1.js
+++ integration-tests/billing/tests/two-part-tarrif/2PT1.js
@@ -5,10 +5,14 @@ const chargeModuleTransactionsService = require('../../services/charge-module-tr
 const {
   experiment,
   test,
-  before
+  beforeEach,
+  before,
+  after
 } = exports.lab = require('@hapi/lab').script();
+const { omit } = require('lodash');
 
 const services = require('../../services');
+const transactionTests = require('../transaction-tests');
 
 experiment('two part tariff ref: 2PT1', () => {
   let batch;
@@ -38,18 +42,183 @@ experiment('two part tariff ref: 2PT1', () => {
     console.log(batch);
 
     twoPartTariffBatch = await services.scenarios.approveTwoPartTariffBatch(batch.billingBatchId);
+    console.log(twoPartTariffBatch);
 
     chargeModuleTransactions = await chargeModuleTransactionsService.getTransactionsForBatch(twoPartTariffBatch);
     console.log(chargeModuleTransactions);
   });
 
   experiment('has expected batch details', () => {
-    test('the batch is "supplementary"', async () => {
-      expect('annual').to.equal('annual');
+    test('the batch is "two-part-tariff"', async () => {
+      expect(batch.batchType).to.equal('two-part-tariff');
+    });
+
+    test('the batch has the correct financial year range', async () => {
+      expect(batch.fromFinancialYearEnding).to.equal(2020);
+      expect(batch.toFinancialYearEnding).to.equal(2020);
+    });
+
+    test('the batch is in "ready" status', async () => {
+      expect(batch.status).to.equal('ready');
+    });
+
+    test('the batch has been created in the charge module', async () => {
+      expect(batch.billRunNumber).to.be.a.number();
+      expect(batch.externalId).to.be.a.string().length(36);
+    });
+
+    test('no error codes are generated', async () => {
+      expect(batch.errorCode).to.equal(null);
     });
   });
 
-  // after(async () => {
-  //   await services.tearDown.tearDown(annualBatch);
-  // });
+  experiment('has expected invoice details', () => {
+    test('1 invoice is generated', async () => {
+      expect(batch.billingInvoices).to.have.length(1);
+    });
+
+    experiment('the first invoice', () => {
+      let invoice;
+
+      beforeEach(async () => {
+        invoice = batch.billingInvoices[0];
+      });
+
+      test('has the correct invoice account', async () => {
+        expect(invoice.invoiceAccountNumber).to.equal('A99999999A');
+      });
+
+      test('has the correct invoice address', async () => {
+        expect(omit(invoice.address, 'uprn')).to.equal({
+          town: 'Testington',
+          county: 'Testingshire',
+          country: 'UK',
+          postcode: 'TT1 1TT',
+          addressLine1: 'Big Farm',
+          addressLine2: 'Windy road',
+          addressLine3: 'Buttercup meadow',
+          addressLine4: null,
+          source: 'nald'
+        });
+      });
+
+      test('has 1 licence on the invoice', async () => {
+        expect(invoice.billingInvoiceLicences).to.have.length(1);
+      });
+
+      experiment('the first invoice licence', () => {
+        let licence;
+
+        beforeEach(async () => {
+          licence = invoice.billingInvoiceLicences[0];
+        });
+
+        // test('has the correct licence name', async () => {
+        //   expect(licence.licenceHolderName.lastName).to.equal('Testerson');
+        //   expect(licence.licenceHolderName.firstName).to.equal('John');
+        //   expect(licence.licenceHolderName.title).to.equal('Mr');
+        // });
+
+        // test('has the correct licence holder address', async () => {
+        //   expect(omit(licence.licenceHolderAddress, 'id')).to.equal({
+        //     town: 'Testington',
+        //     county: 'Testingshire',
+        //     country: 'UK',
+        //     postcode: 'TT1 1TT',
+        //     addressLine1: 'Big Farm',
+        //     addressLine2: 'Windy road',
+        //     addressLine3: 'Buttercup meadow',
+        //     addressLine4: null
+        //   });
+        // });
+
+        test('has 1 transaction', async () => {
+          expect(licence.billingTransactions).to.have.length(2);
+        });
+
+        experiment('the first transaction', () => {
+          let transaction;
+          beforeEach(async () => {
+            transaction = licence.billingTransactions[1];
+          });
+
+          test('is a standard charge', async () => {
+            expect(transaction.chargeType).to.equal('standard');
+            expect(transaction.isCredit).to.be.false();
+            expect(transaction.isTwoPartTariffSupplementary).to.be.false();
+            expect(transaction.isDeMinimis).to.be.false();
+            expect(transaction.isNewLicence).to.be.false();
+          });
+
+          test('has the correct charge period', async () => {
+            expect(transaction.startDate).to.equal('2019-04-01');
+            expect(transaction.endDate).to.equal('2020-03-31');
+          });
+
+          test('has the correct abstraction period', async () => {
+            expect(transaction.abstractionPeriod).to.equal({
+              endDay: 31,
+              endMonth: 3,
+              startDay: 1,
+              startMonth: 4
+            });
+          });
+
+          test('has the correct factors', async () => {
+            expect(transaction.source).to.equal('unsupported');
+            expect(transaction.season).to.equal('summer');
+            expect(transaction.loss).to.equal('low');
+          });
+
+          test('has the correct quantities', async () => {
+            expect(transaction.authorisedQuantity).to.equal('50');
+            expect(transaction.billableQuantity).to.equal('50');
+            expect(transaction.volume).to.equal('50');
+          });
+
+          test('has the correct authorised/billable days', async () => {
+            expect(transaction.authorisedDays).to.equal(366);
+            expect(transaction.billableDays).to.equal(366);
+          });
+
+          test('has been sent to the charge module', async () => {
+            expect(transaction.externalId).to.have.length(36);
+            expect(transaction.status).to.equal('charge_created');
+          });
+
+          test('has the correct description', async () => {
+            expect(transaction.description).to.equal('CE1');
+          });
+
+          test('has the correct agreements', async () => {
+            expect(transaction.section126Factor).to.equal(null);
+            expect(transaction.section127Agreement).to.equal(false);
+            expect(transaction.section130Agreement).to.equal(null);
+          });
+
+          test('has a stable transaction key', async () => {
+            expect(transaction.transactionKey).to.equal('dc1467401acc8ff1124ac55ef6033a15');
+          });
+        });
+      });
+    });
+  });
+
+  experiment('transactions', () => {
+    test('the batch and charge module have the same number of transactions', async () => {
+      transactionTests.assertNumberOfTransactions(batch, chargeModuleTransactions);
+    });
+
+    test('the batch and charge module contain the same transactions', async () => {
+      transactionTests.assertTransactionsAreInEachSet(batch, chargeModuleTransactions);
+    });
+
+    test('the charge module transaction contain the expected data', async () => {
+      transactionTests.assertBatchTransactionDataExistsInChargeModule(batch, chargeModuleTransactions);
+    });
+  });
+
+  after(async () => {
+    await services.tearDown.tearDown(batch);
+  });
 });
