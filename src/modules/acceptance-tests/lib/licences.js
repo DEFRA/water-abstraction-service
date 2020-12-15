bf7c429d158ec989d43f5c233494241513099126
Tim <tim@tamimi.dev>
2020-12-15 11:25:56 +0000

    Just tidying

--- src/modules/acceptance-tests/lib/licences.js
+++ src/modules/acceptance-tests/lib/licences.js
@@ -14,7 +14,16 @@ const deleteLicences = () => {
 
 const createLicence = async (companyId, licenceId, licenceRef) => {
   const regions = await regionsConnector.find();
-  return licencesConnector.create(licenceRef, regions.find(x => x.naldRegionId === 6).regionId, new Date().toJSON().slice(0, 10), null, { historicalAreaCode: 'SAAR', regionalChargeArea: 'Southern' }, true, true);
+  return licencesConnector.create(
+    licenceRef,
+    regions.find(x => x.naldRegionId === 6).regionId,
+    new Date().toJSON().slice(0, 10),
+    null, {
+      historicalAreaCode: 'SAAR',
+      regionalChargeArea: 'Southern'
+    },
+    true,
+    true);
 };
 
 exports.create = createLicence;
