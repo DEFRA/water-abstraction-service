2dd1e2659923764eb150595c7ec0c7a87eda0b9b
Stephan Hough <43169948+Stephan-Hough@users.noreply.github.com>
2021-02-22 20:38:31 +0000

    feat/water-3001 -- cron job changed to run every 6 hours

--- src/modules/charge-versions/cron.js
+++ src/modules/charge-versions/cron.js
@@ -4,9 +4,9 @@ const cron = require('node-cron');
 const createChargeVersionWorkflows = require('./lib/charge-version-workflow-jobs-processor');
 
 /**
- * Scheduled jobs for batch notification handling
+ * Scheduled jobs for creating charge version workflows
  */
-const scheduleJobs = () => cron.schedule('*/5 * * * *', createChargeVersionWorkflows);
+const scheduleJobs = () => cron.schedule('0 /6 * * *', createChargeVersionWorkflows);
 
 module.exports = {
   scheduleJobs
