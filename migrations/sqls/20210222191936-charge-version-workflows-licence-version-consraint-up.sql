576c1b94d93c2ecf751f479af2f8fbc35f064fb7
Stephan Hough <43169948+Stephan-Hough@users.noreply.github.com>
2021-02-24 09:07:14 +0000

    feat/water-3001 -- date_deleted added to unique cionstraint on charge_version_workflows

--- migrations/sqls/20210222191936-charge-version-workflows-licence-version-consraint-up.sql
+++ migrations/sqls/20210222191936-charge-version-workflows-licence-version-consraint-up.sql
@@ -8,7 +8,7 @@ ADD COLUMN date_deleted TIMESTAMP DEFAULT NULL;
 CREATE TEMP TABLE tmp_top_licence_versions AS
 (SELECT * FROM 
  (SELECT *, rank() OVER (PARTITION BY licence_id ORDER BY ranked DESC)
-  FROM (SELECT *,LPAD(concat(issue, increment)::text, 9, '0') AS ranked from water.licence_versions) as lvs) AS rankedLicences
+  FROM (SELECT *,LPAD(concat(issue, increment)::TEXT, 9, '0') AS ranked FROM water.licence_versions) AS lvs) AS rankedLicences
 WHERE rank = 1);
 
 UPDATE water.charge_version_workflows cw  SET
@@ -21,4 +21,4 @@ DROP TABLE tmp_top_licence_versions;
 
 --create the constraints
 ALTER TABLE water.charge_version_workflows
-ADD CONSTRAINT unique_licence_version_id UNIQUE (licence_version_id);
+ADD CONSTRAINT unique_licence_version_id_date_deleted UNIQUE (licence_version_id, date_deleted);
