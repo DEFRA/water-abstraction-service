0ee89e23b91b590fc01ba9805bb5c88235e1d9ad
James Carmichael <james@siteclick.co.uk>
2021-02-25 08:48:37 +0000

    Remove code to create hashed transaction keys

--- migrations/sqls/20210224114453-add-source-transaction-id-down.sql
+++ migrations/sqls/20210224114453-add-source-transaction-id-down.sql
@@ -1,3 +1,4 @@
 /* Replace with your SQL commands */
 alter table water.billing_transactions
-  drop column source_transaction_id;
+  drop column source_transaction_id,
+  add column transaction_key uuid;
