950a03e48d3089db62e890e3322891ef0b8be1dd
Stephan Hough <43169948+Stephan-Hough@users.noreply.github.com>
2021-02-26 12:53:05 +0000

    feat/water-3001 --  getLicencesWithoutChargeVersions route removed, this data is now provided through the charge-versions/getChargeVersionWorkflows route

--- src/lib/connectors/repos/queries/licence-versions.js
+++ src/lib/connectors/repos/queries/licence-versions.js
@@ -1,6 +1,17 @@
-exports.findIdsCreatedAfterDate = `SELECT licence_version_id, licence_id
-FROM water.licence_versions
-WHERE date_created >=:dateAndTime 
-AND licence_version_id NOT IN (
+exports.findIdsCreatedAfterDate = `
+SELECT licence_version_id, licence_id
+FROM (
+SELECT * FROM
+water.licence_versions
+WHERE date_created >=:dateAndTime
+UNION
+select lv.*
+from water.licences l
+  inner join water.licence_versions lv
+    on l.licence_id = lv.licence_id and lv.status in ('superseded', 'current')
+  left join water.charge_versions cv
+    on l.licence_ref = cv.licence_ref
+where cv.licence_ref is null) AS licenceVersions
+WHERE licence_version_id NOT IN (
 SELECT licence_version_id
 FROM water.charge_version_workflows);`;
