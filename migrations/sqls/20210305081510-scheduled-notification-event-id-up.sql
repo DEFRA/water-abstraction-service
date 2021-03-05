0bb895f61b32079b7762ac84044f7aeb5c1278c2
James Carmichael <james@siteclick.co.uk>
2021-03-05 09:57:14 +0000

    Use uuid fields for event_id

--- migrations/sqls/20210305081510-scheduled-notification-event-id-up.sql
+++ migrations/sqls/20210305081510-scheduled-notification-event-id-up.sql
@@ -1,3 +1,7 @@
+-- Convert events table to use proper GUID
+alter table water.events 
+	alter column event_id type uuid using event_id::uuid;
+
 -- Set any invalid event_id to null in scheduled_notification table
 update water.scheduled_notification
 set event_id=null
@@ -7,9 +11,13 @@ from (
 	where 
 	  sn.event_id is not null 
 	  and sn.event_id not in (
-	  select event_id from water.events
+	  select event_id::varchar from water.events
 	)
 ) sn2 where water.scheduled_notification.id=sn2.id;
+
+-- Make event_id column a guid
+alter table water.scheduled_notification
+  alter column event_id type uuid using event_id::uuid;
 
 -- Set up foreign key constraint to events table
 alter table water.scheduled_notification
