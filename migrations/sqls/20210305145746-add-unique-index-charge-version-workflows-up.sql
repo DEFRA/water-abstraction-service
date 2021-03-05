acb84e3482942ed2ef1d6324808646e8b4110c6d
Stephan Hough <43169948+Stephan-Hough@users.noreply.github.com>
2021-03-05 16:49:31 +0000

    bugfix/water-3001 -- migration fixed

--- migrations/sqls/20210305145746-add-unique-index-charge-version-workflows-up.sql
+++ migrations/sqls/20210305145746-add-unique-index-charge-version-workflows-up.sql
@@ -2,6 +2,6 @@
 DELETE FROM water.charge_version_workflows;
 
 ALTER TABLE water.charge_version_workflows
-DROP CONSTRAINT unique_licence_version_id_date_deleted
+DROP CONSTRAINT unique_licence_version_id_date_deleted;
 
-CREATE UNIQUE INDEX unique_licence_version_id_date_deleted_null ON water.charge_version_workflows (licence_version_id, (date_deleted IS NULL)) WHERE date_deleted IS NULL;
+CREATE UNIQUE INDEX unique_licence_version_id_date_deleted_null ON water.charge_version_workflows (licence_version_id) WHERE date_deleted IS NULL;
