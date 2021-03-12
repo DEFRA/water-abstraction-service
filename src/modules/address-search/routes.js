b5bedcc01bc6a2243feb9f6913625873487fb7e3
James Carmichael <james@siteclick.co.uk>
2021-03-12 13:18:09 +0000

    Fix code smell

--- src/modules/address-search/routes.js
+++ src/modules/address-search/routes.js
@@ -3,7 +3,11 @@
 const Joi = require('@hapi/joi');
 const controller = require('./controller');
 
-const postcodeRegex = /^(([A-Z]{1,2}[0-9][A-Z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) ?[0-9][A-Z]{2}|BFPO ?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]{2} ?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$/;
+const postcodePart1 = '(([A-Z]{1,2}[0-9][A-Z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA)';
+const postcodePart2 = '[0-9][A-Z]{2}|BFPO ?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]{2} ?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)';
+
+// https://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom#Validation
+const postcodeRegex = new RegExp(`^${postcodePart1} ?${postcodePart2}$`);
 
 module.exports = {
   getAddressSearch: {
