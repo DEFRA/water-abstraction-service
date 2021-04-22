cd7fff579d8bfbf904e6e7bb0c712165fd30cca6
Stephan Hough <43169948+Stephan-Hough@users.noreply.github.com>
2021-04-22 15:46:49 +0100

    feat/water-3041 -- migration to add notify service alert template

---
+++ migrations/sqls/20210422144120-add-notify-service-alert-template-down.sql
@@ -0,0 +1,2 @@
+/* Replace with your SQL commands */
+DELETE FROM water.notify_templates WHERE template_id = 'c34d1b16-694b-4364-8e7e-83e9dbd34a62';
---
+++ migrations/sqls/20210422144120-add-notify-service-alert-template-up.sql
@@ -0,0 +1,3 @@
+/* Replace with your SQL commands */
+INSERT INTO water.notify_templates (message_ref, template_id, notes, notify_key) 
+VALUES ('service_status_alert', 'c34d1b16-694b-4364-8e7e-83e9dbd34a62', 'template to send service alerts for technical errors', 'live');
