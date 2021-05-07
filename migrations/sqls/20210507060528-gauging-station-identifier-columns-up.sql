bd44770d53ad770a3485c1a4cce520e21f14e1ef
Tim <tim@tamimi.dev>
2021-05-07 07:32:34 +0100

    Adds migration scripts

---
+++ migrations/sqls/20210507060528-gauging-station-identifier-columns-up.sql
@@ -0,0 +1,10 @@
+-- Drop the current constraint and id column
+ALTER TABLE water.gauging_stations DROP CONSTRAINT gauging_stations_pkey;
+ALTER TABLE water.gauging_stations DROP COLUMN id;
+
+-- Add the new primary key
+ALTER TABLE water.gauging_stations ADD COLUMN gauging_station_id uuid DEFAULT public.gen_random_uuid();
+ALTER TABLE water.gauging_stations ADD PRIMARY KEY (gauging_station_id);
+
+-- Add a new Unique col called hydrology_station_id to hold the external guids
+ALTER TABLE water.gauging_stations ADD COLUMN hydrology_station_id uuid UNIQUE;
