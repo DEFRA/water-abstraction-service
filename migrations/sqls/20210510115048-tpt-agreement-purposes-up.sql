4fc72ec7a7e555a326db99f1480ec43c98384f0e
James Carmichael <james@siteclick.co.uk>
2021-05-11 14:25:32 +0100

    Tweak migration

--- migrations/sqls/20210510115048-tpt-agreement-purposes-up.sql
+++ migrations/sqls/20210510115048-tpt-agreement-purposes-up.sql
@@ -13,3 +13,5 @@ create type water.licence_agreement_source AS ENUM ('nald', 'wrls');
 alter table water.licence_agreements
   add column source water.licence_agreement_source default 'wrls' not null;
 
+/* Initially set all records to "NALD" source */
+update water.licence_agreements set source='nald';
