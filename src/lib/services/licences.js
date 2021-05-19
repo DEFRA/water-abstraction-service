95a950910b22a1e3a59b2ee532ba8df9eb2dbc37
Tim <tim@tamimi.dev>
2021-05-19 13:15:34 +0100

    WIP

--- src/lib/services/licences.js
+++ src/lib/services/licences.js
@@ -30,8 +30,6 @@ const getLicenceById = async licenceId =>
  * @param {Number} regionCode
  */
 const getLicenceByLicenceRef = async (licenceRef, regionCode) => {
-  console.log(licenceRef);
-  console.log(regionCode);
   const licences = await repos.licences.findOneByLicenceRef(licenceRef);
   const licence = licences.find(licence => licence.region.naldRegionId === +regionCode);
 
