9bbae5e3fc2aa608edf2f3719fe3bb70dd134cac
Axel Sunnehall <axels@dashup.co>
2021-06-06 21:29:05 +0100

    Updated query for gauging station restrictions/licences

--- src/lib/connectors/repos/queries/gauging-stations.js
+++ src/lib/connectors/repos/queries/gauging-stations.js
@@ -1,4 +1,15 @@
-exports.findGaugingStations = `SELECT water.gauging_stations.* from water.gauging_stations 
-join water.gauging_station_condition on  water.gauging_stations.gauging_station_id = water.gauging_station_condition.gauging_station_id  
-join water.licences on water.gauging_station_condition.licence_id = water.licences.licence_id
-join water.licence_versions on licence_versions.licence_id = water.licences.licence_id WHERE water.gauging_stations.gauging_station_id=:id`;
+exports.findGaugingStations = `select 
+water.licence_version_purpose_condition_types.*,
+water.licence_version_purpose_conditions.*,
+water.licence_version_purposes.*,
+water.licence_versions.end_date,
+water.gauging_stations.* from 
+water.gauging_stations join water.gauging_station_condition on (water.gauging_stations.gauging_station_id = water.gauging_station_condition.gauging_station_id)  
+join water.licences on (water.gauging_station_condition.licence_id = water.licences.licence_id)
+join water.licence_versions on (water.licences.licence_id = licence_versions.licence_id)
+join water.licence_version_purposes on (water.licence_versions.licence_version_id = water.licence_version_purposes.licence_version_id)
+join water.licence_version_purpose_conditions on (water.licence_version_purposes.licence_version_purpose_id = water.licence_version_purpose_conditions.licence_version_purpose_id) 
+join water.licence_version_purpose_condition_types on (water.licence_version_purpose_conditions.licence_version_purpose_condition_type_id =
+water.licence_version_purpose_condition_types.licence_version_purpose_condition_type_id) 
+where (water.licence_versions.end_date is null or water.licence_versions.end_date > now())
+and water.gauging_stations.gauging_station_id=:gaugingStationId;`;
