85a774204047561fdb15523dd11bc9b0219f5d74
James Carmichael <james@siteclick.co.uk>
2021-06-09 12:21:58 +0100

    Tweak to use controller pattern

--- src/modules/billing/controllers/invoice-licences.js
+++ src/modules/billing/controllers/invoice-licences.js
@@ -1,16 +1,12 @@
 'use strict';
 
 const invoiceLicenceService = require('../services/invoice-licences-service');
-const mapErrorResponse = require('../../../lib/map-error-response');
+const controller = require('../../../lib/controller');
 
-const deleteInvoiceLicence = async (request, h) => {
-  const { invoiceLicenceId } = request.params;
-  try {
-    await invoiceLicenceService.deleteByInvoiceLicenceId(invoiceLicenceId);
-    return h.response().code(204);
-  } catch (err) {
-    return mapErrorResponse(err);
-  }
-};
+const deleteInvoiceLicence = (request, h) => controller.deleteEntity(
+  invoiceLicenceService.deleteByInvoiceLicenceId,
+  h,
+  request.params.invoiceLicenceId
+);
 
 exports.deleteInvoiceLicence = deleteInvoiceLicence;
