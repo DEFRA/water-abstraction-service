36a1f8bc6e9034e9ec16a4ee5628028e16b68100
Tim <tim@tamimi.dev>
2021-06-21 14:14:49 +0100

    Fixes a broken test

--- test/lib/services/licence-version-purpose-conditions.js
+++ test/lib/services/licence-version-purpose-conditions.js
@@ -43,7 +43,7 @@ experiment('src/lib/services/licence-version-purpose-conditions', () => {
     const tempGuid = uuid();
     experiment('when the licence is found', () => {
       beforeEach(async () => {
-        lvpcRepo.findManyByLicenceId.resolves([]);
+        lvpcRepo.findManyByLicenceId.resolves({ rows: [] });
         await lvpcService.getLicenceVersionPurposeConditionsByLicenceId(tempGuid);
       });
 
