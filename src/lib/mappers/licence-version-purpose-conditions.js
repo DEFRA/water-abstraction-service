1228d837ba239de5e65e428e398c6a7af328a063
Tim <tim@tamimi.dev>
2021-06-21 14:16:07 +0100

    Update licence-version-purpose-conditions.js

---
+++ src/lib/mappers/licence-version-purpose-conditions.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const LicenceVersionPurposeCondition = require('../models/licence-version-purpose-condition');
+const { createMapper } = require('../object-mapper');
+const helpers = require('./lib/helpers');
+
+const dbToModelMapper = createMapper()
+  .copy(
+    'param1',
+    'param2',
+    'licenceVersionPurposeConditionId',
+    'notes'
+  );
+
+const dbToModel = row =>
+  helpers.createModel(LicenceVersionPurposeCondition, row, dbToModelMapper);
+
+exports.dbToModel = dbToModel;
