0fe34ebd541a7999b36e7050eb78b628b5e9ccf9
Tim <tim@tamimi.dev>
2021-06-28 09:55:51 +0100

    Updates Charging Go live default date + query for creating new charge version workflows

--- src/lib/connectors/repos/queries/licence-versions.js
+++ src/lib/connectors/repos/queries/licence-versions.js
@@ -1,17 +1,19 @@
-exports.findIdsCreatedAfterDate = `
-SELECT DISTINCT licence_version_id, licence_id
-FROM (
-SELECT * FROM
-water.licence_versions
-WHERE date_created >=:dateAndTime
-UNION
-select lv.*
-from water.licences l
-  inner join water.licence_versions lv
-    on l.licence_id = lv.licence_id and lv.status in ('superseded', 'current')
-  left join water.charge_versions cv
-    on l.licence_ref = cv.licence_ref
-where cv.licence_ref is null) AS licenceVersions
-WHERE licence_version_id NOT IN (
-SELECT licence_version_id
-FROM water.charge_version_workflows);`;
+const { naldSwitchOverDate } = require('../../../../../config').billing;
+
+exports.findIdsCreatedAfterDate = `select
+distinct licenceVersions.licence_version_id,
+  licenceVersions.licence_id
+from
+(select * from water.licence_versions where
+date_created >= ${naldSwitchOverDate}::timestamp
+union
+select lv.* from water.licences l
+inner join water.licence_versions lv on
+l.licence_id = lv.licence_id
+and lv.status in ('superseded', 'current')
+left join water.charge_versions cv on
+l.licence_ref = cv.licence_ref where
+cv.licence_ref is null) as licenceVersions where
+licence_version_id not in (select
+licence_version_id from water.charge_version_workflows where 
+licence_version_id is not null);`;
