92009b73742bcd3df5072ddbcd7a0a68b0ea7254
Tim <tim@tamimi.dev>
2021-08-09 18:24:39 +0100

    WIP

--- src/lib/services/licence-gauging-stations-service.js
+++ src/lib/services/licence-gauging-stations-service.js
@@ -1,6 +1,8 @@
 'use strict';
 const licenceGaugingStationsRepo = require('../connectors/repos/licence-gauging-stations');
 
+const findLicenceGaugingStationsByFilter = licenceGaugingStationsRepo.findLicenceGaugingStationsByFilter;
+
 const getLicenceGaugingStationById = async licenceGaugingStationId => licenceGaugingStationsRepo.findOneById(licenceGaugingStationId);
 
 const createNewLicenceLink = (gaugingStationId, licenceId, properties = {
@@ -15,6 +17,7 @@ const createNewLicenceLink = (gaugingStationId, licenceId, properties = {
 
 const deleteLicenceLink = licenceGaugingStationsRepo.deleteOne;
 
+exports.findLicenceGaugingStationsByFilter = findLicenceGaugingStationsByFilter;
 exports.getLicenceGaugingStationById = getLicenceGaugingStationById;
 exports.createNewLicenceLink = createNewLicenceLink;
 exports.deleteLicenceLink = deleteLicenceLink;
--- src/modules/gauging-stations/jobs/sync-licence-conditions-from-digitise.js
+++ src/modules/gauging-stations/jobs/sync-licence-conditions-from-digitise.js
@@ -11,6 +11,8 @@ const JOB_NAME = 'gauging-stations.copy-lvpc-from-digitise';
 
 // Handy stuff
 const config = require('../../../../config');
+const licenceGaugingStationsService = require('../../../lib/services/licence-gauging-stations-service');
+const licencesService = require('../../../lib/services/licences');
 
 const createMessage = () => ([
   JOB_NAME,
@@ -33,20 +35,53 @@ const handler = async () => {
     const licences = await permitConnector.licences.getWaterLicencesThatHaveConditionsThatNeedToBeCopiedFromDigitise();
 
     // Iterate through each licence's licence_data_value column.
-    return licences.map(eachLicence => {
+    return licences.map(async eachLicence => {
       const edits = get(eachLicence, 'licence_data_value', {});
       if (edits.status === 'Approved') {
       // put it through the reducer
+        // console.log(edits.actions.map(n => n.payload.data));
+        console.log(edits.actions);
         const { payload: finalShape } = merge({}, ...edits.actions);
 
-        const { max_rate: maxRate, max_rate_unit: maxRateUnit, gauging_station: GaugingStation } = finalShape.data;
+        const { max_rate: maxRate, max_rate_unit, gauging_station: GaugingStation } = finalShape.data;
+        const maxRateUnit = max_rate_unit.replace('³', '3');
         const { id: gaugingStationId } = GaugingStation;
         // Look for the relevant sub-part of the object, if one exists
         if (maxRate && maxRateUnit && gaugingStationId) {
-          console.log(finalShape.data);
-          console.log(maxRate);
-          console.log(maxRateUnit);
-          console.log(gaugingStationId);
+          const flowUnits = ['Ml/d', 'm3/s', 'm3/d', 'l/s'];
+          const isFlowOrLevel = flowUnits.includes(maxRateUnit) ? 'flow' : 'level';
+
+          // Check if an identical linkage already exists
+          const licenceRecord = await licencesService.getLicenceByLicenceRef(eachLicence.licence_ref);
+
+          /**
+           * 2.1; 2.3; 2.5; 2.9.1;2.9.2;2.9.3; 2.9.4; 2.9.5; 2.9.6; 2.9.7; 2.9.8 are all Stop conditions
+           *
+           * 2.2; 2.4; 2.6 and 2.7 are reduce conditions.
+           */
+          const existingLinkage = await licenceGaugingStationsService.findLicenceGaugingStationsByFilter({
+            licence_id: licenceRecord.id,
+            gauging_station_id: gaugingStationId,
+            restriction_type: isFlowOrLevel,
+            threshold_unit: maxRateUnit,
+            threshold_value: maxRate
+          });
+
+          if (existingLinkage.length === 0) {
+            console.log('CREATING A LINKAGE');
+            await licenceGaugingStationsService.createNewLicenceLink(gaugingStationId, licenceRecord.id, {
+              licenceVersionPurposeConditionId: null, // Todo
+              thresholdUnit: maxRateUnit,
+              thresholdValue: maxRate,
+              abstractionPeriod: null,
+              restrictionType: isFlowOrLevel,
+              alertType: 'reduce', // TODO
+              source: 'digitse'
+            });
+          } else {
+            console.log('LINKAGE ALREADY EXISTS');
+          }
+
         // For the successful records,
         // mark them as processed by updating the datestamp
         // in permit.licence.date_licence_version_purpose_conditions_last_copied
