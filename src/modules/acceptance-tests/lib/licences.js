7c8fdd74f9ab6946d72f5a2cb1864c113243408a
James Chapman <james.chapman@defra.gov.uk>
2021-08-10 14:47:02 +0100

    Remove licenses teardown

--- src/modules/acceptance-tests/lib/licences.js
+++ src/modules/acceptance-tests/lib/licences.js
@@ -1,18 +1,5 @@
-const ACCEPTANCE_TEST_SOURCE = 'acceptance-test-setup';
-const { bookshelf } = require('../../../lib/connectors/bookshelf');
-
 const licencesConnector = require('../../../lib/connectors/repos/licences');
 const regionsConnector = require('../../../lib/connectors/repos/regions');
-
-const deleteLicencesQuery = `
-    delete from
-    water.licences
-    where licence_ref IN (SELECT system_external_id FROM crm.document_header
-    where metadata->>'dataType' = '${ACCEPTANCE_TEST_SOURCE}')
-    OR is_test = true;
-    `;
-
-const deleteLicences = async () => bookshelf.knex.raw(deleteLicencesQuery);
 
 const createLicence = async (companyId, licenceId, licenceRef) => {
   const regions = await regionsConnector.find();
@@ -29,4 +16,3 @@ const createLicence = async (companyId, licenceId, licenceRef) => {
 };
 
 exports.create = createLicence;
-exports.delete = deleteLicences;
