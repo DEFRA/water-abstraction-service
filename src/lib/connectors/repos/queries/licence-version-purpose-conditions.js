cb64be78dac8718119652ad0a9d501f5ebe385ff
Tim <tim@tamimi.dev>
2021-09-03 14:44:27 +0100

    Update licence-version-purpose-conditions.js

--- src/lib/connectors/repos/queries/licence-version-purpose-conditions.js
+++ src/lib/connectors/repos/queries/licence-version-purpose-conditions.js
@@ -1,24 +1,26 @@
 exports.findLicenceVersionPurposeConditionsByLicenceId = `
-  SELECT lvpc.licence_version_purpose_condition_id, lvpc.param_1, lvpc.param_2, lvpc.notes
-  FROM water.licence_version_purpose_conditions lvpc 
+  SELECT * FROM (SELECT distinct on (lvpc.param_1, lvpc.param_2, lvpc.notes) lvpc.licence_version_purpose_condition_id, lvpc.param_1, lvpc.param_2, lvpc.notes, lvpc.date_created
+  FROM water.licence_version_purpose_conditions lvpc
   JOIN water.licence_version_purposes lvp on lvp.licence_version_purpose_id = lvpc.licence_version_purpose_id
   JOIN water.licence_versions lv on lv.licence_version_id = lvp.licence_version_id
-  WHERE lv.licence_id = :licenceId;`;
+  JOIN water.licences l on lv.licence_id = l.licence_id
+  WHERE lv.licence_id = :licenceId) AS temp ORDER BY date_created;`;
 
 exports.findLicenceVersionPurposeConditionsByLicenceIdWithSpecificCode = `
-  SELECT lvpc.licence_version_purpose_condition_id, lvpc.param_1, lvpc.param_2, lvpc.notes
-  FROM water.licence_version_purpose_conditions lvpc 
+  SELECT * FROM (SELECT distinct on (lvpc.param_1, lvpc.param_2, lvpc.notes) lvpc.licence_version_purpose_condition_id, lvpc.param_1, lvpc.param_2, lvpc.notes, lvpc.date_created
+  FROM water.licence_version_purpose_conditions lvpc
   JOIN water.licence_version_purpose_condition_types lvpct on lvpc.licence_version_purpose_condition_type_id = lvpct.licence_version_purpose_condition_type_id
   JOIN water.licence_version_purposes lvp on lvp.licence_version_purpose_id = lvpc.licence_version_purpose_id
   JOIN water.licence_versions lv on lv.licence_version_id = lvp.licence_version_id
+  JOIN water.licences l on lv.licence_id = l.licence_id
   WHERE lv.licence_id = :licenceId
-  AND lvpct.code = :code;`;
+  AND lvpct.code = :code) AS temp ORDER BY date_created;`;
 
 exports.getLicenceVersionConditionByPartialExternalId = `
   SELECT lvpc.licence_version_purpose_condition_id, lvpc.licence_version_purpose_id
   FROM water.licence_version_purpose_conditions lvpc
   WHERE lvpc.external_id LIKE :partialExternalId
-`;
+  ORDER BY lvpc.date_created desc;`;
 
 exports.getLicenceVersionConditionType = `
   SELECT lvpc.licence_version_purpose_condition_type_id 
