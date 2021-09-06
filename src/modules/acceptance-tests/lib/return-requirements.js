61f56e5db5f141be1b3b4b876c3d339cb3b156ba
Stephan Hough <43169948+Stephan-Hough@users.noreply.github.com>
2021-09-06 14:44:37 +0100

    feat/water-3292 -- returns requirements tear down async bug fixed

--- src/modules/acceptance-tests/lib/return-requirements.js
+++ src/modules/acceptance-tests/lib/return-requirements.js
@@ -1,12 +1,11 @@
 const { bookshelf } = require('../../../lib/connectors/bookshelf');
 const queries = require('./queries/return-requirements');
 
-const tearDownData = query => async () => bookshelf.knex.raw(query);
 
 const tearDown = async () => {
-  await tearDownData(queries.deleteReturnRequirementPurposes);
-  await tearDownData(queries.deleteReturnRequirements);
-  await tearDownData(queries.deleteReturnVersions);
+  await bookshelf.knex.raw(queries.deleteReturnRequirementPurposes);
+  await bookshelf.knex.raw(queries.deleteReturnRequirements);
+  await bookshelf.knex.raw(queries.deleteReturnVersions);
 };
 
 exports.tearDown = tearDown;
