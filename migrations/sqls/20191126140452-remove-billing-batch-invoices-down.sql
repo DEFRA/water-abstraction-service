1457cb9685efd97431170fd4ea693e40c6c5904b
Tim <tim@tamimimail.com>
2021-11-30 21:41:47 +0000

    Update 20191126140452-remove-billing-batch-invoices-down.sql

--- migrations/sqls/20191126140452-remove-billing-batch-invoices-down.sql
+++ migrations/sqls/20191126140452-remove-billing-batch-invoices-down.sql
@@ -3,7 +3,7 @@
 -- Table Definition ----------------------------------------------
 
 CREATE TABLE IF NOT EXISTS water.billing_batch_invoices (
-    billing_batch_invoice_id uuid DEFAULT public.gen_random_uuid() PRIMARY KEY,
+    billing_batch_invoice_id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
     billing_batch_id uuid NOT NULL REFERENCES water.billing_batches(billing_batch_id),
     billing_invoice_id uuid NOT NULL REFERENCES water.billing_invoices(billing_invoice_id),
     date_created timestamp without time zone NOT NULL DEFAULT now(),
