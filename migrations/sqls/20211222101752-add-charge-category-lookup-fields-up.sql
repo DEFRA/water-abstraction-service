457f433ed77b843f583f213f3c10e8ded37cbfa1
Stephan Hough <43169948+Stephan-Hough@users.noreply.github.com>
2021-12-22 12:37:23 +0000

    migrations updated

--- migrations/sqls/20211222101752-add-charge-category-lookup-fields-up.sql
+++ migrations/sqls/20211222101752-add-charge-category-lookup-fields-up.sql
@@ -2,4 +2,6 @@ ALTER TABLE water.billing_charge_categories
 add column is_tidal boolean default false,
 add column loss_factor varchar null,
 add column model_tier varchar default null,
-add column restricted_source boolean default false;
+add column restricted_source boolean default false,
+add column min_volume integer null,
+add column max_volume integer null;
