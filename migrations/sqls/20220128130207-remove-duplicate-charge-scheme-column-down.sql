4e44ad4df2da23609669fe0bc0f99a6235d6eed4
Stephan Hough <43169948+Stephan-Hough@users.noreply.github.com>
2022-01-28 16:17:37 +0000

    bugfix/water-3491 -- remove duplicate charge versions charge scheme column

---
+++ migrations/sqls/20220128130207-remove-duplicate-charge-scheme-column-down.sql
@@ -0,0 +1,6 @@
+create type water.charging_scheme as enum ('presroc', 'sroc');
+
+ALTER table water.charge_versions
+add column charging_scheme water.charging_scheme default 'presroc';
+
+update water.charge_versions set charging_scheme = 'presroc';
