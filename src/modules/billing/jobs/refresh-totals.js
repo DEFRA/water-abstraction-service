ec2996f9b7d12411e5b607437bdb39231c751a24
Tim <tim@tamimi.dev>
2022-02-02 12:44:22 +0000

    Update refresh-totals.js

--- src/modules/billing/jobs/refresh-totals.js
+++ src/modules/billing/jobs/refresh-totals.js
@@ -74,3 +74,7 @@ exports.createMessage = createMessage;
 exports.handler = handler;
 exports.hasScheduler = true;
 exports.onFailed = onFailedHandler;
+exports.workerOptions = {
+  lockDuration: 3600000,
+  lockRenewTime: 3600000 / 2
+};
