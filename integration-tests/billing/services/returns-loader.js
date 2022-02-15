fd85275f2937def3cd22d0da80fef82e7f8a1882
Stephan Hough <43169948+Stephan-Hough@users.noreply.github.com>
2022-02-15 16:20:03 +0000

    Revert "Water-3529 fix cypress test bulk upload (#1626)"
    This reverts commit 4a43ac26fc897fb7bfb709f61d7f2b4a6ff97ab5.

--- integration-tests/billing/services/returns-loader.js
+++ integration-tests/billing/services/returns-loader.js
@@ -3,7 +3,7 @@
 const FixtureLoader = require('./fixture-loader/FixtureLoader');
 const AsyncAdapter = require('./fixture-loader/adapters/AsyncAdapter');
 
-const { returns, serviceRequest } = require('@envage/water-abstraction-helpers');
+const { serviceRequest } = require('@envage/water-abstraction-helpers');
 const config = require('../../../config');
 const urlJoin = require('url-join');
 const createReturnsUrl = (...parts) => urlJoin(config.services.returns, ...parts);
@@ -11,7 +11,6 @@ const moment = require('moment');
 
 // Resolve path to fixtures directory
 const path = require('path');
-const { last } = require('lodash');
 const dir = path.resolve(__dirname, '../fixtures');
 
 const createConnector = tail => async body => {
@@ -20,19 +19,14 @@ const createConnector = tail => async body => {
 };
 
 const create = () => {
-  const returnCycleDates = last(returns.date.createReturnCycles(undefined, undefined));
   // create a future date and pass it to the fixture loader as reference for use in yaml objects
   const refDates = {
     name: '$returnDates',
     obj: {
-      dueDateSummerFalse: moment(returnCycleDates.dueDate).add(1, 'month').format('YYYY-MM-DD'),
-      endDateSummerFalse: moment(returnCycleDates.endDate).subtract(1, 'month').format('YYYY-MM-DD'),
-      startDateSummerFalse: moment(returnCycleDates.startDate).add(1, 'month').format('YYYY-MM-DD'),
-      refIdSummerFalse: `v1:1:AT/CURR/MONTHLY/02:9999992:${moment(returnCycleDates.startDate).add(1, 'month').format('YYYY-MM-DD')}:${moment(returnCycleDates.endDate).subtract(1, 'month').format('YYYY-MM-DD')}`,
-      dueDateSummerTrue: moment(returnCycleDates.dueDate).add(2, 'month').format('YYYY-MM-DD'),
-      endDateSummerTrue: moment(returnCycleDates.endDate).subtract(2, 'month').format('YYYY-MM-DD'),
-      startDateSummerTrue: moment(returnCycleDates.startDate).add(2, 'month').format('YYYY-MM-DD'),
-      refIdSummerTrue: `v1:1:AT/CURR/MONTHLY/02:9999992:${moment(returnCycleDates.startDate).add(2, 'month').format('YYYY-MM-DD')}:${moment(returnCycleDates.endDate).subtract(2, 'month').format('YYYY-MM-DD')}`
+      dueDate: moment().add(3, 'month').format('YYYY-MM-DD'),
+      endDate: moment().subtract(3, 'month').format('YYYY-MM-DD'),
+      startDate: moment().subtract(6, 'month').format('YYYY-MM-DD'),
+      refId: `v1:1:AT/CURR/MONTHLY/02:9999992:${moment().subtract(6, 'month').format('YYYY-MM-DD')}:${moment().subtract(3, 'month').format('YYYY-MM-DD')}`
     }
   };
 
