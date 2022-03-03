06456d1c68df14c83a7baba750c589a489be1fa0
Tim <tim@tamimi.dev>
2022-03-03 17:44:32 +0000

    Adds a unit test for the email case

--- test/modules/batch-notifications/config/water-abstraction-alerts/lib/get-recipients.js
+++ test/modules/batch-notifications/config/water-abstraction-alerts/lib/get-recipients.js
@@ -74,8 +74,7 @@ experiment('getRecipients', () => {
     sandbox.stub(licenceGaugingStationConnector, 'findOneById').resolves({
       licenceVersionPurposeConditionId
     });
-    sandbox.stub(lvpcConnector, 'findOneById').resolves({
-    });
+    sandbox.stub(lvpcConnector, 'findOneById').resolves({});
     sandbox.stub(licenceGaugingStationConnector, 'updateStatus').resolves();
     sandbox.stub(eventHelpers, 'markAsProcessed');
     sandbox.stub(logger, 'error').returns();
@@ -85,122 +84,139 @@ experiment('getRecipients', () => {
     sandbox.restore();
   });
 
-  test('finds each relevant document if there is a document Id', async () => {
-    await getRecipients(jobData);
-    expect(crmV2Connector.documents.getDocumentByRefAndDate.callCount).to.equal(jobData.ev.metadata.options.linkages.length);
-  });
-
-  test('finds each relevant contact', async () => {
-    await getRecipients(jobData);
-    expect(crmV2Connector.contacts.getContact.callCount).to.equal(jobData.ev.metadata.options.linkages.length);
-  });
-
-  test('doesnt try to find each relevant contact if there is no contact Id for a given row', async () => {
-    crmV2Connector.documents.getDocumentByRefAndDate.resolves({
-      contactId: undefined
+  experiment('for letters', () => {
+    test('finds each relevant document if there is a document Id', async () => {
+      await getRecipients(jobData);
+      expect(crmV2Connector.documents.getDocumentByRefAndDate.callCount).to.equal(jobData.ev.metadata.options.linkages.length);
     });
-    await getRecipients(jobData);
-    expect(crmV2Connector.contacts.getContact.callCount).to.equal(0);
-  });
 
-  test('finds each relevant company', async () => {
-    await getRecipients(jobData);
-    expect(crmV2Connector.companies.getCompany.callCount).to.equal(jobData.ev.metadata.options.linkages.length);
-  });
-
-  test('doesnt try to find each relevant company if there is no company Id for a given row', async () => {
-    crmV2Connector.documents.getDocumentByRefAndDate.resolves({
-      companyId: undefined
+    test('finds each relevant contact', async () => {
+      await getRecipients(jobData);
+      expect(crmV2Connector.contacts.getContact.callCount).to.equal(jobData.ev.metadata.options.linkages.length);
     });
-    await getRecipients(jobData);
-    expect(crmV2Connector.companies.getCompany.callCount).to.equal(0);
-  });
 
-  test('finds each relevant address', async () => {
-    await getRecipients(jobData);
-    expect(crmV2Connector.addresses.getAddress.callCount).to.equal(jobData.ev.metadata.options.linkages.length);
-  });
-
-  test('doesnt try to find each relevant address if there is no address Id for a given row', async () => {
-    crmV2Connector.documents.getDocumentByRefAndDate.resolves({
-      addressId: undefined
+    test('doesnt try to find each relevant contact if there is no contact Id for a given row', async () => {
+      crmV2Connector.documents.getDocumentByRefAndDate.resolves({
+        contactId: undefined
+      });
+      await getRecipients(jobData);
+      expect(crmV2Connector.contacts.getContact.callCount).to.equal(0);
     });
-    await getRecipients(jobData);
-    expect(crmV2Connector.addresses.getAddress.callCount).to.equal(0);
+
+    test('finds each relevant company', async () => {
+      await getRecipients(jobData);
+      expect(crmV2Connector.companies.getCompany.callCount).to.equal(jobData.ev.metadata.options.linkages.length);
+    });
+
+    test('doesnt try to find each relevant company if there is no company Id for a given row', async () => {
+      crmV2Connector.documents.getDocumentByRefAndDate.resolves({
+        companyId: undefined
+      });
+      await getRecipients(jobData);
+      expect(crmV2Connector.companies.getCompany.callCount).to.equal(0);
+    });
+
+    test('finds each relevant address', async () => {
+      await getRecipients(jobData);
+      expect(crmV2Connector.addresses.getAddress.callCount).to.equal(jobData.ev.metadata.options.linkages.length);
+    });
+
+    test('doesnt try to find each relevant address if there is no address Id for a given row', async () => {
+      crmV2Connector.documents.getDocumentByRefAndDate.resolves({
+        addressId: undefined
+      });
+      await getRecipients(jobData);
+      expect(crmV2Connector.addresses.getAddress.callCount).to.equal(0);
+    });
+
+    test('schedules a message for each de-duped contact', async () => {
+      await getRecipients(jobData);
+      expect(scheduledNotificationsService.createScheduledNotification.callCount).to.equal(1);
+      const [row] = scheduledNotificationsService.createScheduledNotification.lastCall.args;
+
+      expect(Object.keys(row)).to.only.include([
+        '_messageRef',
+        '_eventId',
+        '_licences',
+        '_messageType',
+        '_personalisation']
+      );
+    });
+
+    test('sends a stop alert to a licence if it has a stop threshold and its a stop alert being issued', async () => {
+      await getRecipients(jobData);
+      const [row] = scheduledNotificationsService.createScheduledNotification.lastCall.args;
+
+      expect(row.messageRef).to.equal('water_abstraction_alert_stop');
+    });
+
+    test('sends a reduce alert to a licence if it has a reduce threshold and its a reduce alert being issued', async () => {
+      jobData.ev.metadata.options.sendingAlertType = 'reduce';
+      jobData.ev.metadata.options.linkages[0][0].alertType = 'reduce';
+      await getRecipients(jobData);
+      const [row] = scheduledNotificationsService.createScheduledNotification.lastCall.args;
+
+      expect(row.messageRef).to.equal('water_abstraction_alert_reduce');
+    });
+
+    test('sends a reduce or stop alert to a licence if it has a reduce or stop threshold and its a reduce alert being issued', async () => {
+      jobData.ev.metadata.options.sendingAlertType = 'reduce';
+      jobData.ev.metadata.options.linkages[0][0].alertType = 'stop_or_reduce';
+      await getRecipients(jobData);
+      const [row] = scheduledNotificationsService.createScheduledNotification.lastCall.args;
+
+      expect(row.messageRef).to.equal('water_abstraction_alert_reduce_or_stop');
+    });
+
+    test('sends a reduce or stop warning to a licence if it has a reduce or stop threshold and its a warning being issued', async () => {
+      jobData.ev.metadata.options.sendingAlertType = 'warning';
+      jobData.ev.metadata.options.linkages[0][0].alertType = 'stop_or_reduce';
+      await getRecipients(jobData);
+      const [row] = scheduledNotificationsService.createScheduledNotification.lastCall.args;
+
+      expect(row.messageRef).to.equal('water_abstraction_alert_reduce_or_stop_warning');
+    });
+
+    test('sends a reduce warning to a licence if it has a reduce threshold and its a warning being issued', async () => {
+      jobData.ev.metadata.options.sendingAlertType = 'warning';
+      jobData.ev.metadata.options.linkages[0][0].alertType = 'reduce';
+      await getRecipients(jobData);
+      const [row] = scheduledNotificationsService.createScheduledNotification.lastCall.args;
+
+      expect(row.messageRef).to.equal('water_abstraction_alert_reduce_warning');
+    });
+
+    test('sends a stop warning to a licence if it has a stop threshold and its a warning being issued', async () => {
+      jobData.ev.metadata.options.sendingAlertType = 'warning';
+      jobData.ev.metadata.options.linkages[0][0].alertType = 'stop';
+      await getRecipients(jobData);
+      const [row] = scheduledNotificationsService.createScheduledNotification.lastCall.args;
+
+      expect(row.messageRef).to.equal('water_abstraction_alert_stop_warning');
+    });
+
+    test('sends a resume notice to a licence if its a resume notice being issued', async () => {
+      jobData.ev.metadata.options.sendingAlertType = 'resume';
+      jobData.ev.metadata.options.linkages[0][0].alertType = 'stop_or_reduce';
+      await getRecipients(jobData);
+      const [row] = scheduledNotificationsService.createScheduledNotification.lastCall.args;
+
+      expect(row.messageRef).to.equal('water_abstraction_alert_resume');
+    });
   });
 
-  test('schedules a message for each de-duped contact', async () => {
-    await getRecipients(jobData);
-    expect(scheduledNotificationsService.createScheduledNotification.callCount).to.equal(1);
-    const [row] = scheduledNotificationsService.createScheduledNotification.lastCall.args;
+  experiment('for emails', () => {
+    const emails = [{ email: 'some@email.com' }, { email: 'some-other@email.com' }];
 
-    expect(Object.keys(row)).to.only.include([
-      '_messageRef',
-      '_eventId',
-      '_licences',
-      '_messageType',
-      '_personalisation']
-    );
-  });
-
-  test('sends a stop alert to a licence if it has a stop threshold and its a stop alert being issued', async () => {
-    await getRecipients(jobData);
-    const [row] = scheduledNotificationsService.createScheduledNotification.lastCall.args;
-
-    expect(row.messageRef).to.equal('water_abstraction_alert_stop');
-  });
-
-  test('sends a reduce alert to a licence if it has a reduce threshold and its a reduce alert being issued', async () => {
-    jobData.ev.metadata.options.sendingAlertType = 'reduce';
-    jobData.ev.metadata.options.linkages[0][0].alertType = 'reduce';
-    await getRecipients(jobData);
-    const [row] = scheduledNotificationsService.createScheduledNotification.lastCall.args;
-
-    expect(row.messageRef).to.equal('water_abstraction_alert_reduce');
-  });
-
-  test('sends a reduce or stop alert to a licence if it has a reduce or stop threshold and its a reduce alert being issued', async () => {
-    jobData.ev.metadata.options.sendingAlertType = 'reduce';
-    jobData.ev.metadata.options.linkages[0][0].alertType = 'stop_or_reduce';
-    await getRecipients(jobData);
-    const [row] = scheduledNotificationsService.createScheduledNotification.lastCall.args;
-
-    expect(row.messageRef).to.equal('water_abstraction_alert_reduce_or_stop');
-  });
-
-  test('sends a reduce or stop warning to a licence if it has a reduce or stop threshold and its a warning being issued', async () => {
-    jobData.ev.metadata.options.sendingAlertType = 'warning';
-    jobData.ev.metadata.options.linkages[0][0].alertType = 'stop_or_reduce';
-    await getRecipients(jobData);
-    const [row] = scheduledNotificationsService.createScheduledNotification.lastCall.args;
-
-    expect(row.messageRef).to.equal('water_abstraction_alert_reduce_or_stop_warning');
-  });
-
-  test('sends a reduce warning to a licence if it has a reduce threshold and its a warning being issued', async () => {
-    jobData.ev.metadata.options.sendingAlertType = 'warning';
-    jobData.ev.metadata.options.linkages[0][0].alertType = 'reduce';
-    await getRecipients(jobData);
-    const [row] = scheduledNotificationsService.createScheduledNotification.lastCall.args;
-
-    expect(row.messageRef).to.equal('water_abstraction_alert_reduce_warning');
-  });
-
-  test('sends a stop warning to a licence if it has a stop threshold and its a warning being issued', async () => {
-    jobData.ev.metadata.options.sendingAlertType = 'warning';
-    jobData.ev.metadata.options.linkages[0][0].alertType = 'stop';
-    await getRecipients(jobData);
-    const [row] = scheduledNotificationsService.createScheduledNotification.lastCall.args;
-
-    expect(row.messageRef).to.equal('water_abstraction_alert_stop_warning');
-  });
-
-  test('sends a resume notice to a licence if its a resume notice being issued', async () => {
-    jobData.ev.metadata.options.sendingAlertType = 'resume';
-    jobData.ev.metadata.options.linkages[0][0].alertType = 'stop_or_reduce';
-    await getRecipients(jobData);
-    const [row] = scheduledNotificationsService.createScheduledNotification.lastCall.args;
-
-    expect(row.messageRef).to.equal('water_abstraction_alert_resume');
+    beforeEach(async () => {
+      await crmV2Connector.companies.getCompanyWAAEmailContacts.resolves(emails);
+      await getRecipients(jobData);
+    });
+    test('calls the getCompanyWAAContacts method', () => {
+      expect(crmV2Connector.companies.getCompanyWAAEmailContacts.called).to.be.true();
+    });
+    test('calls scheduledNotificationService.createScheduledNotification as many times as there are email recipients', () => {
+      expect(scheduledNotificationsService.createScheduledNotification.callCount).to.equal(emails.length);
+    });
   });
 });
