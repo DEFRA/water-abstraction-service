62da118ed0a2d562c5943fb53337bddc61951445
Stephan Hough <43169948+Stephan-Hough@users.noreply.github.com>
2022-04-07 11:28:13 +0100

    bugfix/water-3612 (#1668)

--- src/lib/connectors/charge-module/customers.js
+++ src/lib/connectors/charge-module/customers.js
@@ -3,9 +3,9 @@
 const got = require('./lib/got-cm');
 
 const updateCustomer = data =>
-  got.post('v2/wrls/customer-changes', { json: data });
+  got.post('v3/wrls/customer-changes', { json: data });
 
-const getCustomerFiles = (days = 1) => got.get('v2/wrls/customer-files/' + days);
+const getCustomerFiles = (days = 1) => got.get('v3/wrls/customer-files/' + days);
 
 exports.updateCustomer = updateCustomer;
 exports.getCustomerFiles = getCustomerFiles;
