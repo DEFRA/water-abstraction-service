bf5944410cb653c8560d6ade28fb401fb5bd1e2a
Jonathan Goulding <jonathangoulding21@gmail.com>
2022-04-11 10:17:19 +0100

    fix: code coverage
    try change

--- src/logger.js
+++ src/logger.js
@@ -3,4 +3,24 @@ const { createLogger } = require('@envage/water-abstraction-helpers').logger;
 
 const logger = createLogger(config.logger);
 
+const newFunc = () => {
+  console.log('Empty func test coverage');
+};
+
+const newFunc2 = () => {
+  console.log('Empty func test coverage');
+};
+
+const newFunc3 = () => {
+  console.log('Empty func test coverage');
+};
+
+const newFunc4 = () => {
+  console.log('Empty func test coverage');
+};
+
 exports.logger = logger;
+exports.newFunc = newFunc;
+exports.newFunc2 = newFunc2;
+exports.newFunc3 = newFunc3;
+exports.newFunc4 = newFunc4;
