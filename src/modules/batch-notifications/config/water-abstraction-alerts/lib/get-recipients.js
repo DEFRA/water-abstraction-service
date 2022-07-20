29ab3e5b461d57f77ce4e0f8dd61a22da8d87af3
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2022-07-20 15:02:05 +0100

    Postal address issue - water abstraction alerts (#1730)
    https://eaflood.atlassian.net/browse/WATER-3717
    
    When creating a water abstraction alert, the alert will be sent by letter to the licence holder if no water abstraction alert contact has been added and opted in to receive email alerts.
    
    As part of the 'Create a water abstraction alert' process the postal address the alert is sent to is being cut down, removing lines 1 and 2 from the address.
    
    The addressMapper.pojoToModel was not mapping the first 4 lines of the address due to it expecting the address to be named differently.
    Switched to the crmToModel mapper as it does the mapping we require and works with the address in it's current format.
    
    We realised that the address wasn't being checked as part of the existing unit tests, so we add a test to ensure this is being handled correctly
    
    Co-authored-by: Stuart Adair <43574728+StuAA78@users.noreply.github.com>

--- src/modules/batch-notifications/config/water-abstraction-alerts/lib/get-recipients.js
+++ src/modules/batch-notifications/config/water-abstraction-alerts/lib/get-recipients.js
@@ -56,7 +56,8 @@ const getRecipients = async eventData => {
           condition_text: conditionText && conditionText.length > 0 ? `Effect of restriction: ${conditionText}` : '',
           ...notifyMapper.mapModelsToNotifyAddress({
             company: companyMapper.pojoToModel(licenceHolderCompany),
-            address: addressMapper.pojoToModel(licenceHolderAddress),
+            // Address is in CRM format when we receive it so we use a different mapper to the others
+            address: addressMapper.crmToModel(licenceHolderAddress),
             contact: contactMapper.pojoToModel(licenceContact)
           })
         }
