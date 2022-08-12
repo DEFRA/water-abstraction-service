d08292575ca9e22e2e50373a89f8c0dd48ef3670
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-08-12 16:19:44 +0100

    Add remaining workers to be registered
    What we are registering is based on the output we see when the foreground service starts up; QueueManager logs all the queues it creates, which previously would have also been all the workers.
    
    We took that list and used it to create our `RegisterWorkersService`. This specific change is us adding the remaining workers and shifting everything into alphabetical order.

--- src/lib/worker-manager/register-workers-service.js
+++ src/lib/worker-manager/register-workers-service.js
@@ -1,27 +1,39 @@
 'use strict'
 
-const returnsUpload = require('../../modules/returns/lib/jobs/start-upload')
-const returnsUploadToJson = require('../../modules/returns/lib/jobs/map-to-json')
-const valudateUploadedReturnsData = require('../../modules/returns/lib/jobs/validate-returns')
-const persistBulkReturns = require('../../modules/returns/lib/jobs/persist-returns')
+const billingApproveBatch = require('../../modules/billing/jobs/approve-batch')
+const billingChargeCategoriesSyncFromCsv = require('../../modules/billing/jobs/sync-charge-categories')
 const billingCreateBillRun = require('../../modules/billing/jobs/create-bill-run')
-const billingRebilling = require('../../modules/billing/jobs/rebilling')
 const billingCreateCharge = require('../../modules/billing/jobs/create-charge')
+const billingCustomerFileRefresh = require('../../modules/billing/jobs/customer-file-refresh')
+const billingDeleteErroredBatch = require('../../modules/billing/jobs/delete-errored-batch')
+const billingFindUpdateInvoiceAccounts = require('../../modules/billing/jobs/check-for-updated-invoice-accounts')
 const billingPopulateBatchChargeVersions = require('../../modules/billing/jobs/populate-batch-charge-versions')
 const billingPrepareTransactions = require('../../modules/billing/jobs/prepare-transactions')
-const billingProcessChargeVersionYear = require('../../modules/billing/jobs/process-charge-version-year')
 const billingProcessChargeVersions = require('../../modules/billing/jobs/process-charge-versions')
+const billingProcessChargeVersionYear = require('../../modules/billing/jobs/process-charge-version-year')
+const billingRebilling = require('../../modules/billing/jobs/rebilling')
 const billingRefreshTotals = require('../../modules/billing/jobs/refresh-totals')
-const billingTwoPartTariffMatching = require('../../modules/billing/jobs/two-part-tariff-matching')
-const billingChargeCategoriesSyncFromCsv = require('../../modules/billing/jobs/sync-charge-categories')
 const billingSupportedSourcesSyncFromCsv = require('../../modules/billing/jobs/sync-supported-sources')
-const billingCustomerFileRefresh = require('../../modules/billing/jobs/customer-file-refresh')
-const notificationsCheckStatus = require('../../modules/batch-notifications/lib/jobs/check-status')
-const notificationsSendMessage = require('../../modules/batch-notifications/lib/jobs/send-message')
-const notificationsRefreshEvent = require('../../modules/batch-notifications/lib/jobs/refresh-event')
-const gaugingStationsSyncFromCsv = require('../../modules/gauging-stations/jobs/sync-gauging-stations')
+const billingTwoPartTariffMatching = require('../../modules/billing/jobs/two-part-tariff-matching')
+const billingUpdateCustomerAccount = require('../../modules/billing/jobs/update-customer')
+const billingUpdateInvoices = require('../../modules/billing/jobs/update-invoices')
+const chargeInformationUploadStart = require('../../modules/charge-versions-upload/jobs/update-charge-information-start')
+const chargeInformationUploadToJson = require('../../modules/charge-versions-upload/jobs/update-charge-information-to-json')
 const gaugingStationsCopyLicenceGaugingStationsFromDigitise = require('../../modules/gauging-stations/jobs/sync-licence-gauging-stations-from-digitise')
 const gaugingStationsCopyLvpcFromDigitise = require('../../modules/gauging-stations/jobs/sync-licence-version-purpose-conditions-from-digitise')
+const gaugingStationsSyncFromCsv = require('../../modules/gauging-stations/jobs/sync-gauging-stations')
+const newLicenceVersion = require('../../modules/charge-versions/jobs/create-charge-version-workflows')
+const notificationsCheckStatus = require('../../modules/batch-notifications/lib/jobs/check-status')
+const notificationsGetRecipients = require('../../modules/batch-notifications/lib/jobs/get-recipients')
+const notificationsRefreshEvent = require('../../modules/batch-notifications/lib/jobs/refresh-event')
+const notificationsSendMessage = require('../../modules/batch-notifications/lib/jobs/send-message')
+const notifySend = require('../../modules/notify/lib/notify-send')
+const persistBulkReturns = require('../../modules/returns/lib/jobs/persist-returns')
+const returnsNotificationSend = require('../../modules/returns-notifications/lib/returns-notification-send')
+const returnsUpload = require('../../modules/returns/lib/jobs/start-upload')
+const returnsUploadToJson = require('../../modules/returns/lib/jobs/map-to-json')
+const updateChargeInformationSave = require('../../modules/charge-versions-upload/jobs/update-charge-information-save')
+const valudateUploadedReturnsData = require('../../modules/returns/lib/jobs/validate-returns')
 
 class RegisterWorkersService {
   static go (workerManager) {
@@ -32,28 +44,40 @@ class RegisterWorkersService {
 
   static _jobs () {
     return [
-      returnsUpload,
-      returnsUploadToJson,
-      valudateUploadedReturnsData,
-      persistBulkReturns,
+      billingApproveBatch,
+      billingChargeCategoriesSyncFromCsv,
       billingCreateBillRun,
-      billingRebilling,
       billingCreateCharge,
+      billingCustomerFileRefresh,
+      billingDeleteErroredBatch,
+      billingFindUpdateInvoiceAccounts,
       billingPopulateBatchChargeVersions,
       billingPrepareTransactions,
-      billingProcessChargeVersionYear,
       billingProcessChargeVersions,
+      billingProcessChargeVersionYear,
+      billingRebilling,
       billingRefreshTotals,
-      billingTwoPartTariffMatching,
-      billingChargeCategoriesSyncFromCsv,
       billingSupportedSourcesSyncFromCsv,
-      billingCustomerFileRefresh,
-      notificationsCheckStatus,
-      notificationsSendMessage,
-      notificationsRefreshEvent,
-      gaugingStationsSyncFromCsv,
+      billingTwoPartTariffMatching,
+      billingUpdateCustomerAccount,
+      billingUpdateInvoices,
+      chargeInformationUploadStart,
+      chargeInformationUploadToJson,
       gaugingStationsCopyLicenceGaugingStationsFromDigitise,
-      gaugingStationsCopyLvpcFromDigitise
+      gaugingStationsCopyLvpcFromDigitise,
+      gaugingStationsSyncFromCsv,
+      newLicenceVersion,
+      notificationsCheckStatus,
+      notificationsGetRecipients,
+      notificationsRefreshEvent,
+      notificationsSendMessage,
+      notifySend,
+      persistBulkReturns,
+      returnsNotificationSend,
+      returnsUpload,
+      returnsUploadToJson,
+      updateChargeInformationSave,
+      valudateUploadedReturnsData
     ]
   }
 }
