444155de34289358ab60cfe0940bf3c280f03f97
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2022-08-26 11:05:41 +0100

    addeded Stuarts magic to the test

--- test/modules/address-search/controller.test.js
+++ test/modules/address-search/controller.test.js
@@ -10,7 +10,6 @@ const {
 const { expect } = require('@hapi/code')
 const sandbox = require('sinon').createSandbox()
 
-const Address = require('../../../src/lib/models/address')
 const controller = require('../../../src/modules/address-search/controller')
 
 const addressService = require('../../../src/modules/address-search/services/address-service')
@@ -27,21 +26,12 @@ experiment('modules/application-state/controller', () => {
       request = {
         query: { q: 'TT1 1TT' }
       }
-      sandbox.stub(addressService, 'getAddressesByPostcode').resolves({
-        data: [
-          new Address()
-        ]
-      })
+      sandbox.stub(addressService, 'getAddressesByPostcode').resolvesArg(0)
       result = await controller.getAddressSearch(request)
     })
 
-    test('fetches the data via the address service', async () => {
-      expect(addressService.getAddressesByPostcode.calledWith('TT1 1TT')).to.be.true()
-    })
-
     test('returns the data', async () => {
-      expect(result.data).to.be.an.array().length(1)
-      expect(result.data[0] instanceof Address).to.be.true()
+      expect(result).to.equal('TT1 1TT')
     })
   })
 })
