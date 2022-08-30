2fdd4b27312bc7ff4d1c97a8abc6eb53f8ad46e8
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-08-30 11:56:32 +0100

    Add unit tests for `notify/callback...
    ...because we didn't have any. Yeah!

--- test/controllers/notify.test.js
+++ test/controllers/notify.test.js
@@ -15,6 +15,7 @@ const nock = require('nock')
 
 const { server, start } = require('../../index.js')
 const { logger } = require('../../src/logger')
+const scheduledNotificationsService = require('../../src/lib/services/scheduled-notifications')
 
 const sinon = require('sinon')
 const sandbox = sinon.createSandbox()
@@ -111,7 +112,7 @@ experiment('Notify controller', () => {
       before(() => {
         nock('https://api.notifications.service.gov.uk:443')
           .post('/v2/template/8ac8a279-bf93-44da-b536-9b05703cb928/preview')
-          .reply(200, { body: 'It has a test value of 00/00/00/00', type: 'email'})
+          .reply(200, { body: 'It has a test value of 00/00/00/00', type: 'email' })
 
         request = createRequest('/water/1.0/notifyLater/unit_test_email')
       })
@@ -124,7 +125,7 @@ experiment('Notify controller', () => {
     })
 
     experiment('when the request is invalid', () => {
-      test.only('we return a 400 response', async () => {
+      test('we return a 400 response', async () => {
         nock('https://api.notifications.service.gov.uk:443')
           .post('/v2/template/abcd/preview')
           .reply(400, { errors: [{ error: 'ValidationError', message: 'id is not a valid UUID' }], status_code: 400 })
@@ -134,6 +135,78 @@ experiment('Notify controller', () => {
         const res = await server.inject(request)
 
         expect(res.statusCode).to.equal(400)
+      })
+    })
+  })
+
+  experiment('notify/callback', () => {
+    let request
+
+    const createRequest = (url) => {
+      return {
+        method: 'POST',
+        url,
+        payload: {
+          id: '9b23538e-c493-4b4f-baa7-2a76f643aa1f',
+          reference: 'foobar',
+          to: 'stuart@example.com',
+          status: 'delivered',
+          created_at: '2022-08-09T10:10:31+0000',
+          completed_at: '2022-08-09T10:10:31+0000',
+          sent_at: '2022-08-09T10:10:31+0000',
+          notification_type: 'email',
+          template_id: '8ac8a279-bf93-44da-b536-9b05703cb928',
+          template_version: 2
+        },
+        headers: {
+          Authorization: process.env.JWT_TOKEN
+        }
+      }
+    }
+
+    beforeEach(async () => {
+      sandbox.stub(scheduledNotificationsService, 'updateScheduledNotificationWithNotifyCallback')
+
+      request = createRequest('/water/1.0/notify/callback')
+    })
+
+    afterEach(() => {
+      nock.cleanAll()
+    })
+
+    experiment('when the request is valid', () => {
+      beforeEach(() => {
+        sandbox.stub(scheduledNotificationsService, 'getScheduledNotificationByNotifyId').resolves({ id: '123' })
+      })
+
+      test('we return a 204 response', async () => {
+        const res = await server.inject(request)
+
+        expect(res.statusCode).to.equal(204)
+      })
+    })
+
+    experiment('when the request is invalid', () => {
+      beforeEach(() => {
+        sandbox.stub(scheduledNotificationsService, 'getScheduledNotificationByNotifyId').resolves(null)
+      })
+
+      test('we return a 404 response', async () => {
+        const res = await server.inject(request)
+
+        expect(res.statusCode).to.equal(404)
+      })
+    })
+
+    experiment('when an error occurs', () => {
+      beforeEach(() => {
+        sandbox.stub(scheduledNotificationsService, 'getScheduledNotificationByNotifyId').throws()
+      })
+
+      test('we return a 500 response', async () => {
+        const res = await server.inject(request)
+
+        expect(res.statusCode).to.equal(500)
       })
     })
   })
@@ -305,7 +378,7 @@ experiment('Notify controller', () => {
       expect(res.statusCode).to.equal(200)
     })
 
-    test.only('The API should throw an error when invalid date is supplied when scheduling a message', async () => {
+    test('The API should throw an error when invalid date is supplied when scheduling a message', async () => {
       const request = {
         method: 'POST',
         url: '/water/1.0/notifyLater/unit_test_email',
