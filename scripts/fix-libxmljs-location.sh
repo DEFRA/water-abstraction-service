1be4df3f59318eea450e9814573e46d4d8a69338
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2022-09-09 09:31:56 +0100

    Fix libxmljs update issue (#1812)
    The latest version of libxmljs 0.19.10 does not put the binary in the correct location when installing locally.
    
    A postinstall script has been added to check if the binary is in the correct location and to move it if it isn't.

---
+++ scripts/fix-libxmljs-location.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# There is an issue with the package libxmljs version 0.19.10 where the binary isn't placed in the correct location
+# when running locally. This script fixes that issue
+
+echo "Checking if we need to move the libxmljs binary"
+if [ -e ./node_modules/libxmljs/build/Release/xmljs.node ]
+  then
+    echo "Binary is found, nothing to do"
+  else
+    cp ./node_modules/libxmljs/build/Release/obj.target/xmljs.node ./node_modules/libxmljs/build/Release/xmljs.node
+    echo "Copied binary"
+fi
