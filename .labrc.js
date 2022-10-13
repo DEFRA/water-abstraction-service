6b0139dcd98a70788c83571bddc950360808323e
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2022-10-13 17:44:57 +0100

    fixed unit test memory leak
    Solution for fix found here https://github.com/hapijs/lab/issues/928

--- .labrc.js
+++ .labrc.js
@@ -17,7 +17,7 @@ module.exports = {
     '__decorate','__param','__metadata','__awaiter','__generator','__exportStar','__createBinding','__values','__read',
     '__spread','__spreadArrays','__await','__asyncGenerator','__asyncDelegator','__asyncValues','__makeTemplateObject',
     '__importStar','__importDefault','__classPrivateFieldGet','__classPrivateFieldSet','__core-js_shared__','CSS',
-    'regeneratorRuntime','core','__spreadArray','global-symbol-property'
+    'regeneratorRuntime','core','__spreadArray','global-symbol-property','__classPrivateFieldIn'
   ].join(','),
   'coverage-exclude': ['integration-tests']
 };
