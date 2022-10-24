65d3ea9c19b42b8018919e42d641fd8b2730b251
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2022-10-24 15:45:55 +0100

    added unit tests for health page

---
+++ test/modules/health/controller.test.js
@@ -0,0 +1,23 @@
+const { test, experiment, before } = exports.lab = require('@hapi/lab').script()
+const { expect } = require('@hapi/code')
+
+const controller = require('../../../src/modules/health/controller')
+const pkg = require('../../../package.json')
+
+experiment.only('modules/service-status/controller', () => {
+  experiment('.getStatus', () => {
+    let status
+
+    before(async () => {
+      status = await controller.getStatus()
+    })
+
+    test('contains the expected water service version', async () => {
+      expect(status.version).to.equal(pkg.version)
+    })
+
+    test('contains the git commit hash', async () => {
+      expect(status.commit).to.exist()
+    })
+  })
+})
