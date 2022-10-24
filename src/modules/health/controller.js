00a5261c1494dfd9bcf0e677c45b7e5df44bfb3d
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2022-10-24 15:54:10 +0100

    Create health status endpoint (#1862)
    https://github.com/DEFRA/water-abstraction-system/pull/8
    
    Creating a new endpoint for use by the System to display health data. Currently this will be version and git commit hash

---
+++ src/modules/health/controller.js
@@ -0,0 +1,23 @@
+// We use promisify to wrap exec in a promise. This allows us to await it without resorting to using callbacks.
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+
+const pkg = require('../../../package.json')
+
+const _getCommitHash = async () => {
+  try {
+    const { stdout, stderr } = await exec('git rev-parse HEAD')
+    return stderr ? `ERROR: ${stderr}` : stdout.replace('\n', '')
+  } catch (error) {
+    return `ERROR: ${error.message}`
+  }
+}
+
+const getStatus = async () => {
+  return {
+    version: pkg.version,
+    commit: await _getCommitHash()
+  }
+}
+
+exports.getStatus = getStatus
