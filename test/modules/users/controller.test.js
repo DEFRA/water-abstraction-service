4ce73ab440b76de575e17f663f31d0878a2be3ff
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-02-13 14:05:37 +0000

    Fix no contacts in external login (#2014)
    https://github.com/DEFRA/water-abstraction-team/issues/25
    
    A recent change as part of our efforts to remove the code base's dependency on Lodash has introduced an error. The Lodash `get()` method doesn't care if the object you are getting a property from is undefined. It will just return whatever default value you provide.
    
    Our issue is that the existing code nor the tests gave any clue this might be the case. So, though we test for the property `name` we were assuming `doc.metadata.contacts[0]` would always be defined.
    
    Turns out this is not the case. So, this change fixes the issue and prevents an error from being thrown.
    
    For reference the error we saw was `Uncaught TypeError: Cannot read property '0' of undefined`.

--- test/modules/users/controller.test.js
+++ test/modules/users/controller.test.js
@@ -112,11 +112,7 @@ const getDocumentHeaderResponse = () => ({
       document_id: 'lic_3_document_id',
       system_external_id: '03/456',
       metadata: {
-        Name: 'Max Irrigation',
-        contacts: [
-          { name: 'Max Irrigation LH', role: 'Licence holder' },
-          { name: 'Max Irrigation Other', role: 'other role' }
-        ]
+        Name: 'Max Irrigation'
       },
       company_entity_id: 'max_irrigation_id'
     }
@@ -352,7 +348,7 @@ experiment('modules/users/controller', () => {
           {
             documentId: 'lic_3_document_id',
             licenceRef: '03/456',
-            licenceHolder: 'Max Irrigation LH',
+            licenceHolder: '',
             licence: licences[2]
           }
         ])
