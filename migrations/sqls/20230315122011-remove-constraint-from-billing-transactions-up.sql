661e7c42b718c6fb243fa31fb90f99f8daaf2f88
Jason Claxton <jason.claxton@defra.gov.uk>
2023-03-15 12:49:31 +0000

    Fix drop command

--- migrations/sqls/20230315122011-remove-constraint-from-billing-transactions-up.sql
+++ migrations/sqls/20230315122011-remove-constraint-from-billing-transactions-up.sql
@@ -1,2 +1,2 @@
 /* Replace with your SQL commands */
-ALTER TABLE water.billing_transactions DROP CONSTRAINT IF EXISTS billing_invoice_licence_id;
+ALTER TABLE water.billing_transactions DROP CONSTRAINT IF EXISTS billing_transactions_billing_invoice_licence_id_fkey;
