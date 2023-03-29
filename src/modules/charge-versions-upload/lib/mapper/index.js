6fc53b1d7c45375c35aa420585287551a2c541b3
Rebecca Ransome <beckyrose200@aol.com>
2023-03-29 09:31:31 +0100

    Refactor Lodash code

--- src/modules/charge-versions-upload/lib/mapper/index.js
+++ src/modules/charge-versions-upload/lib/mapper/index.js
@@ -11,6 +11,20 @@ const mapToRowObject = headers => row => row.reduce((acc, column, index) => {
   return { ...acc, [headers[index]]: column }
 }, {})
 
+// sorting the rows by licenceNumber first and then chargeReferenceDetailsChargeElementGroup
+const sortBy = (row) => {
+  return row.sort((a, b) => {
+    if (a.licenceNumber !== b.licenceNumber) {
+      return a.licenceNumber < b.licenceNumber ? -1 : 1
+    }
+    if (a.chargeReferenceDetailsChargeElementGroup !== b.chargeReferenceDetailsChargeElementGroup) {
+      return a.chargeReferenceDetailsChargeElementGroup < b.chargeReferenceDetailsChargeElementGroup ? -1 : 1
+    }
+
+    return 0
+  })
+}
+
 /**
  * Maps a CSV file in string form to an array of return objects
  * @param  {String}  csvStr - CSV file in string form
@@ -33,19 +47,8 @@ const mapCsv = async (csvStr, user, event) => {
 
   // Convert the fields in the csv rows into key value pairs with the csv headings as the keys converted to camel case.
   // Then sort by the licence number and group.
-  const result = rows.map(mapToRowObject(headers.map(header => toCamelCase(header))))
-
-  // sorting the rows by licenceNumber first and then chargeReferenceDetailsChargeElementGroup
-  const rowObjects = result.sort((a, b) => {
-    if (a.licenceNumber !== b.licenceNumber) {
-      return a.licenceNumber < b.licenceNumber ? -1 : 1
-    }
-    if (a.chargeReferenceDetailsChargeElementGroup !== b.chargeReferenceDetailsChargeElementGroup) {
-      return a.chargeReferenceDetailsChargeElementGroup < b.chargeReferenceDetailsChargeElementGroup ? -1 : 1
-    }
-
-    return 0
-  })
+  const mappedRows = rows.map(mapToRowObject(headers.map(header => toCamelCase(header))))
+  const rowObjects = sortBy(mappedRows)
 
   const chargeVersions = []
   let groupByLicenceNumber = []
