4c48b7bde7dae0cb681e1e7fac03ddc8b831889a
Rebecca Ransome <beckyrose200@aol.com>
2023-09-21 12:15:25 +0100

    Fix script

--- migrations/sqls/20230921102640-delete-broken-charge-versions-up.sql
+++ migrations/sqls/20230921102640-delete-broken-charge-versions-up.sql
@@ -1,7 +1,7 @@
 /* deletes charge versions in the workflow that have incorrect data */
 CREATE TEMP TABLE tempChargeVersionId
 AS (
-    SELECT charge_version_id AS id
+    SELECT charge_version_workflow_id AS id
     FROM water.charge_version_workflows
     WHERE charge_version_workflow_id = '4fe385f1-a50c-4608-91ee-96a3b15ad3b6'
 );
