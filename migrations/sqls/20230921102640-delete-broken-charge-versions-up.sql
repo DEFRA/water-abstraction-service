997bd4b7a60fd73b7c525b5373e8bdd96ddf9dab
Becky Ransome <91424856+Beckyrose200@users.noreply.github.com>
2023-09-21 14:52:48 +0100

    Migration to delete broken charge version (#2284)
    https://eaflood.atlassian.net/browse/WATER-4147
    
    This is a migration to delete a broken charge version record that doesn't have a start date.

---
+++ migrations/sqls/20230921102640-delete-broken-charge-versions-up.sql
@@ -0,0 +1,3 @@
+/* deletes charge versions in the workflow that have incorrect data */
+
+DELETE FROM water.charge_version_workflows WHERE charge_version_workflow_id = '4fe385f1-a50c-4608-91ee-96a3b15ad3b6';
