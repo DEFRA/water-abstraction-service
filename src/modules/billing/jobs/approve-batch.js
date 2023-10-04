d8f41476302b52ae1a7fa23158c0c137ab6ee89a
Becky Ransome <91424856+Beckyrose200@users.noreply.github.com>
2023-10-04 09:24:16 +0100

    Improve Bill Run Queue Handling to Prevent Duplicate Jobs (#2293)
    https://eaflood.atlassian.net/browse/WATER-4100
    
    In our current codebase, we utilize BullMQ for queuing bill-run jobs, with each bill run assigned a unique ID that doubles as the job ID within the queue. BullMQ's behavior prevents the existence of two jobs with the same ID in the queue. Initially, we believed that double-clicking the "Send to Bill Run" button would result in the second request being ignored, as the subsequent job would share the same ID and BullMQ would naturally dismiss it.
    
    However, upon closer examination, we realized that the job processes on the queue with remarkable speed, allowing it to be removed from the queue before the same job can re-enter when the user inadvertently double-clicks the button. This rapid processing has led to bill runs getting stuck, as the queue attempts to process the second request, which triggers a conflict request in the charging module (due to the bill having already been successfully billed). Consequently, the approveBatch function encounters an error, causing the status to revert from 'sent' to 'ready'.
    
    This pull request addresses the issue by changing the configuration of the queue, specifically for the approveBatch queue.
    This PR is changing the settings to maintain a record of completed bill-run jobs and remove any jobs that have failed. Keeping completed jobs in the queue allows BullMQ to identify duplicate bill runs and ignore them instead of processing them. Additionally, removing failed jobs enables the bill run to re-enter the queue if it fails without being flagged as a duplicate job.

--- src/modules/billing/jobs/approve-batch.js
+++ src/modules/billing/jobs/approve-batch.js
@@ -4,17 +4,21 @@ const JOB_NAME = 'billing.approve-batch'
 
 const batchService = require('../services/batch-service')
 const batchJob = require('./lib/batch-job')
-const { logger } = require('../../../logger')
 const { jobName: refreshTotalsJobName } = require('./refresh-totals')
+const helpers = require('./lib/helpers')
 
 const createMessage = (batchId, user) => ([
+// We set `removeOnComplete` to 500 to briefly retain completed bill run jobs, preventing duplicate bill runs,
+// while `removeOnFail` is set to true to prevent bill runs from becoming stuck on the queue, allowing for re-runs.
   JOB_NAME,
   {
     batchId,
     user
   },
   {
-    jobId: `${JOB_NAME}.${batchId}`
+    jobId: `${JOB_NAME}.${batchId}`,
+    removeOnComplete: 500,
+    removeOnFail: true
   }
 ])
 
@@ -40,22 +44,8 @@ const onComplete = async (job, queueManager) => {
   }
 }
 
-const onFailedHandler = async (job, err) => {
-  // If a previous attempt to send the bill run has been tried but errored the logic in
-  // src/modules/billing/services/batch-service.js approveBatch() will reset the status of the bill run back to READY.
-  // We have found that subsequent attempts to send the bill run cause it to become 'stuck' This is because it seems
-  // the failed job is left in the queue. As the job ID is based on the billing batch ID when we add further approve
-  // jobs for this bill run BullMQ is ignoring them. It sees them as duplicates because they have the same key as a
-  // job already in the queue.
-  //
-  // To allow further attempts to happen we call 'remove()' on the job if it fails. This will remove the
-  // the job from the queue and allow a new job with the same ID to be processed.
-  logger.error(`Job ${job.name} ${job.id} failed`, err.stack)
-  await job.remove()
-}
-
 exports.jobName = JOB_NAME
 exports.createMessage = createMessage
 exports.handler = handler
 exports.onComplete = onComplete
-exports.onFailed = onFailedHandler
+exports.onFailed = helpers.onFailedHandler
