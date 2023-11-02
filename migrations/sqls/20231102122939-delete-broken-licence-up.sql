c744fd8287131b6392841b7ca1e4c2e0ffbcf2d0
Rebecca Ransome <beckyrose200@aol.com>
2023-11-02 14:37:38 +0000

    Refactor

--- migrations/sqls/20231102122939-delete-broken-licence-up.sql
+++ migrations/sqls/20231102122939-delete-broken-licence-up.sql
@@ -1,18 +1,18 @@
--- crm_v2
+/* This query is deleting a bad licence data that has a carriage return at the end of it */
+
 DELETE FROM crm_v2.document_roles dr WHERE dr.document_id IN (
   SELECT document_id FROM crm_v2.documents d WHERE d.document_ref LIKE 'NW/072/0417/002/R01' || CHR(13)
 );
 
 DELETE FROM crm_v2.documents d WHERE d.document_ref LIKE 'NW/072/0417/002/R01' || CHR(13);
--- crm schema
+
 DELETE FROM crm.document_header dh WHERE dh.system_external_id LIKE 'NW/072/0417/002/R01' || CHR(13);
 
--- Permit Schema
 DELETE FROM permit.licence l WHERE l.licence_ref LIKE 'NW/072/0417/002/R01' || CHR(13);
--- Returns Schema
+
 DELETE FROM "returns"."returns" r  WHERE r.licence_ref LIKE 'NW/072/0417/002/R01' || CHR(13);
 
--- Water Schema
+
 DELETE FROM water.licence_version_purpose_conditions lvpc WHERE lvpc.licence_version_purpose_id IN (
   SELECT lvp.licence_version_purpose_id FROM water.licence_version_purposes lvp WHERE lvp.licence_version_id IN (
     SELECT lv.licence_version_id FROM water.licence_versions lv WHERE lv.licence_id IN (
