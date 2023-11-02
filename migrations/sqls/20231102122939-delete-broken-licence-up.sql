44ae378d644ed96cea1f5eb0b66080a0df310137
Rebecca Ransome <beckyrose200@aol.com>
2023-11-02 14:43:08 +0000

    Refactor

--- migrations/sqls/20231102122939-delete-broken-licence-up.sql
+++ migrations/sqls/20231102122939-delete-broken-licence-up.sql
@@ -1,52 +1,52 @@
 /* This query is deleting a bad licence data that has a carriage return at the end of it */
 
-DELETE FROM crm_v2.document_roles dr WHERE dr.document_id IN (
-  SELECT document_id FROM crm_v2.documents d WHERE d.document_ref LIKE 'NW/072/0417/002/R01' || CHR(13)
+DELETE FROM crm_v2.document_roles WHERE document_id IN (
+  SELECT document_id FROM crm_v2.documents WHERE document_ref LIKE 'NW/072/0417/002/R01' || CHR(13)
 );
 
-DELETE FROM crm_v2.documents d WHERE d.document_ref LIKE 'NW/072/0417/002/R01' || CHR(13);
+DELETE FROM crm_v2.documents WHERE document_ref LIKE 'NW/072/0417/002/R01' || CHR(13);
 
-DELETE FROM crm.document_header dh WHERE dh.system_external_id LIKE 'NW/072/0417/002/R01' || CHR(13);
+DELETE FROM crm.document_header WHERE system_external_id LIKE 'NW/072/0417/002/R01' || CHR(13);
 
-DELETE FROM permit.licence l WHERE l.licence_ref LIKE 'NW/072/0417/002/R01' || CHR(13);
+DELETE FROM permit.licence WHERE licence_ref LIKE 'NW/072/0417/002/R01' || CHR(13);
 
-DELETE FROM "returns"."returns" r  WHERE r.licence_ref LIKE 'NW/072/0417/002/R01' || CHR(13);
+DELETE FROM "returns"."returns" WHERE licence_ref LIKE 'NW/072/0417/002/R01' || CHR(13);
 
 
-DELETE FROM water.licence_version_purpose_conditions lvpc WHERE lvpc.licence_version_purpose_id IN (
-  SELECT lvp.licence_version_purpose_id FROM water.licence_version_purposes lvp WHERE lvp.licence_version_id IN (
-    SELECT lv.licence_version_id FROM water.licence_versions lv WHERE lv.licence_id IN (
-      SELECT licence_id FROM water.licences l WHERE l.licence_ref LIKE 'NW/072/0417/002/R01' || CHR(13)
+DELETE FROM water.licence_version_purpose_conditions WHERE licence_version_purpose_id IN (
+  SELECT licence_version_purpose_id FROM water.licence_version_purposes WHERE licence_version_id IN (
+    SELECT licence_version_id FROM water.licence_versions WHERE licence_id IN (
+      SELECT licence_id FROM water.licences WHERE licence_ref LIKE 'NW/072/0417/002/R01' || CHR(13)
     )
   )
 );
 
-DELETE FROM water.licence_version_purposes lvp WHERE lvp.licence_version_id IN (
-  SELECT lv.licence_version_id FROM water.licence_versions lv WHERE lv.licence_id IN (
-    SELECT licence_id FROM water.licences l WHERE l.licence_ref LIKE 'NW/072/0417/002/R01' || CHR(13)
+DELETE FROM water.licence_version_purposes WHERE licence_version_id IN (
+  SELECT licence_version_id FROM water.licence_versions WHERE licence_id IN (
+    SELECT licence_id FROM water.licences WHERE licence_ref LIKE 'NW/072/0417/002/R01' || CHR(13)
   )
 );
 
-DELETE FROM water.licence_versions lv WHERE lv.licence_id IN (
-  SELECT licence_id FROM water.licences l WHERE l.licence_ref LIKE 'NW/072/0417/002/R01' || CHR(13)
+DELETE FROM water.licence_versions WHERE licence_id IN (
+  SELECT licence_id FROM water.licences WHERE licence_ref LIKE 'NW/072/0417/002/R01' || CHR(13)
 )
 
-DELETE FROM water.return_requirement_purposes rrp WHERE rrp.return_requirement_id IN (
-  SELECT rr.return_requirement_id  FROM water.return_requirements rr WHERE rr.return_version_id IN (
-    SELECT rv.return_version_id FROM water.return_versions rv WHERE rv.licence_id IN (
-      SELECT licence_id FROM water.licences l WHERE l.licence_ref LIKE 'NW/072/0417/002/R01' || CHR(13)
+DELETE FROM water.return_requirement_purposes WHERE return_requirement_id IN (
+  SELECT return_requirement_id  FROM water.return_requirements WHERE return_version_id IN (
+    SELECT return_version_id FROM water.return_versions WHERE licence_id IN (
+      SELECT licence_id FROM water.licences WHERE licence_ref LIKE 'NW/072/0417/002/R01' || CHR(13)
     )
   )
 );
 
-DELETE FROM water.return_requirements rr WHERE rr.return_version_id IN (
-  SELECT return_version_id FROM water.return_versions rv WHERE rv.licence_id IN(
-    SELECT licence_id FROM water.licences l WHERE l.licence_ref LIKE 'NW/072/0417/002/R01' || CHR(13)
+DELETE FROM water.return_requirements WHERE return_version_id IN (
+  SELECT return_version_id FROM water.return_versions WHERE licence_id IN(
+    SELECT licence_id FROM water.licences WHERE licence_ref LIKE 'NW/072/0417/002/R01' || CHR(13)
   )
 );
 
-DELETE FROM water.return_versions rv WHERE rv.licence_id IN (
-  SELECT licence_id FROM water.licences l WHERE l.licence_ref LIKE 'NW/072/0417/002/R01' || CHR(13)
+DELETE FROM water.return_versions WHERE licence_id IN (
+  SELECT licence_id FROM water.licences WHERE licence_ref LIKE 'NW/072/0417/002/R01' || CHR(13)
 );
 
-DELETE FROM water.licences l WHERE licence_ref LIKE 'NW/072/0417/002/R01' || CHR(13);
+DELETE FROM water.licences WHERE licence_ref LIKE 'NW/072/0417/002/R01' || CHR(13);
