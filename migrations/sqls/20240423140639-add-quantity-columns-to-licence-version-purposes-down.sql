642ca21435bacb8599ccedd24b8a072d9074a682
Robert Parkinson <robert.parkinson@defra.gov.uk>
2024-04-25 10:45:20 +0100

    add abstraction data columns to licence-version-purposes

---
+++ migrations/sqls/20240423140639-add-quantity-columns-to-licence-version-purposes-down.sql
@@ -0,0 +1,4 @@
+ALTER TABLE water.licence_version_purposes
+    DROP column instant_quantity;
+    DROP column hourly_quantity;
+    DROP column daily_quantity;
---
+++ migrations/sqls/20240423140639-add-quantity-columns-to-licence-version-purposes-up.sql
@@ -0,0 +1,4 @@
+ALTER TABLE water.licence_version_purposes
+    ADD column instant_quantity numeric;
+    ADD column hourly_quantity numeric;
+    ADD column daily_quantity numeric;
