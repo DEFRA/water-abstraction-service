e65f8e7968811e6b43c1c31c22f4bf8b8b8d116e
Robert Parkinson <robert.parkinson@defra.gov.uk>
2024-04-25 13:27:57 +0100

    add comas instead of semicolons

--- migrations/sqls/20240423140639-add-quantity-columns-to-licence-version-purposes-down.sql
+++ migrations/sqls/20240423140639-add-quantity-columns-to-licence-version-purposes-down.sql
@@ -1,4 +1,4 @@
 ALTER TABLE water.licence_version_purposes
-    DROP column instant_quantity;
-    DROP column hourly_quantity;
+    DROP column instant_quantity,
+    DROP column hourly_quantity,
     DROP column daily_quantity;
