1b8cc2ea2cf0ca27ef9ed2c837acdfadb24ca016
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-06-05 17:45:25 +0100

    Remove column correction
    It had already been dealt with.

--- migrations/sqls/20240605155346-alter-return-requirement-points-down.sql
+++ migrations/sqls/20240605155346-alter-return-requirement-points-down.sql
@@ -4,6 +4,4 @@ BEGIN;
 
 ALTER TABLE IF EXISTS water.return_requirement_points DROP COLUMN nald_point_id;
 
-ALTER TABLE IF EXISTS water.return_requirements RENAME COLUMN fifty_six_exception TO fixty_six_exception;
-
 COMMIT;
--- migrations/sqls/20240605155346-alter-return-requirement-points-up.sql
+++ migrations/sqls/20240605155346-alter-return-requirement-points-up.sql
@@ -8,9 +8,6 @@
   The submissions (returns.returns) link to `water.return_requirements` which in turn is linked to
   `water.return_requirement_points`. If that table contains the NALD point ID then they should be able to link
   submissions to specific points.
-
-  We also missed in our tidy up of that return requirement tables that one of the fields we added has a typo. So, we
-  fix that as well.
 */
 
 BEGIN;
@@ -21,8 +18,5 @@ ALTER TABLE IF EXISTS water.return_requirement_points ADD COLUMN nald_point_id I
 UPDATE water.return_requirement_points SET nald_point_id = split_part(external_id, ':', 3)::int;
 -- Now apply the NOT NULL constraint so we can ensure it is always populated in future
 ALTER TABLE IF EXISTS water.return_requirement_points ALTER COLUMN nald_point_id  SET NOT NULL;
-
--- Correct the column name typo
-ALTER TABLE IF EXISTS water.return_requirements RENAME COLUMN fixty_six_exception TO fifty_six_exception;
 
 COMMIT;
