d3b62ac84cbf75a7e9d643afb5482ad761e1288b
Rebecca Ransome <beckyrose200@aol.com>
2024-08-12 16:37:35 +0100

    Update comment

--- src/lib/connectors/system/supplementary-billing.js
+++ src/lib/connectors/system/supplementary-billing.js
@@ -4,7 +4,7 @@ const config = require('../../../../config')
 const { serviceRequest } = require('@envage/water-abstraction-helpers')
 
 /**
- * Posts to
+ * Posts to system repo to flag new charge versions for SROC supplementary billing
  * @return {Promise}
  */
 const flagSupplementaryBilling = (chargeVersionId) => {
