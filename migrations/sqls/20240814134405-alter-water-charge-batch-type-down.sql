f6deb1c7447b11ddc52328e6dfb074727cf87954
Jason Claxton <jason.claxton@defra.gov.uk>
2024-08-14 15:22:04 +0100

    Create migration to update `water.charge_batch_type`

---
+++ migrations/sqls/20240814134405-alter-water-charge-batch-type-down.sql
@@ -0,0 +1,3 @@
+/*
+  It is not possible to remove the new batch type tpt_supplementary once it has been added
+*/
---
+++ migrations/sqls/20240814134405-alter-water-charge-batch-type-up.sql
@@ -0,0 +1,9 @@
+/*
+  Alters the charge_batch_type data type to add the new batch type tpt_supplementary
+*/
+
+BEGIN;
+
+ALTER TYPE water.charge_batch_type ADD VALUE 'tpt_supplementary';
+
+COMMIT;
