da32ffc5e9b5e9c3eb4032da3ce7a3632d46029f
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2024-08-14 12:08:20 +0100

    Update migrations/sqls/20240814102304-alter-licence-version-created-and-updated-at-down.sql
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- migrations/sqls/20240814102304-alter-licence-version-created-and-updated-at-down.sql
+++ migrations/sqls/20240814102304-alter-licence-version-created-and-updated-at-down.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-ALTER TABLE IF EXISTS water.licence_versions ALTER COLUMN date_created SET NOT NULL;
-ALTER TABLE IF EXISTS water.licence_versions ALTER COLUMN date_updated SET NOT NULL;
+ALTER TABLE IF EXISTS water.licence_versions ALTER COLUMN date_created DROP DEFAULT;
+ALTER TABLE IF EXISTS water.licence_versions ALTER COLUMN date_updated DROP DEFAULT;
 
 COMMIT;
