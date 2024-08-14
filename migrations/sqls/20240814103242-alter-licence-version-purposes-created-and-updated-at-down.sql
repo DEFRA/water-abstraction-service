52c74badeadb812f3eedf3aae3da4c6c87773907
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2024-08-14 12:08:14 +0100

    Update migrations/sqls/20240814103242-alter-licence-version-purposes-created-and-updated-at-down.sql
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- migrations/sqls/20240814103242-alter-licence-version-purposes-created-and-updated-at-down.sql
+++ migrations/sqls/20240814103242-alter-licence-version-purposes-created-and-updated-at-down.sql
@@ -1,7 +1,7 @@
 /* Replace with your SQL commands */
 BEGIN;
 
-ALTER TABLE IF EXISTS water.licence_version_purposes ALTER COLUMN date_created SET NOT NULL;
-ALTER TABLE IF EXISTS water.licence_version_purposes ALTER COLUMN date_updated SET NOT NULL;
+ALTER TABLE IF EXISTS water.licence_version_purposes ALTER COLUMN date_created DROP DEFAULT;
+ALTER TABLE IF EXISTS water.licence_version_purposes ALTER COLUMN date_updated DROP DEFAULT;
 
 COMMIT;
