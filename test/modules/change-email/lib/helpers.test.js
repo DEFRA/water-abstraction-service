0cb60f33a4ab670dc9933bb80f22f144b0657a3c
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-08-15 22:24:16 +0100

    Fix helper test

--- test/modules/change-email/lib/helpers.test.js
+++ test/modules/change-email/lib/helpers.test.js
@@ -1,32 +1,26 @@
 const { expect } = require('@hapi/code')
 const {
-  beforeEach,
-  afterEach,
   experiment,
   test
 } = exports.lab = require('@hapi/lab').script()
 
-const sinon = require('sinon')
-const sandbox = sinon.createSandbox()
-
 const helpers = require('../../../../src/modules/change-email/lib/helpers')
-const event = require('../../../../src/lib/event')
-
-const guidRegex = /[\d|\w]{8}-[\d|\w]{4}-[\d|\w]{4}-[\d|\w]{4}-[\d|\w]{12}/
+const Event = require('../../../../src/lib/models/event.js')
 
 experiment('createEventObject', () => {
-  beforeEach(() => {
-    sandbox.stub(event, 'create')
-  })
+  test('it creates a new event model instance', () => {
+    const result = helpers.createEventObject(
+      'old@example.co.uk', '0b082764-39df-41a8-8c61-11ea395da7e2', 'new@domain.com', '1234'
+    )
 
-  afterEach(async () => sandbox.restore())
-
-  test('it calls event.create with the expected arguments', () => {
-    helpers.createEventObject('test-username', 'test-id', 'teset-email@domain.com', '1234')
-    const [args] = event.create.lastCall.args
-    expect(args.eventId).to.match(guidRegex)
-    expect(args.issuer).to.equal('test-username')
-    expect(args.entities).to.equal(['test-id'])
-    expect(args.metadata.oldEmail).to.equal('test-username')
+    expect(result).to.be.an.instanceOf(Event)
+    expect(result.type).to.equal('user-account')
+    expect(result.subtype).to.equal('email-change')
+    expect(result.issuer).to.equal('old@example.co.uk')
+    expect(result.entities).to.equal([{ entityId: '0b082764-39df-41a8-8c61-11ea395da7e2' }])
+    expect(result.metadata).to.equal({
+      oldEmail: 'old@example.co.uk', newEmail: 'new@domain.com', userId: '1234'
+    })
+    expect(result.status).to.equal('completed')
   })
 })
