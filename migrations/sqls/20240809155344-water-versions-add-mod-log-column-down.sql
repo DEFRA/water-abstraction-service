72dd4c2bef32e7e857bcbeb75e34556d98f957d2
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-08-19 09:33:34 +0100

    Delete previous migrations

--- migrations/sqls/20240809155344-water-versions-add-mod-log-column-down.sql
+++
@@ -1,9 +0,0 @@
-/* revert changes made */
-
-BEGIN;
-
-ALTER TABLE water.charge_versions DROP COLUMN mod_log;
-ALTER TABLE water.return_versions DROP COLUMN mod_log;
-ALTER TABLE water.licence_versions DROP COLUMN mod_log;
-
-COMMIT;
