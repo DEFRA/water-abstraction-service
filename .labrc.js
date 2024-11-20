fae671bc941f5bda82a388a15d1e561bd97b9dc0
jonathangoulding <jonathangoulding21@gmail.com>
2024-11-20 16:18:35 +0000

    Spike upgrading to node v22
    https://eaflood.atlassian.net/browse/WATER-4729
    
    We want to upgrade to node v22, but we have a blocker with the xml parser we are using.
    
    The tool being used is libxmljs and has some open issues:
    
    https://github.com/libxmljs/libxmljs/issues/664
    https://github.com/libxmljs/libxmljs/issues/660
    
    Mainly the issues are:
    - is it being maintained
    - is there going to be a release to make it work with node v22.
    
    Rebuilding the package locally during our npm install has not worked, (through minimum effort) and if we did make it to build there is no guarantee it will actually work when built.
    
    This spike investigates the effort required to get any xml parser to work with the least possible effort.
    
    The change suggest using libxmljs2. Which works.
    
    However, if you look at the documentation it states : "LibXML bindings for node.js This package was forked as the original one is fairly unmaintained."
    
    Sounds good. But no it clearly now states it is "NO LONGER MAINTAINED".
    
    This can be a starting point to make a decision and we should not commit this branch.
    
    https://www.npmjs.com/package/xml-js looks a lot more popular and could do with a spike to assess the effort required to replace the existing parser.
    
    Lets discuss.

--- .labrc.js
+++ .labrc.js
@@ -4,13 +4,13 @@
 // This is overridden if arguments are passed to lab via the command line.
 module.exports = {
   verbose: true,
-  coverage: true,
+  // coverage: true,
   // Means when we use *.only() in our tests we just get the output for what we've flagged rather than all output but
   // greyed out to show it was skipped
   'silent-skips': true,
   // lcov reporter required for SonarCloud
-  reporter: ['console', 'html', 'lcov'],
-  output: ['stdout', 'coverage/coverage.html', 'coverage/lcov.info'],
+  // reporter: ['console', 'html', 'lcov'],
+  // output: ['stdout', 'coverage/coverage.html', 'coverage/lcov.info'],
   // This version global seems to be introduced by sinon.
   globals: [
     'version','payload','fetch','Response','Headers','Request','__coverage__','__extends','__assign','__rest',
--- scripts/fix-libxmljs-location.sh
+++ scripts/fix-libxmljs-location.sh
@@ -4,10 +4,13 @@
 # when running locally. This script fixes that issue
 
 echo "Checking if we need to move the libxmljs binary"
-if [ -e ./node_modules/libxmljs/build/Release/xmljs.node ]
-  then
-    echo "Binary is found, nothing to do"
-  else
-    cp ./node_modules/libxmljs/build/Release/obj.target/xmljs.node ./node_modules/libxmljs/build/Release/xmljs.node
-    echo "Copied binary"
-fi
+#if [ -e ./node_modules/libxmljs/build/Release/xmljs.node ]
+#  then
+#    echo "Binary is found, nothing to do"
+#  else
+#    cp ./node_modules/libxmljs/build/Release/obj.target/xmljs.node ./node_modules/libxmljs/build/Release/xmljs.node
+#    echo "Copied binary"
+#fi
+
+# Work out if this is needed after every install or just once
+# npm rebuild libxmljs2 --build-from-source
