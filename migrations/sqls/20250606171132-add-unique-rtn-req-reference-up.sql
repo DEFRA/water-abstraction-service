6777f98bbea8f84e3ca98e545cc9e377c4c216e1
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-06-07 19:57:31 +0100

    Version that works for CI
    The next problem was in CI, `return_requirements` is empty so `max_legacy_id` is NULL causing the query to fail.
    
    This new version supports the query being run in CI and in an environment with data.

--- migrations/sqls/20250606171132-add-unique-rtn-req-reference-up.sql
+++ migrations/sqls/20250606171132-add-unique-rtn-req-reference-up.sql
@@ -1,14 +1,18 @@
 DO $$
 DECLARE
   max_legacy_id INTEGER;
+  restart_value INTEGER;
 BEGIN
-  -- Get the highest legacy_id
-  SELECT MAX(legacy_id) INTO max_legacy_id FROM water.return_requirements;
+  -- Get highest legacy_id, default to 0 if no rows exist
+  SELECT COALESCE(MAX(legacy_id), 0) INTO max_legacy_id FROM water.return_requirements;
 
-  -- Create the sequence (starts at 1 by default)
+  -- Compute next value for sequence
+  restart_value := max_legacy_id + 1;
+
+  -- Create sequence
   CREATE SEQUENCE water.return_reference_seq;
 
-  -- Restart the sequence at max_legacy_id + 1
-  EXECUTE 'ALTER SEQUENCE water.return_reference_seq RESTART WITH ' || (max_legacy_id + 1);
+  -- Set sequence to start at next value
+  EXECUTE 'ALTER SEQUENCE water.return_reference_seq RESTART WITH ' || restart_value;
 END;
 $$;
