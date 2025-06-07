0283ab3e7004e079ac3bd6403efc6d8ec7242f95
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-06-07 17:52:30 +0100

    Debug

--- migrations/sqls/20250606171132-add-unique-rtn-req-reference-up.sql
+++ migrations/sqls/20250606171132-add-unique-rtn-req-reference-up.sql
@@ -34,8 +34,8 @@ ALTER TABLE water.return_requirements ADD COLUMN reference INTEGER;
 
 -- 2. Create a new sequence that will be used to generate new references. We initialise it to the highest `legacy_id`.
 DO $$
+  DECLARE max_legacy_id INTEGER;
   BEGIN
-    DECLARE max_legacy_id INTEGER;
     SELECT MAX(legacy_id) INTO max_legacy_id FROM water.return_requirements;
     EXECUTE format('CREATE SEQUENCE return_reference_seq START %s', max_legacy_id + 1);
   END
