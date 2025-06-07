ef359045a1ce412d7c6b7bb27cfb9f0c30a625f4
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-06-07 19:53:49 +0100

    Version that does not depend on format()

--- migrations/sqls/20250606171132-add-unique-rtn-req-reference-up.sql
+++ migrations/sqls/20250606171132-add-unique-rtn-req-reference-up.sql
@@ -1,7 +1,14 @@
 DO $$
-  DECLARE max_legacy_id INTEGER;
-  BEGIN
-    SELECT MAX(legacy_id) INTO max_legacy_id FROM water.return_requirements;
-    EXECUTE format('CREATE SEQUENCE water.return_reference_seq START WITH %s INCREMENT BY 1;', max_legacy_id + 1);
-  END;
+DECLARE
+  max_legacy_id INTEGER;
+BEGIN
+  -- Get the highest legacy_id
+  SELECT MAX(legacy_id) INTO max_legacy_id FROM water.return_requirements;
+
+  -- Create the sequence (starts at 1 by default)
+  CREATE SEQUENCE water.return_reference_seq;
+
+  -- Restart the sequence at max_legacy_id + 1
+  EXECUTE 'ALTER SEQUENCE water.return_reference_seq RESTART WITH ' || (max_legacy_id + 1);
+END;
 $$;
