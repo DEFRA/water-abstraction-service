26d338981e9340129bbbd8aa9893a9a942a88c46
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-06-07 20:27:14 +0100

    Change starting value in CI
    It better represents what a reference actually looks like, and its what our tests will expect.

--- migrations/sqls/20250606171132-add-unique-rtn-req-reference-up.sql
+++ migrations/sqls/20250606171132-add-unique-rtn-req-reference-up.sql
@@ -39,8 +39,8 @@ DECLARE
   max_legacy_id INTEGER;
   restart_value INTEGER;
 BEGIN
-  -- Get highest legacy_id, default to 0 if no rows exist
-  SELECT COALESCE(MAX(legacy_id), 0) INTO max_legacy_id FROM water.return_requirements;
+  -- Get highest legacy_id, default to 9999999 if no rows exist. This means the sequence will start at 10000000 in CI.
+  SELECT COALESCE(MAX(legacy_id), 9999999) INTO max_legacy_id FROM water.return_requirements;
 
   -- Compute next value for sequence
   restart_value := max_legacy_id + 1;
