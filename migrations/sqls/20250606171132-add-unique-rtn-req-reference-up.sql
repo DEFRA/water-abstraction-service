788959a9b4696b283223a9ad70fb1a0617d6aeaf
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-06-07 19:45:20 +0100

    Debug

--- migrations/sqls/20250606171132-add-unique-rtn-req-reference-up.sql
+++ migrations/sqls/20250606171132-add-unique-rtn-req-reference-up.sql
@@ -2,6 +2,6 @@ DO $$
   DECLARE max_legacy_id INTEGER;
   BEGIN
     SELECT MAX(legacy_id) INTO max_legacy_id FROM water.return_requirements;
-    EXECUTE format('CREATE SEQUENCE water.return_reference_seq START WITH %s', max_legacy_id + 1);
+    EXECUTE format('CREATE SEQUENCE water.return_reference_seq START WITH %s;', max_legacy_id + 1);
   END;
 $$;
