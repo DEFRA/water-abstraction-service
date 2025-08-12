1682c9a54927e876ccf72e133f25c315b2d45441
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-08-12 17:58:55 +0100

    Remove redundant code to update cust. file details

--- src/lib/services/invoice-accounts-service.js
+++ src/lib/services/invoice-accounts-service.js
@@ -204,13 +204,9 @@ const decorateWithInvoiceAccount = async model => {
   return model
 }
 
-const updateInvoiceAccountsWithCustomerFileReference =
-  invoiceAccountsConnector.updateInvoiceAccountsWithCustomerFileReference
-
 exports.getByInvoiceAccountIds = getByInvoiceAccountIds
 exports.getByInvoiceAccountId = getByInvoiceAccountId
 exports.decorateWithInvoiceAccount = decorateWithInvoiceAccount
 exports.deleteInvoiceAccount = deleteInvoiceAccount
 exports.createInvoiceAccount = createInvoiceAccount
 exports.createInvoiceAccountAddress = createInvoiceAccountAddress
-exports.updateInvoiceAccountsWithCustomerFileReference = updateInvoiceAccountsWithCustomerFileReference
