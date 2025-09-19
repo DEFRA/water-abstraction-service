babce82470687bcc92cf47dbc3c2f2b7a2ce6a9f
jonathangoulding <goulding.jon@icloud.com>
2025-09-19 09:51:52 +0100

    Add migration to add a return ids column to scheduled notification
    https://eaflood.atlassian.net/browse/WATER-5226
    
    The returns ids are currently stored in the metadata column.
    
    We want to make these easily accessible so we are adding a 'return_ids' column.
    
    This will store the return ids for a notification as a json array e.g ["v:12345"].
    
    Some notifications are linked to only one return id (such as the paper forms) but others are linked to multiple return ids. This is why we use the jsonb field and saved the ids in an array.

---
+++ migrations/sqls/20250919082701-add-returnids-to-scheduled-notification-up.sql
@@ -0,0 +1,12 @@
+/*
+  The returns ids are currently stored in the metadata column.
+
+  We want to make these easily accessible so we are adding a 'return_ids' column.
+
+  This will store the return ids for a notification as a json array e.g ["v:12345"].
+
+  Some notifications are linked to only one return id (such as the paper forms) but others are linked to multiple return
+  ids. This is why we use the jsonb field and saved the ids in an array.
+*/
+
+ALTER TABLE water.scheduled_notification ADD COLUMN return_ids JSONB DEFAULT NULL;
