53cac8d8fb055c15a0d5aa7cc672af6a28dc4d19
Jason Claxton <jason.claxton@defra.gov.uk>
2025-10-06 16:27:48 +0100

    Add `overall_status` and `status_counts` columns to events table https://eaflood.atlassian.net/browse/WATER-5306
    The `overall_status` column is populated with the overall status of related notifications. This status is calculated based on the statuses of all notifications associated with an event. It provides a quick reference to the overall status of the event without needing to check each notification individually.
    
    The `status_counts` column is a JSONB object that contains counts of each notification status type related to the event.

---
+++ migrations/20251006130947-add-overall-status-to-events.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+let Promise
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, _seedLink) {
+  Promise = options.Promise
+}
+
+exports.up = function (db) {
+  const filePath = path.join(__dirname, 'sqls', '20251006130947-add-overall-status-to-events-up.sql')
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
+      if (err) return reject(err)
+
+      resolve(data)
+    })
+  }).then(function (data) {
+    return db.runSql(data)
+  })
+}
+
+exports.down = function (db) {
+  const filePath = path.join(__dirname, 'sqls', '20251006130947-add-overall-status-to-events-down.sql')
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
+      if (err) return reject(err)
+
+      resolve(data)
+    })
+  }).then(function (data) {
+    return db.runSql(data)
+  })
+}
+
+exports._meta = {
+  version: 1
+}
