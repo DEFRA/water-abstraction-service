a2edb871df6f934adbdebdcdee31f02bf7045b59
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-11-15 17:42:15 +0000

    Update the query the check-status job uses
    Now it will only look at account-related notifications.

--- src/modules/batch-notifications/lib/queries.js
+++ src/modules/batch-notifications/lib/queries.js
@@ -22,16 +22,40 @@ const getSendingMessageBatch = async () => {
  * @return {Promise<Array>}
  */
 const getNotifyStatusChecks = async () => {
-  const query = `SELECT id
-    FROM water.scheduled_notification
-    WHERE send_after>(CURRENT_TIMESTAMP - interval '1 week')
-    AND (notify_status IS NULL OR notify_status NOT IN ('permanent-failure', 'technical-failure', 'delivered', 'received', 'validation-failed', 'cancelled'))
-    AND (next_status_check IS NULL OR next_status_check<=CURRENT_TIMESTAMP)
-    AND notify_id IS NOT NULL
-    AND status=$1
+  const query = `
+    SELECT
+      sn.id
+    FROM
+      water.scheduled_notification sn
+    WHERE
+      sn.send_after > (CURRENT_TIMESTAMP - interval '1 week')
+      AND (
+        sn.notify_status IS NULL
+        OR sn.notify_status NOT IN ('permanent-failure', 'technical-failure', 'delivered', 'received', 'validation-failed', 'cancelled')
+      )
+      AND (
+        sn.next_status_check IS NULL
+        OR sn.next_status_check <= CURRENT_TIMESTAMP
+      )
+      AND sn.notify_id IS NOT NULL
+      AND sn.status=$1
+      AND sn.message_ref IN (
+        'email_change_email_in_use_email',
+        'email_change_verification_code_email',
+        'existing_user_verification_email',
+        'expiry_notification_email',
+        'new_internal_user_email',
+        'new_user_verification_email',
+        'password_locked_email',
+        'password_reset_email',
+        'security_code_letter',
+        'share_existing_user',
+        'share_new_user'
+      );
   `
   const params = [MESSAGE_STATUS_SENT]
   const { rows } = await pool.query(query, params)
+
   return rows
 }
 
