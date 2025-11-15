df4369b30ee9c61ae59827034e5b7524015f663f
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-11-15 17:52:08 +0000

    Update send-message query
    Now it will only try and send account-related notifications.

--- src/modules/batch-notifications/lib/queries.js
+++ src/modules/batch-notifications/lib/queries.js
@@ -6,14 +6,35 @@ const { MESSAGE_STATUS_SENDING, MESSAGE_STATUS_SENT } = require('./message-statu
  * @return {Promise} resolves with messages in 'sending' status
  */
 const getSendingMessageBatch = async () => {
-  const query = `SELECT id
-    FROM water.scheduled_notification
-    WHERE status=$1
-    AND send_after<NOW() AND send_after>(CURRENT_TIMESTAMP - interval '1 week')
-    ORDER BY send_after
-    LIMIT 100`
+  const query = `
+    SELECT
+      sn.id
+    FROM
+      water.scheduled_notification sn
+    WHERE
+      sn.status = $1
+      AND sn.send_after < NOW() AND sn.send_after > (CURRENT_TIMESTAMP - interval '1 week')
+      AND sn.message_ref IN (
+        'email_change_email_in_use_email',
+        'email_change_verification_code_email',
+        'existing_user_verification_email',
+        'expiry_notification_email',
+        'new_internal_user_email',
+        'new_user_verification_email',
+        'password_locked_email',
+        'password_reset_email',
+        'security_code_letter',
+        'share_existing_user',
+        'share_new_user'
+      )
+    ORDER BY
+      send_after
+    LIMIT 100;
+  `
+
   const params = [MESSAGE_STATUS_SENDING]
   const { rows } = await pool.query(query, params)
+
   return rows
 }
 
