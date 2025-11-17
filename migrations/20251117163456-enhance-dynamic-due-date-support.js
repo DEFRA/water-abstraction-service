19e45be92ce2be4ae7cf82757471821a94e94a99
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-11-17 16:40:29 +0000

    Rename migration
    We wanted to update the timestamp because it is a little out of date and other migrations have been added since this PR was raised.
    
    But we also wanted to rename it due to the extra work it is now doing.

--- migrations/20251117163456-enhance-dynamic-due-date-support.js
+++ migrations/20251117163456-enhance-dynamic-due-date-support.js
@@ -13,7 +13,7 @@ exports.setup = function (options, _seedLink) {
 }
 
 exports.up = function (db) {
-  const filePath = path.join(__dirname, 'sqls', '20251104150325-drop-alternate-notification-id-column-up.sql')
+  const filePath = path.join(__dirname, 'sqls', '20251117163456-enhance-dynamic-due-date-support-up.sql')
   return new Promise(function (resolve, reject) {
     fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
       if (err) return reject(err)
@@ -28,7 +28,7 @@ exports.up = function (db) {
 }
 
 exports.down = function (db) {
-  const filePath = path.join(__dirname, 'sqls', '20251104150325-drop-alternate-notification-id-column-down.sql')
+  const filePath = path.join(__dirname, 'sqls', '20251117163456-enhance-dynamic-due-date-support-down.sql')
   return new Promise(function (resolve, reject) {
     fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
       if (err) return reject(err)
