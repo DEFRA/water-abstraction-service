eff255256fa4aac3b179db418f155635e0780638
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2026-01-14 13:18:02 +0000

    Add `return_log_id` to `review_returns` table (#2765)
    https://eaflood.atlassian.net/browse/WATER-5427
    
    To progress with the work being carried out in PR https://github.com/DEFRA/water-abstraction-system/pull/2800 we need to add the new UUID format `return_log_id` column to the `water.review_returns` table. This column will be used to join to the `returns.returns` table in place of the current `return_id`.
    
    The current `return_id` column will be dropped at a later date once the work to fix the return ID confusion is complete.

---
+++ migrations/20260113150329-add-return-log-id-to-review-returns.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+let Promise
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, _seedLink) {
+  Promise = options.Promise
+}
+
+exports.up = function (db) {
+  const filePath = path.join(__dirname, 'sqls', '20260113150329-add-return-log-id-to-review-returns-up.sql')
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
+      if (err) return reject(err)
+
+      resolve(data)
+    })
+  })
+    .then(function (data) {
+      return db.runSql(data)
+    })
+}
+
+exports.down = function (db) {
+  const filePath = path.join(__dirname, 'sqls', '20260113150329-add-return-log-id-to-review-returns-down.sql')
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
+      if (err) return reject(err)
+
+      resolve(data)
+    })
+  })
+    .then(function (data) {
+      return db.runSql(data)
+    })
+}
+
+exports._meta = {
+  version: 1
+}
