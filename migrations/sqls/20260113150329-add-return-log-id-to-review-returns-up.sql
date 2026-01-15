db0cc41054ac4f8917e87305c0ac014047d91735
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-01-15 10:34:48 +0000

    Amend migration 2 add return_log_id to review rtns (#2767)
    https://eaflood.atlassian.net/browse/WATER-5427
    
    We previously [Added return_log_id to review_returns table](https://github.com/DEFRA/water-abstraction-service/pull/2765).
    
    Having populated any existing records, the script then sets a `NOT NULL` constraint on the new column. Only this is failing because there are some orphaned review return records without a corresponding return log.
    
    This is in all environments, including `prod`.
    
    The main reason is that we assumed that when a TPT bill run is marked as ready, the associated review records would be deleted. This doesn't seem to be happening, so there are lots of records we didn't expect to be there.
    
    Second, if a new return version is added, any existing return logs in the period covered by the new version are 'voided'. If they have had no submissions or been marked as received, our overnight 'clean' job removes them.
    
    Either way, for the migration script to work, we need to delete any orphaned review return records before running it.

--- migrations/sqls/20260113150329-add-return-log-id-to-review-returns-up.sql
+++ migrations/sqls/20260113150329-add-return-log-id-to-review-returns-up.sql
@@ -5,7 +5,63 @@
 
   This migration will add a new return_log_id UUID field to the table water.review_returns. It will then populate this
   field with the corresponding id from the returns.returns table based on the return_id foreign key.
+
+  Some review_returns have become orphaned due to deletions in the returns.returns table, so before adding the column
+  and its NOT NUL constraint, we clean out review records that are linked to processed bill runs, and any orphaned
+  review returns.
 */
+
+-- Delete any review records that are orphaned, just in case any were left behind after a bill run has been cancelled.
+DELETE FROM
+  water.review_licences rl
+WHERE
+  NOT EXISTS (
+    SELECT 1 FROM water.billing_batches bb WHERE bb.billing_batch_id = rl.bill_run_id
+  );
+
+-- Delete any review records linked to processed bill runs. We've these deleted we can then just delete all orphaned
+-- review records
+DELETE FROM
+  water.review_licences rl
+WHERE
+  EXISTS (
+    SELECT 1 FROM water.billing_batches bb WHERE bb.status IN ('sent', 'error') AND bb.billing_batch_id = rl.bill_run_id
+  );
+
+DELETE FROM
+  water.review_charge_versions rcv
+WHERE
+  NOT EXISTS (
+    SELECT 1 FROM water.review_licences rl WHERE rl.id = rcv.review_licence_id
+  );
+
+DELETE FROM
+  water.review_charge_references rcr
+WHERE
+  NOT EXISTS (
+    SELECT 1 FROM water.review_charge_versions rcv WHERE rcv.id = rcr.review_charge_version_id
+  );
+
+DELETE FROM
+  water.review_charge_elements rce
+WHERE
+  NOT EXISTS (
+    SELECT 1 FROM water.review_charge_references rcr WHERE rcr.id = rce.review_charge_reference_id
+  );
+
+DELETE FROM
+  water.review_charge_element_returns rcer
+WHERE
+  NOT EXISTS (
+    SELECT 1 FROM water.review_charge_elements rce WHERE rce.id = rcer.review_charge_element_id
+  );
+
+DELETE FROM
+  water.review_returns rcr
+WHERE
+  NOT EXISTS (
+    SELECT 1 FROM water.review_licences rl WHERE rl.id = rcr.review_licence_id
+  );
 
 BEGIN;
   -- Add the column without NOT NULL constraint first
@@ -17,6 +73,12 @@ BEGIN;
       IF EXISTS
         (SELECT 1 FROM information_schema.tables WHERE table_schema = 'returns' AND table_name = 'returns')
       THEN
+        -- Final script to ensure no orphaned records remain
+        DELETE FROM water.review_returns rr WHERE NOT EXISTS (
+          SELECT 1 FROM "returns"."returns" r WHERE r.return_id = rr.return_id
+        );
+
+        -- Populate the column from existing data
         UPDATE water.review_returns rr
         SET return_log_id = r.id
         FROM "returns"."returns" r
