71292fd1f8a08aa2942ea433e19a098ebc8d9ec6
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-01-16 15:17:45 +0000

    Remove presroc supp. billing flag migration, again (#2770)
    https://eaflood.atlassian.net/browse/WATER-5451
    
    A long-standing issue with the old scheme and the legacy code has resulted in large numbers of licences being flagged for presroc supplementary billing. By design, there is no front-end way to remove billing flags to maintain an audit trail of licences that should be billed.
    
    B&D has completed work identifying licences with a billing flag, but they are billed to date. They've asked whether we can remove the billing flag, as there is no other way to do so.
    
    We've done this before (see <https://github.com/DEFRA/water-abstraction-service/pull/2623> and WATER-4644), thinking it would be the last time. We were wrong!
    
    We suspect something is going on, either with pre-sroc billing not removing the flags or with the import still flagging stuff it shouldn't. Either way, the sooner the legacy code is dead, the better!
    
    Till then, here is another migration. 😩

---
+++ migrations/sqls/20260116150521-unflag-presroc-flag-selected-licences-down.sql
@@ -0,0 +1,8 @@
+/*
+ * Remove presroc supp. billing flag migration
+ *
+ * https://eaflood.atlassian.net/browse/WATER-5451
+ *
+ * The up script is dealing with bad data. There is no value in putting bad data back into the service hence no down
+ * script is needed.
+*/
